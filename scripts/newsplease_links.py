ef0639df66e0ee916bcf30bc440e9ff651612778
Sean McGregor <gitcommit@seanbmcgregor.com>
2019-06-29 15:56:54 -0700

    added additional fields

--- scripts/newsplease_links.py
+++ scripts/newsplease_links.py
@@ -36,6 +36,30 @@ def get_json_name(incident_id, link_number):
     out_name =  incident_id + ".link." + str(link_number) + ".json"
     return out_name
 
+def add_csv_to_doc(doc, csv_row):
+    """Add various attributes to the document for indexing that are found in the CSV doc"""
+    doc["Submitter"] = ""
+    row_keys = [["From AI Failures list?", ["Submitter", "Catherine Olsson"]],
+        ["From published Yampolskiy papers", ["Submitter", "Roman Yampolskiy"]],
+        ["From Google form answers", ["Submitter", "Anonymous"]],
+        ["From unpublished Yampolskiy links", ["Submitter", "Roman Yampolskiy"]],
+        ["Start date", "Start Date"],
+        ["End date (if known)", "End Date"],
+        ["Meets preliminary definition", "Meets Preliminary Definition"],
+        ["Uncertain whether this is an incident?", "Uncertain"],
+        ["Short description of scenario", "Sam Yoon Description"],
+        ["Scale of harm (# of harmed)", "Sam Yoon Estimated Scale of Harm"],
+        ["Harmed party", "Sam Yoon Assessed Harmed Party"],
+        ["Harm caused", "Sam Yoon Assessed Harm Caused"],
+        ["Response or mitigation (as time allows)", "Sam Yoon Assessed Response or Mitigation"],
+        ["Search words", "Sam Yoon Search Words"]]
+    for row_key in row_keys:
+        if type(row_key[1]) is list:
+            if csv_row[row_key[0]] ==  "Y":
+                doc["Submitter"] = row_key[1][1]
+        else:
+            doc[row_key[1]] = csv_row[row_key[0]]
+
 def collect_jsons(filepath, web_capture_output_path):
     """Collect the web content associated with each incident record
     and save it to the disc as a JSON.
@@ -54,7 +78,7 @@ def collect_jsons(filepath, web_capture_output_path):
     print("Writing errors to logfile: ", logname)
 
     # Names of the incident link columns
-    link_fields = ['link ' + str(i) for i in range(1,31)] 
+    link_fields = ['Link ' + str(i) for i in range(1,31)]
 
     with open(filepath, 'r') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
@@ -68,24 +92,41 @@ def collect_jsons(filepath, web_capture_output_path):
                     json_name = get_json_name(row["Incident number"], idx)
                     json_path = os.path.join(web_capture_output_path, json_name)
                     if os.path.exists(json_path):
-                        print("Skipping: " + json_path)
-                        continue
-                    print("Collecting: " + row[link_number])
-                    print("...and storing to: " + json_path)
-                    try:
-                        article = NewsPlease.from_url(row[link_number])
-                        assert article.date_download is not None
-                        with open(json_path, 'w') as outfile:
-                            json.dump(article.get_dict(), outfile, indent=4, sort_keys=True, default=str)
-                    except Exception as err:
-                        print("Exception Written to Log")
-                        with open(logname, "a") as logfile:
-                            err_details = "{},{},{},{}\n".format(row[link_number], idx, link_number, str(err))
-                            logfile.write(err_details)
-
-def merge_web_with_local():
-    """todo"""
-    raise NotImplementedError
+                        print("Skipping download of: " + json_path)
+                        with open(json_path) as json_file:
+                            doc = json.load(json_file)
+                    else:
+                        print("Collecting: " + row[link_number])
+                        print("...and storing to: " + json_path)
+                        try:
+                            article = NewsPlease.from_url(row[link_number])
+                            assert article.date_download is not None
+                            doc = article.get_dict()
+                        except Exception as err:
+                            print("Exception Written to Log")
+                            with open(logname, "a") as logfile:
+                                err_details = "{},{},{},{}\n".format(row[link_number], idx, link_number, str(err))
+                                logfile.write(err_details)
+                            doc = {
+                                "authors": [],
+                                "date_download": "",
+                                "date_modify": "",
+                                "date_publish": "",
+                                "description": "",
+                                "filename": "",
+                                "image_url": "",
+                                "language": "",
+                                "localpath": "",
+                                "source_domain": "",
+                                "text": "",
+                                "title": "",
+                                "title_page": "",
+                                "title_rss": "",
+                                "url": ""
+                            }
+                    add_csv_to_doc(doc, row)
+                    with open(json_path, 'w') as outfile:
+                        json.dump(doc, outfile, indent=4, sort_keys=True, default=str)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
