d0124e17cfd4ab72c32c66d27daff474acbecb67
Sean McGregor <aiid_commit@seanbmcgregor.com>
2020-07-26 22:59:24 -0700

    make anchors not open new windows if they are internal

--- site/gatsby-gitbook/src/components/mdxComponents/anchor.js
+++ site/gatsby-gitbook/src/components/mdxComponents/anchor.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
 const AnchorTag = ({ children: link, ...props }) => {
+  let target = "_blank";
+  if(props.href.indexOf("/") === 0) {
+    target = "";
+  }
   if (link) {
     return (
-      <a href={props.href} target="_blank" rel="noopener noreferrer">
+      <a href={props.href} target={target} rel="noopener noreferrer">
         {link}
       </a>
     );
