79e20230440b91116595014cbe9ba3dd5a5bc32f
Sean McGregor <aiid_commit@seanbmcgregor.com>
2020-07-26 23:01:07 -0700

    remove github link from docs

--- site/gatsby-gitbook/src/templates/docs.js
+++ site/gatsby-gitbook/src/templates/docs.js
@@ -92,13 +92,6 @@ export default class MDXRuntimeTest extends Component {
         </Helmet>
         <div className={'titleWrapper'}>
           <StyledHeading>{mdx.fields.title}</StyledHeading>
-          <Edit className={'mobileView'}>
-            {docsLocation && (
-              <Link className={'gitBtn'} to={`${docsLocation}/${mdx.parent.relativePath}`}>
-                <img src={gitHub} alt={'Github logo'} /> Edit
-              </Link>
-            )}
-          </Edit>
         </div>
         <StyledMainWrapper>
           <MDXRenderer>{mdx.body}</MDXRenderer>
