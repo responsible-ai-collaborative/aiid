896dfe01a6226c0da1a876232a8fa478c7e43e07
Sean McGregor <aiid_commit@seanbmcgregor.com>
2020-08-11 09:49:30 -0700

    updated package.json

--- site/gatsby-gitbook/gatsby-browser.js
+++ site/gatsby-gitbook/gatsby-browser.js
@@ -1,6 +1,6 @@
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `This tutorial has been updated. ` +
+    `This website has been updated. ` +
       `Reload to display the latest version?`
   )
   if (answer === true) {
