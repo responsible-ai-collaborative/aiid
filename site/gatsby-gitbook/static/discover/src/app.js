428ebe2a28986646f35844b05dc96564c131bb9b
Sean McGregor <aiid_commit@seanbmcgregor.com>
2020-09-08 21:38:06 -0700

    fix modal title

--- site/gatsby-gitbook/static/discover/src/app.js
+++ site/gatsby-gitbook/static/discover/src/app.js
@@ -197,7 +197,7 @@ function renderIncidentDetailCard(hits, cardCSS) {
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Incident Citation Bibtex</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
