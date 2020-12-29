c05b11d43534207f089b068d9a7280d0b0ae6eb2
jmchorse <jmchorse.tech@gmail.com>
2020-12-29 11:44:01 -0800

    add typography utils

---
+++ site/gatsby-site/src/utils/typography.js
@@ -0,0 +1,35 @@
+/**
+ * Check if last word is parenthesized
+ *
+ * @param {string} str
+ * @return {boolean} result
+ */
+export function checkParenthetical(str) {
+  return /\((.*)\)/gm.test(str);
+}
+
+/**
+ * Get comma seperated formatted name. First Name is the last word of the string
+ *
+ * @param {string} str
+ * @return {string} "First Name, Last Name"
+ */
+export function getFormattedName(str) {
+  const split = str.split(" ");
+
+  // One word, return itself
+  if (split.length <= 1) {
+    return str;
+  }
+
+  if (checkParenthetical(split[split.length - 1])) {
+    const lastWord = split.pop();
+    if (split.length > 1) {
+      // More than two words + parenthesized word, return "FirstName, LastName"
+      return `${split[split.length - 1]}, ${split.slice(0, split.length - 1).join(" ")}`;
+    } else {
+      // One word + parenthesized word, return origin
+      return str;
+    }
+  }
+}
