3bd783bc8276c49252f7970b197773d82128e3bd
jt-mchorse <jmchorse.tech@gmail.com>
2021-03-16 08:44:12 -0700

    add new components

---
+++ site/gatsby-site/src/components/TextInputGroup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+import Label from './Label';
+import * as POP_OVERS from './PopOvers'
+
+const TextInputGroup = ({
+  name,
+  label,
+  placeholder,
+  value,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  ...props
+}) => (
+  <Form.Group>
+    <Label popover={POP_OVERS[name]} label={label} />
+    <Form.Control
+      type="text"
+      name={name}
+      placeholder={placeholder}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values[name]}
+      className={touched[name] && errors[name] ? 'has-error' : null}
+      {...props}
+    />
+    {touched[name] && errors[name] ? <div className="error-message">{errors[name]}</div> : null}
+  </Form.Group>
+);
+
+export default TextInputGroup;
