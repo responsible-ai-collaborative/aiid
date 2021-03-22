32b8fd221cf98b6ac9cd1ee25e76e63ef72ee926
Alex Musca <musca.alexandru@gmail.com>
2021-03-22 15:07:09 +0200

    Rename middleLayout uppercase

--- site/gatsby-site/src/components/index.js
+++ site/gatsby-site/src/components/index.js
@@ -2,5 +2,5 @@ export * from './theme';
 export mdxComponents from './mdxComponents';
 export ThemeProvider from './theme/themeProvider';
 export Layout from './layout';
-export MiddleLayout from './middleLayout';
+export MiddleLayout from './MiddleLayout';
 export Link from './link';
