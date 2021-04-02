b8db824d769ff5b9537db4fe23d892d4eab0cb0c
Alexandru Muscă <musca.alexandru@gmail.com>
2021-04-02 08:55:58 +0300

    Removed algolia css dependency from new discover app (#121)
    * Removed algolia css dependency from new discover app
    
    * Add algolia css for pagination

--- site/gatsby-site/pages/apps/discover.js
+++ site/gatsby-site/pages/apps/discover.js
@@ -27,7 +27,6 @@ import {
 import { useModal, CustomModal } from '../../src/components/useModal';
 import LayoutHideSidebar from 'components/LayoutHideSidebar';
 
-import '../../static/discover/src/algolia.css';
 import '../../static/discover/src/app.css';
 import '../../static/discover/src/index.css';
 
@@ -183,6 +182,81 @@ const RefinementListContainer = styled.div`
 
 const StyledPagination = styled(Pagination)`
   padding: 50px 0 50px 0;
+
+  .ais-Pagination {
+    color: #3a4570;
+  }
+
+  .ais-Pagination-list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+  }
+
+  .ais-Pagination-item + .ais-Pagination-item {
+    margin-left: 0.3rem;
+  }
+
+  .ais-Pagination-link {
+    color: #0096db;
+    -webkit-transition: color 0.2s ease-out;
+    transition: color 0.2s ease-out;
+    padding: 0.3rem 0.6rem;
+    display: block;
+    border: 1px solid #c4c8d8;
+    border-radius: 5px;
+    -webkit-transition: background-color 0.2s ease-out;
+    transition: background-color 0.2s ease-out;
+
+    :hover {
+      color: #0073a8;
+      background-color: #e3e5ec;
+    }
+
+    :focus {
+      color: #0073a8;
+      background-color: #e3e5ec;
+    }
+  }
+
+  .ais-Pagination-item--disabled .ais-Pagination-link {
+    opacity: 0.6;
+    cursor: not-allowed;
+    color: #a5abc4;
+
+    :hover {
+      color: #a5abc4;
+      background-color: #fff;
+    }
+
+    :focus {
+      color: #a5abc4;
+      background-color: #fff;
+    }
+  }
+
+  .ais-Pagination-item--selected .ais-Pagination-link {
+    color: #fff;
+    background-color: #0096db;
+    border-color: #0096db;
+
+    :hover {
+      color: #fff;
+    }
+
+    :focus {
+      color: #fff;
+    }
+  }
 `;
 
 const CardFooter = styled.div`
@@ -243,11 +317,47 @@ const CardBody = styled.div`
 const StyledSearchInput = styled.input`
   padding: 0.3rem 0.3rem !important;
   max-width: 100% !important;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  padding: 0.3rem 1.7rem;
+  width: 100%;
+  position: relative;
+  background-color: #fff;
+  border: 1px solid #c4c8d8;
+  border-radius: 5px;
+`;
+
+const SearchResetButton = styled.button`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  position: absolute;
+  z-index: 1;
+  width: 20px;
+  height: 20px;
+  top: 50%;
+  right: 0.3rem;
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+
+  padding: 0;
+  overflow: visible;
+  font: inherit;
+  line-height: normal;
+  color: inherit;
+  background: none;
+  border: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 `;
 
 const Header = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   max-width: 100%;
 
   @media (max-width: 767px) {
@@ -255,6 +365,15 @@ const Header = styled.div`
       display: none;
     }
   }
+`;
+
+const SearchContainer = styled.div`
+  flex-grow: 1;
+`;
+
+const SearchForm = styled.form`
+  display: block;
+  position: relative;
 `;
 
 const StyledRefinementList = ({
@@ -516,11 +635,10 @@ const StyledSearchBox = ({ refine, defaultRefinement, customRef }) => {
   }, 500);
 
   return (
-    <div className="ais-SearchBox flex-grow-1">
-      <form className="ais-SearchBox-form" noValidate>
+    <SearchContainer>
+      <SearchForm noValidate>
         <StyledSearchInput
           ref={customRef}
-          className="ais-SearchBox-input"
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="off"
@@ -531,18 +649,13 @@ const StyledSearchBox = ({ refine, defaultRefinement, customRef }) => {
           defaultValue={defaultRefinement}
           onChange={(event) => debouncedRefine(event.currentTarget.value)}
         />
-        <button
-          className="ais-SearchBox-reset"
-          type="reset"
-          title="Clear the search query."
-          onClick={() => refine('')}
-        >
+        <SearchResetButton type="reset" title="Clear the search query." onClick={() => refine('')}>
           <FontAwesomeIcon
             icon={faTimesCircle}
             className="pointer fa fa-times-circle"
             title="Authors"
           />
-        </button>
+        </SearchResetButton>
         <StyledStats
           translations={{
             stats(nbHits) {
@@ -554,8 +667,8 @@ const StyledSearchBox = ({ refine, defaultRefinement, customRef }) => {
             },
           }}
         />
-      </form>
-    </div>
+      </SearchForm>
+    </SearchContainer>
   );
 };
 
