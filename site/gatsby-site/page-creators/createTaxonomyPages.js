2cb12860633d5e97cfb7ab7b12f3feb5d68d75cc
Alex Musca <musca.alexandru@gmail.com>
2021-04-26 15:17:56 +0300

    Remove mock taxonomy and added real data and query

--- site/gatsby-site/page-creators/createTaxonomyPages.js
+++ site/gatsby-site/page-creators/createTaxonomyPages.js
@@ -1,37 +1,21 @@
 const path = require('path');
 
-const MOCK_TAXONOMY = [
-  {
-    namespace: 'CSET',
-    description:
-      'This should be a markdown document that will be rendered to HTML by the Gatsby build process',
-    fields: {
-      'field name 1': {
-        type: 'short_string',
-        default: '',
-        mongotype: 'typeval',
-        'short description': 'This goes in tooltips',
-        'long description': 'This goes in the documentation page',
-      },
-      annotator: {
-        type: 'string',
-        'short description': 'CSET researcher applying taxonomy',
-        'long description':
-          'The CSET taxonomy is presently a closed taxonomy, meaning only persons affiliated with CSET are permitted to act as editors within the CSET namespace. The person indicated by the annotator field is the one who applied the taxonomy to the incident.',
-      },
-      '...': {},
-    },
-  },
-];
-
 const createTaxonomyPages = (graphql, createPage) => {
   return new Promise((resolve, reject) => {
     resolve(
       graphql(
         `
           query Taxonomies {
-            allMongodbAiidprodIncidents {
-              distinct(field: incident_id)
+            allMongodbAiidprodTaxa {
+              nodes {
+                namespace
+                description
+                field_list {
+                  long_name
+                  long_description
+                  weight
+                }
+              }
             }
           }
         `
@@ -40,9 +24,9 @@ const createTaxonomyPages = (graphql, createPage) => {
           reject(result.errors);
         }
 
-        MOCK_TAXONOMY.forEach((taxaonomy) => {
+        result.data.allMongodbAiidprodTaxa.nodes.forEach((taxaonomy) => {
           createPage({
-            path: '/taxonomy/' + taxaonomy.namespace,
+            path: '/taxonomy/' + taxaonomy.namespace.toLowerCase(),
             component: path.resolve('./src/templates/taxonomy.js'),
             context: {
               taxaonomy,
--- site/gatsby-site/src/templates/taxonomy.js
+++ site/gatsby-site/src/templates/taxonomy.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Layout from 'components/Layout';
 import styled from 'styled-components';
+import { StyledHeading } from 'components/styles/Docs';
+import md5 from 'md5';
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
+`;
 
+const Description = styled(Row)`
   p {
-    padding-right: 1em;
+    font-size: 1.5em;
+    line-height: 1.5;
   }
+`;
+
+const Card = styled.div`
+  border: 1.5px solid #d9deee;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px 0px #e3e5ec;
+  display: flex;
+  flex-direction: column;
+  padding: 1em 2em 2em 2em;
+  margin-bottom: 2em;
+  width: 100%;
+`;
+
+const FieldNameHeading = styled.h1`
+  font-size: 20px;
+  font-weight: 800;
+  line-height: 1.5;
+  margin-bottom: 0.5em;
 `;
 
 const Taxonomy = (props) => {
@@ -16,34 +39,27 @@ const Taxonomy = (props) => {
     return null;
   }
 
-  const { namespace, description, fields } = props.pageContext.taxaonomy;
+  const { namespace, description, field_list } = props.pageContext.taxaonomy;
 
-  const fieldsArray = [];
+  const sortedFieldsArray = field_list.sort((a, b) => b.weight - a.weight);
 
-  for (const f in fields) {
-    fieldsArray.push({
-      name: f,
-      field: fields[f],
-    });
-  }
+  console.log(sortedFieldsArray);
 
   return (
     <Layout {...props}>
-      <Row>
-        <p>Namespace:</p>
-        <p>{namespace}</p>
-      </Row>
-      <Row>
-        <p>Description:</p>
+      <div className={'titleWrapper'}>
+        <StyledHeading>{namespace}</StyledHeading>
+      </div>
+      <Description>
         <p>{description}</p>
-      </Row>
-      <p>Fields:</p>
-      {fieldsArray.map(({ name, field }) => (
-        <Row key={name}>
-          <>
-            <p>{name}:</p>
-            <p>{JSON.stringify(field)}</p>
-          </>
+      </Description>
+      <h1 className="heading1">Fields</h1>
+      {sortedFieldsArray.map(({ long_name, long_description }) => (
+        <Row key={md5(long_name)}>
+          <Card>
+            <FieldNameHeading>{long_name}</FieldNameHeading>
+            {long_description}
+          </Card>
         </Row>
       ))}
     </Layout>
