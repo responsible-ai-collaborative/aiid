042089e50bc1c38b07109c17766e907ceb3741b9
Cesar Varela <mail@cesarvarela.com>
2021-07-16 21:03:54 -0300

    Add blog section to menu

---
+++ site/gatsby-site/src/components/blog/PostPreview.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import { format } from 'date-fns';
+
+const ThumbnailLink = styled(Link)`
+  display: block;
+  height: 300px;
+  margin: 0 -2rem 0;
+  @media (min-width: 991px) {
+    height: auto;
+    margin: 0 1rem -1rem -2rem;
+    justify-self: stretch;
+    flex: 1;
+  }
+`;
+
+const ThumbnailImg = styled(Img)`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+`;
+
+const TextContainer = styled.div`
+  margin: 1rem auto 0;
+  line-height: 1.7;
+  flex: 1;
+`;
+
+const Title = styled.div`
+  p {
+    font-size: 1.5em;
+  }
+`;
+
+const PostDate = styled.div`
+  p {
+    color: grey;
+  }
+`;
+
+const Description = styled.div`
+  padding-top: 0em;
+  p {
+    line-height: 1.5;
+    margin-bottom: 0;
+  }
+`;
+
+const Wrapper = styled.div`
+  margin-top: -0.75em;
+  padding: 0em 2em 1rem;
+  @media (min-width: 991px) {
+    display: flex;
+  }
+`;
+
+export default function PostPreview({ post }) {
+  return (
+    <Wrapper>
+      <ThumbnailLink to={post.fields.slug}>
+        <ThumbnailImg fluid={post.frontmatter.image.childImageSharp.fluid} />
+      </ThumbnailLink>
+      <TextContainer>
+        <Link to={post.fields.slug}>
+          <Title>
+            <p>{post.fields.title}</p>
+          </Title>
+        </Link>
+        <PostDate>
+          <p>{format(new Date(post.frontmatter.date), 'MMM d, yyyy')}</p>
+        </PostDate>
+        <Description>
+          <p>
+            {post.excerpt}... <Link to={post.fields.slug}>(Read More)</Link>
+          </p>
+        </Description>
+      </TextContainer>
+    </Wrapper>
+  );
+}
---
+++ site/gatsby-site/src/components/blog/PostsListing.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import PostPreview from './PostPreview';
+import { Card } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledCard = styled(Card)`
+  padding: 0.75rem 0 0;
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+export default function PostsListing() {
+  const {
+    allMdx: { edges: posts },
+  } = useStaticQuery(graphql`
+    query {
+      allMdx(
+        filter: { fileAbsolutePath: { glob: "**/content/blog/*" } }
+        sort: { fields: frontmatter___date, order: DESC }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+              title
+            }
+            excerpt
+            frontmatter {
+              date
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      {posts.map((p) => (
+        <StyledCard key={p.node.slug}>
+          <PostPreview post={p.node} />
+        </StyledCard>
+      ))}
+    </>
+  );
+}
