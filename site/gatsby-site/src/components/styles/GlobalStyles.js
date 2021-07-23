4295798f274992e290736801b0a745eb3a0a2714
Cesar Varela <mail@cesarvarela.com>
2021-07-23 19:59:50 -0300

    update injectGlobal to emotion 11

--- site/gatsby-site/src/components/styles/GlobalStyles.js
+++ site/gatsby-site/src/components/styles/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { injectGlobal } from '@emotion/css';
 
-export const baseStyles = injectGlobal`
+injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
   @import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap');
   * {
--- site/gatsby-site/src/components/theme/themeProvider.js
+++ site/gatsby-site/src/components/theme/themeProvider.js
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
-import { Global } from '@emotion/css';
 
 import { theme } from './index';
 import Header from '../Header';
-import { baseStyles } from '../styles/GlobalStyles';
+import '../styles/GlobalStyles';
 
 const ThemeProvider = ({ children }) => (
   <div>
-    <Global styles={baseStyles} />
     <Header />
     <EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
   </div>
