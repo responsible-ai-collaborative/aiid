8172ecc04a137f77538e6b0292dfa7c72e43b01d
Alex Musca <musca.alexandru@gmail.com>
2021-08-13 10:40:11 +0300

    Fetch data needed for form building

--- site/gatsby-site/src/components/TaxonomyForm.js
+++ site/gatsby-site/src/components/TaxonomyForm.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
-import { Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Row, OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
 import styled from 'styled-components';
+import { useUserContext } from 'contexts/userContext';
+import { useMongo } from 'hooks/useMongo';
+import { useFormik } from 'formik';
+import Loader from 'components/Loader';
 
 const ClassificationContainer = styled.div`
   display: flex;
@@ -12,7 +16,7 @@ const ClassificationContainer = styled.div`
 const TaxaCardHeader = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
 
   p {
@@ -44,12 +48,163 @@ const Container = styled.div`
   }
 `;
 
+const FormContainer = styled.div`
+  padding: 1em;
+`;
+
+const EditTaxonomyForm = ({ namespace }) => {
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState('');
+
+  // fetch latest taxonomy data
+  // populate form fields
+  // save new data
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: 'lkjlkj',
+      today: '2018-07-22',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const useRunTaxaQuery = () => {
+    return new Promise((resolve, reject) => {
+      const { runQuery } = useMongo();
+
+      try {
+        runQuery(
+          {
+            namespace,
+          },
+          (res) => {
+            resolve(res);
+          },
+          'mongodb-atlas',
+          'aiidprod',
+          'taxa'
+        );
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const useRunClassificationsQuery = () => {
+    return new Promise((resolve, reject) => {
+      const { runQuery } = useMongo();
+
+      try {
+        runQuery(
+          {
+            'classifications.Publish': true,
+            incident_id: 1,
+          },
+          (res) => {
+            resolve(res);
+          },
+          'mongodb-atlas',
+          'aiidprod',
+          'classifications'
+        );
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    Promise.all([useRunTaxaQuery(), useRunClassificationsQuery()])
+      .then((results) => {
+        console.log(results);
+
+        // Building the for structure
+        // TODO: handle multiple taxonomies/classifications
+        const taxonomy = results[0][0];
+        // console.log(taxonomy)
+
+        taxonomy.field_list.forEach((taxaField) => {
+          console.log({
+            display_type: taxaField.display_type,
+            mongo_type: taxaField.mongo_type,
+            permitted_values: taxaField.permitted_values,
+            placeholder: taxaField.placeholder,
+            required: taxaField.required,
+            short_description: taxaField.short_description,
+            short_name: taxaField.short_name,
+          });
+        });
+
+        const classifications = results[1][0].classifications;
+
+        console.log(classifications);
+
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError('Error fetching. Please try later.');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <Loader loading={loading} />;
+  }
+
+  if (error !== '') {
+    return <span>{error}</span>;
+  }
+
+  return (
+    <FormContainer>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <input
+          id="today"
+          name="today"
+          type="date"
+          onChange={formik.handleChange}
+          value={formik.values.today}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </FormContainer>
+  );
+};
+
 const TaxonomyForm = ({ taxonomy }) => {
   if (!taxonomy) {
     return null;
   }
 
+  const { isAdmin } = useUserContext();
+
   const [showAllClassifications, setShowAllClassifications] = useState(false);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
 
   const renderTooltip = (props, displayText) => (
     <Tooltip id="button-tooltip" {...props}>
@@ -62,38 +217,64 @@ const TaxonomyForm = ({ taxonomy }) => {
       <Container className="card">
         <TaxaCardHeader className="card-header">
           <h4>{`${taxonomy.namespace} Taxonomy Classifications`}</h4>
-          <a href={`/taxonomy/${taxonomy.namespace.toLowerCase()}`}>Taxonomy Details</a>
+          {isAdmin && (
+            <>
+              {isEditing ? (
+                <Button onClick={handleSubmit}>Save</Button>
+              ) : (
+                <Button onClick={() => setIsEditing(true)}>Edit</Button>
+              )}
+            </>
+          )}
+          <a
+            style={{ order: 2, marginLeft: 'auto' }}
+            href={`/taxonomy/${taxonomy.namespace.toLowerCase()}`}
+          >
+            Taxonomy Details
+          </a>
         </TaxaCardHeader>
-        {taxonomy.classificationsArray &&
-          taxonomy.classificationsArray
-            .filter((field) => {
-              if (showAllClassifications) return true;
-              if (!showAllClassifications && field.weight >= 50) {
-                return true;
-              }
-              return false;
-            })
-            .map((field) => (
-              <ClassificationContainer key={field.name} className="card-body">
-                <Field>
-                  <OverlayTrigger
-                    placement="left"
-                    delay={{ show: 100, hide: 400 }}
-                    overlay={(e) => renderTooltip(e, field.shortDescription)}
-                  >
-                    <p>{field.name}</p>
-                  </OverlayTrigger>
-                </Field>
-                <Value>{field.value}</Value>
-              </ClassificationContainer>
-            ))}
-        <button
-          type="button"
-          className="btn btn-secondary btn-sm btn-block assignment-button"
-          onClick={() => setShowAllClassifications(!showAllClassifications)}
-        >
-          {`Show ${showAllClassifications[taxonomy.namespace] ? 'Fewer' : 'All'} Classifications`}
-        </button>
+        <>
+          {!isEditing ? (
+            <>
+              {taxonomy.classificationsArray &&
+                taxonomy.classificationsArray
+                  .filter((field) => {
+                    if (showAllClassifications) return true;
+                    if (!showAllClassifications && field.weight >= 50) {
+                      return true;
+                    }
+                    return false;
+                  })
+                  .map((field) => (
+                    <ClassificationContainer key={field.name} className="card-body">
+                      <Field>
+                        <OverlayTrigger
+                          placement="left"
+                          delay={{ show: 100, hide: 400 }}
+                          overlay={(e) => renderTooltip(e, field.shortDescription)}
+                        >
+                          <p>{field.name}</p>
+                        </OverlayTrigger>
+                      </Field>
+                      <Value>{field.value}</Value>
+                    </ClassificationContainer>
+                  ))}
+              <button
+                type="button"
+                className="btn btn-secondary btn-sm btn-block assignment-button"
+                onClick={() => setShowAllClassifications(!showAllClassifications)}
+              >
+                {`Show ${
+                  showAllClassifications[taxonomy.namespace] ? 'Fewer' : 'All'
+                } Classifications`}
+              </button>
+            </>
+          ) : (
+            <>
+              <EditTaxonomyForm namespace={taxonomy.namespace} />
+            </>
+          )}
+        </>
       </Container>
     </Row>
   );
