d9f8754c2e26f05bc6e990c1e3ab5c7c15aa2ffb
Cesar Varela <mail@cesarvarela.com>
2021-10-07 15:49:17 -0700

    Add min and max ticks to timeline viz

--- site/gatsby-site/src/components/Timeline.js
+++ site/gatsby-site/src/components/Timeline.js
@@ -1,57 +1,72 @@
 import React from 'react';
-import { scaleTime, extent, timeFormat } from 'd3';
+import { scaleTime, scaleLinear, extent, bin, axisLeft, select } from 'd3';
+import styled from 'styled-components';
 
-const width = 960;
+const AxisLeft = ({ yScale, margin }) => {
+  const axisRef = (axis) => {
+    axis && axisLeft(yScale)(select(axis));
+  };
 
-const height = 500;
+  return <g ref={axisRef} transform={`translate(${margin.left}, 0)`} />;
+};
 
-const margin = { top: 20, right: 30, bottom: 65, left: 90 };
+const Title = styled.text`
+  font-size: 14px;
+`;
 
-const AxisLeft = ({ yScale, innerWidth, tickFormat, tickOffset = 3 }) =>
-  yScale.ticks().map((tickValue) => (
-    <g key={tickValue} className="tick" transform={`translate(0,${yScale(tickValue)})`}>
-      <line x2={innerWidth} />
-      <text key={tickValue} style={{ textAnchor: 'end' }} x={-tickOffset} dy=".32em">
-        {tickFormat(tickValue)}
-      </text>
+const Reports = ({ data, yScale, yValue, margin }) => {
+  const [, innerHeight] = yScale.range();
+
+  const minRadius = 3;
+
+  const maxRadius = 14;
+
+  const thresholds = Math.round(innerHeight / maxRadius);
+
+  const binned = bin().value(yValue).thresholds(thresholds)(data);
+
+  const radius = scaleLinear().domain([1, 6]).range([minRadius, maxRadius]);
+
+  return (
+    <g transform={`translate(${margin.left}, 0)`}>
+      {binned
+        .filter((b) => b.length > 0)
+        .map((b) => (
+          <g key={b.x0} transform={`translate(20,${yScale((b.x0 + b.x1) / 2)})`}>
+            <circle r={radius(b.length)} style={{ opacity: 0.7 }}>
+              <text>{b.length}</text>
+            </circle>
+            <a href={`#${b[0].mongodb_id}`}>
+              <Title dx={12} dy={4}>
+                {b[0].title}
+              </Title>
+            </a>
+          </g>
+        ))}
     </g>
-  ));
-
-const Reports = ({ data, yScale, yValue }) => {
-  return data.map((d) => (
-    <g key={d.mongodb_id} transform={`translate(20,${yScale(yValue(d))})`}>
-      <circle r={6} style={{ opacity: 0.7 }} />
-      <a href={`#${d.mongodb_id}`}>
-        <text dx={12} dy={6}>
-          {d.title}
-        </text>
-      </a>
-    </g>
-  ));
+  );
 };
 
 function Timeline({ items }) {
   const data = items[0].edges.map((item) => item.node);
 
-  const innerHeight = height - margin.top - margin.bottom;
+  const width = 640;
 
-  const innerWidth = width - margin.left - margin.right;
+  const height = 480;
+
+  const margin = { top: 20, right: 20, bottom: 20, left: 60 };
 
   const yValue = (d) => new Date(d.date_published);
 
-  const yScale = scaleTime().domain(extent(data, yValue)).range([0, innerHeight]).nice();
+  const yScale = scaleTime()
+    .domain(extent(data, yValue))
+    .range([margin.top, height - margin.top])
+    .nice();
 
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${margin.left},${margin.top})`}>
-        <AxisLeft
-          yScale={yScale}
-          innerWidth={innerWidth}
-          tickOffset={5}
-          tickFormat={timeFormat('%m/%Y')}
-        />
-        <Reports data={data} yScale={yScale} yValue={yValue} />
-      </g>
+    <svg width="100%" viewBox={`0 0 ${width} ${height}`}>
+      <AxisLeft yScale={yScale} margin={margin} />
+      <Reports data={data} yScale={yScale} yValue={yValue} margin={margin} />
     </svg>
   );
 }
