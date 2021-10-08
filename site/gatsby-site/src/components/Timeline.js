23a074df1a5f4c7cf6900153724320bc90ac84d4
Cesar Varela <mail@cesarvarela.com>
2021-10-07 17:12:39 -0700

    Different styling for data buckets

--- site/gatsby-site/src/components/Timeline.js
+++ site/gatsby-site/src/components/Timeline.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { scaleTime, scaleLinear, extent, bin, axisLeft, select } from 'd3';
+import { scaleTime, extent, bin, axisLeft, select } from 'd3';
 import styled from 'styled-components';
 
 const AxisLeft = ({ yScale, margin }) => {
@@ -12,36 +12,58 @@ const AxisLeft = ({ yScale, margin }) => {
 
 const Title = styled.text`
   font-size: 14px;
+  dominant-baseline: middle;
 `;
 
-const Reports = ({ data, yScale, yValue, margin }) => {
+const Line = styled.line`
+  stroke: #5b5b5b;
+  stroke-dasharray: 2;
+`;
+
+const Point = styled.circle``;
+
+const Count = styled.text`
+  fill: #fff;
+  text-anchor: middle;
+  dominant-baseline: middle;
+`;
+
+const DataPoint = ({ bucket, groupRadius, radius, yScale }) => {
+  return (
+    <g key={bucket.x0} transform={`translate(20,${yScale((bucket.x0 + bucket.x1) / 2)})`}>
+      <Point cy={0} r={bucket.length > 1 ? groupRadius : radius} />
+      {bucket.length > 1 && <Count>{bucket.length}</Count>}
+      <a href={`#${bucket[0].mongodb_id}`}>
+        <Title dx={16}>{bucket[0].title}</Title>
+      </a>
+    </g>
+  );
+};
+
+const Reports = ({ data, yScale, yValue, margin, size }) => {
   const [, innerHeight] = yScale.range();
 
-  const minRadius = 3;
+  const radius = 6;
 
-  const maxRadius = 14;
+  const groupRadius = 12;
 
-  const thresholds = Math.round(innerHeight / maxRadius);
+  const thresholds = Math.round(innerHeight / (groupRadius * 2));
 
   const binned = bin().value(yValue).thresholds(thresholds)(data);
 
-  const radius = scaleLinear().domain([1, 6]).range([minRadius, maxRadius]);
-
   return (
     <g transform={`translate(${margin.left}, 0)`}>
+      <Line x1={20} x2={20} y1={margin.top} y2={size.height - margin.bottom} />
       {binned
         .filter((b) => b.length > 0)
         .map((b) => (
-          <g key={b.x0} transform={`translate(20,${yScale((b.x0 + b.x1) / 2)})`}>
-            <circle r={radius(b.length)} style={{ opacity: 0.7 }}>
-              <text>{b.length}</text>
-            </circle>
-            <a href={`#${b[0].mongodb_id}`}>
-              <Title dx={12} dy={4}>
-                {b[0].title}
-              </Title>
-            </a>
-          </g>
+          <DataPoint
+            key={b.x0}
+            bucket={b}
+            yScale={yScale}
+            groupRadius={groupRadius}
+            radius={radius}
+          />
         ))}
     </g>
   );
@@ -50,9 +72,7 @@ const Reports = ({ data, yScale, yValue, margin }) => {
 function Timeline({ items }) {
   const data = items[0].edges.map((item) => item.node);
 
-  const width = 640;
-
-  const height = 480;
+  const size = { width: 640, height: 480 };
 
   const margin = { top: 20, right: 20, bottom: 20, left: 60 };
 
@@ -60,13 +80,13 @@ function Timeline({ items }) {
 
   const yScale = scaleTime()
     .domain(extent(data, yValue))
-    .range([margin.top, height - margin.top])
+    .range([margin.top, size.height - margin.top])
     .nice();
 
   return (
-    <svg width="100%" viewBox={`0 0 ${width} ${height}`}>
-      <AxisLeft yScale={yScale} margin={margin} />
-      <Reports data={data} yScale={yScale} yValue={yValue} margin={margin} />
+    <svg width="100%" viewBox={`0 0 ${size.width} ${size.height}`}>
+      <AxisLeft yScale={yScale} margin={margin} size={size} />
+      <Reports data={data} yScale={yScale} yValue={yValue} margin={margin} size={size} />
     </svg>
   );
 }
