a4c25a844d67841f3ed34266d6ef8a25792def14
Cesar Varela <mail@cesarvarela.com>
2021-10-27 02:06:40 -0400

    Fix wrong colum spacing on md breakpoint

--- site/gatsby-site/src/components/discover/Filters.js
+++ site/gatsby-site/src/components/discover/Filters.js
@@ -35,14 +35,14 @@ function Filters() {
   return (
     <>
       <Row className="d-none d-md-flex justify-content-md-between mb-3">
-        <Col className="d-flex mt-2 gap-1">
+        <Col className="d-flex mt-2 gap-1 col-md-auto">
           {['classifications', 'source_domain', 'authors', 'submitters', 'incident_id']
             .map((a) => REFINEMENT_LISTS.find((list) => list.attribute == a))
             .map((list) => (
               <Filter key={list.attribute} attribute={list.attribute} {...list} />
             ))}
         </Col>
-        <Col className="d-flex gap-1 mt-2">
+        <Col className="d-flex gap-1 mt-2 col-md-auto">
           {['epoch_incident_date', 'epoch_date_published', 'flag']
             .map((a) => REFINEMENT_LISTS.find((list) => list.attribute == a))
             .map((list) => (
