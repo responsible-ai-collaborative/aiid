1a6a2bdb0f6641d170cf11233d0e35c764d78b3f
Cesar Varela <mail@cesarvarela.com>
2021-11-01 13:33:27 -0300

    Set range input to disabled until algolia provides interval

--- site/gatsby-site/src/components/discover/filters/RangeInput.js
+++ site/gatsby-site/src/components/discover/filters/RangeInput.js
@@ -9,10 +9,6 @@ const formatDate = (epoch) => new Date(epoch * 1000).toISOString().substr(0, 10)
 const dateToEpoch = (date) => new Date(date).getTime() / 1000;
 
 const RangeInput = ({ faIcon, label, faClasses, min, max, currentRefinement, refine }) => {
-  if (!min || !max) {
-    return null;
-  }
-
   const onChange = ({ min, max }) => {
     if (currentRefinement.min !== min || currentRefinement.max !== max) {
       refine({ min, max });
@@ -21,42 +17,46 @@ const RangeInput = ({ faIcon, label, faClasses, min, max, currentRefinement, ref
 
   const touched = currentRefinement.min !== min || currentRefinement.max !== max;
 
+  const enabled = min && max;
+
   return (
     <Dropdown autoClose="outside">
-      <Dropdown.Toggle>
+      <Dropdown.Toggle disabled={!enabled}>
         <FontAwesomeIcon icon={faIcon} className={faClasses} />
         {` ${label}`}&nbsp;{touched && <Badge bg="secondary">!</Badge>}
       </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Form className="px-3">
-          <Form.Label>From Date:</Form.Label>
-          <Form.Control
-            required={true}
-            type="date"
-            min={formatDate(min)}
-            max={formatDate(max)}
-            value={formatDate(currentRefinement.min)}
-            onChange={(event) =>
-              onChange({ min: dateToEpoch(event.target.value), max: currentRefinement.max })
-            }
-            onKeyDown={(e) => e.preventDefault()}
-          />
+      {enabled && (
+        <Dropdown.Menu>
+          <Form className="px-3">
+            <Form.Label>From Date:</Form.Label>
+            <Form.Control
+              required={true}
+              type="date"
+              min={formatDate(min)}
+              max={formatDate(max)}
+              value={formatDate(currentRefinement.min)}
+              onChange={(event) =>
+                onChange({ min: dateToEpoch(event.target.value), max: currentRefinement.max })
+              }
+              onKeyDown={(e) => e.preventDefault()}
+            />
 
-          <Form.Label>To Date:</Form.Label>
-          <Form.Control
-            required={true}
-            type="date"
-            min={formatDate(min)}
-            max={formatDate(max)}
-            value={formatDate(currentRefinement.max)}
-            onChange={(event) =>
-              onChange({ min: currentRefinement.min, max: dateToEpoch(event.target.value) })
-            }
-            onKeyDown={(e) => e.preventDefault()}
-          />
-        </Form>
-      </Dropdown.Menu>
+            <Form.Label>To Date:</Form.Label>
+            <Form.Control
+              required={true}
+              type="date"
+              min={formatDate(min)}
+              max={formatDate(max)}
+              value={formatDate(currentRefinement.max)}
+              onChange={(event) =>
+                onChange({ min: currentRefinement.min, max: dateToEpoch(event.target.value) })
+              }
+              onKeyDown={(e) => e.preventDefault()}
+            />
+          </Form>
+        </Dropdown.Menu>
+      )}
     </Dropdown>
   );
 };
