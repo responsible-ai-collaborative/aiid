81be16ffad95ee2724d06783e12112a0937b97dd
Cesar Varela <mail@cesarvarela.com>
2021-11-02 18:25:29 -0300

    Allow filters to wrpa on between md and lg breackpoint

--- site/gatsby-site/src/components/discover/Filters.js
+++ site/gatsby-site/src/components/discover/Filters.js
@@ -7,7 +7,7 @@ function Filters() {
   return (
     <>
       <Row className="d-none d-md-flex justify-content-md-between mb-3">
-        <Col className="d-flex mt-2 gap-1 col-md-auto">
+        <Col className="d-flex mt-2 gap-1 col-md-auto flex-wrap">
           {['classifications', 'source_domain', 'authors', 'submitters', 'incident_id']
             .map((a) => REFINEMENT_LISTS.find((list) => list.attribute == a))
             .map((list) => (
