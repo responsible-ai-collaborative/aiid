cf2a31ed5412e86c1a348ed36f4e2934bc38e2a7
Cesar Varela <mail@cesarvarela.com>
2021-11-06 15:05:03 -0400

    Add Clear All filters button (#321)
    * Improve search box clear button
    
    * Use bootstrap input for searchbox and remove results count
    
    * Create stats component
    
    * Remove results count from searchbox and clean it up
    
    * Add clear filters button
    
    * Center stats component
    
    * Remove console log

--- site/gatsby-site/src/components/discover/Filters.js
+++ site/gatsby-site/src/components/discover/Filters.js
@@ -2,7 +2,9 @@ import React from 'react';
 import REFINEMENT_LISTS from 'components/discover/REFINEMENT_LISTS';
 import { Row, Col } from 'react-bootstrap';
 import Filter from './Filter';
+import Stats from './Stats';
 import styled from 'styled-components';
+import ClearFilters from './ClearFilters';
 
 const StyledFilter = styled(Filter)`
   .dropdown-toggle {
@@ -12,13 +14,23 @@ const StyledFilter = styled(Filter)`
 
 function Filters() {
   return (
-    <Row xs={1} md={2} lg={4} className="d-none d-md-flex mt-2 mb-4 g-2">
-      {REFINEMENT_LISTS.map((list) => (
-        <Col key={list.attribute}>
-          <StyledFilter type={list.type} {...list} />
+    <>
+      <Row xs={1} md={2} lg={4} className="d-none d-md-flex mt-2 mb-4 g-2">
+        {REFINEMENT_LISTS.map((list) => (
+          <Col key={list.attribute}>
+            <StyledFilter type={list.type} {...list} />
+          </Col>
+        ))}
+      </Row>
+      <Row className="d-none d-md-flex">
+        <Col className="d-flex align-items-center">
+          <Stats />
         </Col>
-      ))}
-    </Row>
+        <Col className="d-flex justify-content-end">
+          <ClearFilters>Clear Filters</ClearFilters>
+        </Col>
+      </Row>
+    </>
   );
 }
 
--- site/gatsby-site/src/components/discover/SearchBox.js
+++ site/gatsby-site/src/components/discover/SearchBox.js
@@ -1,65 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { connectSearchBox, Stats } from 'react-instantsearch-dom';
+import { connectSearchBox } from 'react-instantsearch-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { debounce } from 'debounce';
+import { InputGroup, FormControl, Row, Col } from 'react-bootstrap';
 
-const StyledSearchInput = styled.input`
-  padding: 0.3rem 0.3rem !important;
-  max-width: 100% !important;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  padding: 0.3rem 1.7rem;
-  width: 100%;
-  position: relative;
-  background-color: #fff;
-  border: 1px solid #c4c8d8;
-  border-radius: 5px;
-`;
-
-const SearchResetButton = styled.button`
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
+const SearchStatus = styled.div`
   position: absolute;
-  z-index: 1;
+  z-index: 4;
   width: 20px;
   height: 20px;
   top: 50%;
   right: 0.3rem;
-  -webkit-transform: translateY(-50%);
   transform: translateY(-50%);
-
-  padding: 0;
-  overflow: visible;
-  font: inherit;
-  line-height: normal;
-  color: inherit;
-  background: none;
-  border: 0;
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
 `;
 
-const SearchContainer = styled.div`
-  flex-grow: 1;
+const SearchResetButton = styled.button`
+  background: none;
+  user-select: none;
+  border: none;
 `;
 
 const SearchForm = styled.form`
   display: block;
   position: relative;
-`;
-
-const StyledStats = styled(Stats)`
-  position: absolute;
-  top: 4px;
-  right: 28px;
-  z-index: 1;
 `;
 
 function SearchBox({ currentRefinement, refine }) {
@@ -71,48 +36,46 @@ function SearchBox({ currentRefinement, refine }) {
     debouncedRefine(query);
   }, [query]);
 
+  useEffect(() => {
+    setQuery(currentRefinement);
+  }, [currentRefinement]);
+
   const clear = () => {
     setQuery('');
     refine('');
   };
 
   return (
-    <SearchContainer>
-      <SearchForm>
-        <StyledSearchInput
-          type="text"
-          autoComplete="off"
-          autoCorrect="off"
-          autoCapitalize="off"
-          placeholder="Search"
-          spellCheck="false"
-          maxLength={512}
-          value={query}
-          onKeyPress={(e) => {
-            e.key === 'Enter' && e.preventDefault();
-          }}
-          onChange={(event) => setQuery(event.currentTarget.value)}
-        />
-        <SearchResetButton type="reset" title="Clear the search query." onClick={() => clear()}>
-          <FontAwesomeIcon
-            icon={faTimesCircle}
-            className="pointer fa fa-times-circle"
-            title="reset"
-          />
-        </SearchResetButton>
-        <StyledStats
-          translations={{
-            stats(nbHits) {
-              return (
-                <span className="badge bg-secondary badge-pill">{`${
-                  nbHits === 0 ? 'No' : nbHits
-                } reports found`}</span>
-              );
-            },
-          }}
-        />
-      </SearchForm>
-    </SearchContainer>
+    <Row>
+      <Col>
+        <SearchForm>
+          <InputGroup className="position-relative">
+            <FormControl
+              placeholder="Type Here"
+              maxLength={512}
+              value={query}
+              onKeyPress={(e) => {
+                e.key === 'Enter' && e.preventDefault();
+              }}
+              onChange={(event) => setQuery(event.currentTarget.value)}
+            />
+            <SearchStatus>
+              {query !== '' ? (
+                <SearchResetButton
+                  type="reset"
+                  title="Clear the search query."
+                  onClick={() => clear()}
+                >
+                  <FontAwesomeIcon icon={faTimesCircle} className="pointer" title="clear" />
+                </SearchResetButton>
+              ) : (
+                <FontAwesomeIcon opacity={0.5} icon={faSearch} className="pointer" />
+              )}
+            </SearchStatus>
+          </InputGroup>
+        </SearchForm>
+      </Col>
+    </Row>
   );
 }
 
