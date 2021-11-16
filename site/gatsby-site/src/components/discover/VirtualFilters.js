a42cb61a514dac77dea829bb0e9655971d4d9e2e
Cesar Varela <mail@cesarvarela.com>
2021-11-16 04:04:03 -0300

    ADd search box to virtual filters

--- site/gatsby-site/src/components/discover/VirtualFilters.js
+++ site/gatsby-site/src/components/discover/VirtualFilters.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { connectRange, connectRefinementList } from 'react-instantsearch-core';
+import { connectRange, connectRefinementList, connectSearchBox } from 'react-instantsearch-core';
 import REFINEMENT_LISTS from './REFINEMENT_LISTS';
+import useSearch from './useSearch';
 
 const VirtualRefinementList = connectRefinementList(() => null);
 
 const VirtualRange = connectRange(() => null);
+
+const VirtualSearchBox = connectSearchBox(() => null);
 
 const componentsMap = {
   refinement: VirtualRefinementList,
@@ -13,16 +16,24 @@ const componentsMap = {
 
 // SEE: https://github.com/algolia/react-instantsearch/issues/302
 
-export default function VirtualFilters({ searchState }) {
-  return REFINEMENT_LISTS.map((list) => {
-    const Component = componentsMap[list.type];
+export default function VirtualFilters() {
+  const { searchState } = useSearch();
 
-    return (
-      <Component
-        key={list.attribute}
-        attribute={list.attribute}
-        defaultRefinement={searchState.refinementList[list.attribute] || []}
-      />
-    );
-  });
+  return (
+    <>
+      <VirtualSearchBox defaultRefinement={searchState.query} />
+
+      {REFINEMENT_LISTS.map((list) => {
+        const Component = componentsMap[list.type];
+
+        return (
+          <Component
+            key={list.attribute}
+            attribute={list.attribute}
+            defaultRefinement={searchState.refinementList[list.attribute] || []}
+          />
+        );
+      })}
+    </>
+  );
 }
