f8a27bc1ed711462fcbfed62f2fff60deb702b0f
Cesar Varela <mail@cesarvarela.com>
2021-11-30 18:42:38 -0300

    Run translate requests in parallel

--- site/gatsby-site/src/utils/translateIndex.js
+++ site/gatsby-site/src/utils/translateIndex.js
@@ -1,59 +1,82 @@
-// Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate').v2;
+
+const { queue } = require('async');
 
 const config = require('../../config');
 
 const cloneDeep = require('lodash.clonedeep');
 
-const translate = new Translate({ key: config.google.translateApikey });
+const translateClient = new Translate({ key: config.google.translateApikey });
 
 async function getLanguages() {
-  return await translate.getLanguages();
+  return await translateClient.getLanguages();
 }
 
+const getBytes = (string) => Buffer.byteLength(string, 'utf8');
+
+const keys = ['description', 'text', 'title'];
+
 async function translateDiscoverIndex({ index, to }) {
-  const keys = ['description', 'text', 'title'];
-
-  let batch = [];
-
-  // `Max payload size is 204800 bytes.`
-  const batchSize = 10;
+  const concurrency = 100;
 
   const translatedIndex = [];
 
+  const q = queue(async ({ entry, to }, done) => {
+    const translatedEntry = await translateIndexEntry({ entry, to });
+
+    translatedIndex.push(translatedEntry);
+
+    done();
+  }, concurrency);
+
   for (const entry of index) {
-    batch.push(entry);
-
-    if (batch.length == batchSize) {
-      const texts = [];
-
-      for (const batchEntry of batch) {
-        for (const key of keys) {
-          texts.push(batchEntry[key]);
-        }
-      }
-
-      const [results] = await translate.translate(texts, { to });
-
-      for (let i = 0; i < batch.length; i++) {
-        const translatedEntry = cloneDeep(batch[i]);
-
-        for (let j = 0; j < keys.length; j++) {
-          const key = keys[j];
-
-          const result = results[i * keys.length + j];
-
-          translatedEntry[key] = result;
-        }
-
-        translatedIndex.push(translatedEntry);
-      }
-
-      batch = [];
-    }
+    q.push({ entry, to });
   }
 
+  await q.drain();
+
   return translatedIndex;
+}
+
+async function translateIndexEntry({ entry, to }) {
+  const translatedEntry = cloneDeep(entry);
+
+  const payload = [];
+
+  let payloadSize = 0;
+
+  for (const key of keys) {
+    const text = entry[key];
+
+    payloadSize += getBytes(text);
+    payload.push(text);
+  }
+
+  console.log('Sending translate request:', payloadSize);
+
+  const [results] = await translate({ payload, to });
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+
+    const key = keys[i];
+
+    translatedEntry[key] = result;
+  }
+
+  return translatedEntry;
+}
+
+async function translate({ payload, to, mock = true }) {
+  if (mock) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([payload.map((result) => `translated-${to}-${result}`)]);
+      }, 1000);
+    });
+  } else {
+    return translateClient.translate(payload, { to });
+  }
 }
 
 module.exports = {
