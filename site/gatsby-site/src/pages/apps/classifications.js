f1386d224abcb98b514c6cab9361f8d2b48fe633
Alex Musca <musca.alexandru@gmail.com>
2021-12-01 13:17:53 +0200

    Fix column order

--- site/gatsby-site/src/pages/apps/classifications.js
+++ site/gatsby-site/src/pages/apps/classifications.js
@@ -506,11 +506,13 @@ export default function ClassificationsDbView(props) {
       accessor: 'actions',
     };
 
-    setColumnData([
-      actionsColumn,
-      incidentIdColumn,
-      ...taxaData[0].field_list.map(fieldToColumnMap),
-    ]);
+    if (columnData.length === 0) {
+      setColumnData([
+        actionsColumn,
+        incidentIdColumn,
+        ...taxaData[0].field_list.map(fieldToColumnMap),
+      ]);
+    }
 
     let rowQuery = {
       namespace: currentTaxonomy,
