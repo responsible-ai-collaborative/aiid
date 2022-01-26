6f8551de77f29c56cf25cda9177aa7f6fd0dbad1
Cesar Varela <mail@cesarvarela.com>
2022-01-26 15:58:15 -0300

    Convert method to async/await

--- site/gatsby-site/page-creators/createCitiationPages.js
+++ site/gatsby-site/page-creators/createCitiationPages.js
@@ -49,172 +49,161 @@ const getClassificationsArray = (incidentClassifications, taxonomy) => {
   return array;
 };
 
-const createCitiationPages = (graphql, createPage) => {
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          query IncidentIDs {
-            allMongodbAiidprodIncidents {
-              distinct(field: incident_id)
-              group(field: incident_id) {
-                fieldValue
-                edges {
-                  node {
-                    id
-                    submitters
-                    incident_date
-                    date_published
-                    incident_id
-                    report_number
-                    title
-                    url
-                    image_url
-                    cloudinary_id
-                    source_domain
-                    mongodb_id
-                    text
-                    authors
-                  }
-                }
-              }
-            }
-
-            allMongodbAiidprodClassifications(
-              filter: { classifications: { Publish: { eq: true } } }
-            ) {
-              nodes {
+const createCitiationPages = async (graphql, createPage) => {
+  const result = await graphql(
+    `
+      query IncidentIDs {
+        allMongodbAiidprodIncidents {
+          distinct(field: incident_id)
+          group(field: incident_id) {
+            fieldValue
+            edges {
+              node {
+                id
+                submitters
+                incident_date
+                date_published
                 incident_id
-                id
-                namespace
-                notes
-                classifications {
-                  Annotation_Status
-                  Annotator
-                  Ending_Date
-                  Beginning_Date
-                  Full_Description
-                  Intent
-                  Location
-                  Named_Entities
-                  Near_Miss
-                  Quality_Control
-                  Reviewer
-                  Severity
-                  Short_Description
-                  Technology_Purveyor
-                  AI_Applications
-                  AI_System_Description
-                  AI_Techniques
-                  Data_Inputs
-                  Financial_Cost
-                  Harm_Distribution_Basis
-                  Harm_Type
-                  Infrastructure_Sectors
-                  Laws_Implicated
-                  Level_of_Autonomy
-                  Lives_Lost
-                  Nature_of_End_User
-                  Physical_System
-                  Problem_Nature
-                  Public_Sector_Deployment
-                  Relevant_AI_functions
-                  Sector_of_Deployment
-                  System_Developer
-                  Publish
-                }
-              }
-            }
-
-            allMongodbAiidprodTaxa {
-              nodes {
-                id
-                namespace
-                weight
-                description
-                field_list {
-                  public
-                  display_type
-                  long_name
-                  short_name
-                  weight
-                  short_description
-                }
-              }
-            }
-
-            allMongodbAiidprodDuplicates {
-              nodes {
-                true_incident_number
-                duplicate_incident_number
+                report_number
+                title
+                url
+                image_url
+                cloudinary_id
+                source_domain
+                mongodb_id
+                text
+                authors
               }
             }
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors); // eslint-disable-line no-console
-          reject(result.errors);
         }
 
-        const {
-          allMongodbAiidprodIncidents,
-          allMongodbAiidprodClassifications,
-          allMongodbAiidprodDuplicates,
-          allMongodbAiidprodTaxa,
-        } = result.data;
-
-        // Incident reports list
-        const incidentReportsMap = {};
-
-        result.data.allMongodbAiidprodIncidents.group.map((g) => {
-          incidentReportsMap[g.fieldValue] = g.edges;
-        });
-
-        // Incident taxonomy and classifications
-        allMongodbAiidprodIncidents.distinct.forEach((incident_id) => {
-          const incidentClassifications = allMongodbAiidprodClassifications.nodes.filter(
-            (t) => t.incident_id.toString() === incident_id
-          );
-
-          const taxonomies = [];
-
-          allMongodbAiidprodTaxa.nodes.forEach((t) => {
-            taxonomies.push({
-              notes: incidentClassifications.notes,
-              namespace: t.namespace,
-              classificationsArray: getClassificationsArray(incidentClassifications, t),
-              taxonomyFields: t.field_list,
-            });
-          });
-
-          // Create citation pages
-          createPage({
-            path: '/cite/' + incident_id,
-            component: path.resolve('./src/templates/cite.js'),
-            context: {
-              incidentReports: incidentReportsMap[incident_id],
-              taxonomies,
-            },
-          });
-        });
-
-        // Create redirects
-        allMongodbAiidprodDuplicates.nodes.forEach(
-          ({ true_incident_number, duplicate_incident_number }) => {
-            createPage({
-              path: '/cite/' + duplicate_incident_number,
-              component: path.resolve('./src/templates/cite-duplicate.js'),
-              context: {
-                duplicate_incident_number: parseInt(duplicate_incident_number),
-                true_incident_number: parseInt(true_incident_number),
-              },
-            });
+        allMongodbAiidprodClassifications(filter: { classifications: { Publish: { eq: true } } }) {
+          nodes {
+            incident_id
+            id
+            namespace
+            notes
+            classifications {
+              Annotation_Status
+              Annotator
+              Ending_Date
+              Beginning_Date
+              Full_Description
+              Intent
+              Location
+              Named_Entities
+              Near_Miss
+              Quality_Control
+              Reviewer
+              Severity
+              Short_Description
+              Technology_Purveyor
+              AI_Applications
+              AI_System_Description
+              AI_Techniques
+              Data_Inputs
+              Financial_Cost
+              Harm_Distribution_Basis
+              Harm_Type
+              Infrastructure_Sectors
+              Laws_Implicated
+              Level_of_Autonomy
+              Lives_Lost
+              Nature_of_End_User
+              Physical_System
+              Problem_Nature
+              Public_Sector_Deployment
+              Relevant_AI_functions
+              Sector_of_Deployment
+              System_Developer
+              Publish
+            }
           }
-        );
-      })
-    );
+        }
+
+        allMongodbAiidprodTaxa {
+          nodes {
+            id
+            namespace
+            weight
+            description
+            field_list {
+              public
+              display_type
+              long_name
+              short_name
+              weight
+              short_description
+            }
+          }
+        }
+
+        allMongodbAiidprodDuplicates {
+          nodes {
+            true_incident_number
+            duplicate_incident_number
+          }
+        }
+      }
+    `
+  );
+
+  const {
+    allMongodbAiidprodIncidents,
+    allMongodbAiidprodClassifications,
+    allMongodbAiidprodDuplicates,
+    allMongodbAiidprodTaxa,
+  } = result.data;
+
+  // Incident reports list
+  const incidentReportsMap = {};
+
+  result.data.allMongodbAiidprodIncidents.group.map((g) => {
+    incidentReportsMap[g.fieldValue] = g.edges;
   });
+
+  // Incident taxonomy and classifications
+  allMongodbAiidprodIncidents.distinct.forEach((incident_id) => {
+    const incidentClassifications = allMongodbAiidprodClassifications.nodes.filter(
+      (t) => t.incident_id.toString() === incident_id
+    );
+
+    const taxonomies = [];
+
+    allMongodbAiidprodTaxa.nodes.forEach((t) => {
+      taxonomies.push({
+        notes: incidentClassifications.notes,
+        namespace: t.namespace,
+        classificationsArray: getClassificationsArray(incidentClassifications, t),
+        taxonomyFields: t.field_list,
+      });
+    });
+
+    // Create citation pages
+    createPage({
+      path: '/cite/' + incident_id,
+      component: path.resolve('./src/templates/cite.js'),
+      context: {
+        incidentReports: incidentReportsMap[incident_id],
+        taxonomies,
+      },
+    });
+  });
+
+  // Create redirects
+  allMongodbAiidprodDuplicates.nodes.forEach(
+    ({ true_incident_number, duplicate_incident_number }) => {
+      createPage({
+        path: '/cite/' + duplicate_incident_number,
+        component: path.resolve('./src/templates/cite-duplicate.js'),
+        context: {
+          duplicate_incident_number: parseInt(duplicate_incident_number),
+          true_incident_number: parseInt(true_incident_number),
+        },
+      });
+    }
+  );
 };
 
 module.exports = createCitiationPages;
