46dc24f30f64ee9ba18e1d2426fafbc8270c149f
Sean McGregor <aiid_commit@seanbmcgregor.com>
2022-02-04 15:49:45 -0800

    added PAI-drafted language

--- site/gatsby-site/src/components/landing/AboutPartnership.js
+++ site/gatsby-site/src/components/landing/AboutPartnership.js
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby';
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
@@ -6,19 +5,19 @@ export default function AboutPartnership({ className }) {
   return (
     <Card className={className}>
       <Card.Body>
-        <Card.Title as="h2">About Partnership on AI</Card.Title>
+        <Card.Title as="h2">Sponsors</Card.Title>
         <Card.Text className="fst-italic">
-          The <a href="http://partnershiponai.org/">Partnership on AI</a> is the leading forum
-          addressing the most important and difficult decisions on the future of AI. We are a
-          non-profit that invites diverse voices into the process of technical governance, design,
-          and deployment of AI technologies. Partners work together across industry, academia, and
-          civil society to understand the implications of AI advancements and ensure they benefit
-          society equitably.
+          As a Founding Sponsor of the AIID,{' '}
+          <a href="http://partnershiponai.org/">Partnership on AI</a> was instrumental in supporting
+          the start-up of the database, as well as providing ongoing support and guidance.
+          Partnership on AI also invested in promoting the social benefits of the AIID to their
+          Partners and other stakeholders and have an ongoing interest in supporting its outcomes.
         </Card.Text>
         <Card.Text className="fst-italic">
-          The AI Incident Database is one of PAI’s key projects because it is a tangible resource on
-          what can happen if AI is not built or deployed in a manner which considers real world
-          implications. <Link to={`/about/1-governance`}>(Learn More)</Link>
+          Learn more about Partnership on AI{' '}
+          <a href="https://partnershiponai.org/resource/tracking-when-ai-systems-fail/">
+            work on incidents
+          </a>
         </Card.Text>
       </Card.Body>
     </Card>
