f245e4b757c63ae15aa179ad47da7d6c4c9b4909
Sean McGregor <aiid_commit@seanbmcgregor.com>
2022-02-07 15:23:02 -0800

    updated messaging and added back a logo

--- site/gatsby-site/src/components/landing/AboutPartnership.js
+++ site/gatsby-site/src/components/landing/AboutPartnership.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledImage = styled.img`
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: space-around;
+  align-items: center;
+  width: 75%;
+  max-width: 80px;
+`;
 
 export default function AboutPartnership({ className }) {
   return (
     <Card className={className}>
       <Card.Body>
-        <Card.Title as="h2">Sponsors</Card.Title>
+        <Card.Title as="h2">Founding Sponsor</Card.Title>
         <Card.Text className="fst-italic">
-          As a Founding Sponsor of the AIID,{' '}
+          As a founding sponsor of the AIID,{' '}
           <a href="http://partnershiponai.org/">Partnership on AI</a> was instrumental in supporting
           the start-up of the database, as well as providing ongoing support and guidance.
           Partnership on AI also invested in promoting the social benefits of the AIID to their
           Partners and other stakeholders and have an ongoing interest in supporting its outcomes.
         </Card.Text>
-        <Card.Text className="fst-italic">
-          Learn more about Partnership on AI{' '}
-          <a href="https://partnershiponai.org/resource/tracking-when-ai-systems-fail/">
-            work on incidents
-          </a>
-        </Card.Text>
+        <a
+          href="https://partnershiponai.org/resource/tracking-when-ai-systems-fail/"
+          target="_blank" rel="noreferrer"
+        >
+          <StyledImage src="/partership-on-ai-logo-mobile.png" />
+        </a>
       </Card.Body>
     </Card>
   );
