205f296f887444a1cddf7c90f483a140cb157c42
Sean McGregor <gitcommit@seanbmcgregor.com>
2022-02-08 14:44:03 -0800

    fix scroll test (#441)
    * updated test scroll height
    
    * increased test tolerance
    
    Co-authored-by: Sean McGregor <aiid_commit@seanbmcgregor.com>

--- site/gatsby-site/cypress/integration/cite.js
+++ site/gatsby-site/cypress/integration/cite.js
@@ -33,7 +33,7 @@ describe('Cite pages', () => {
 
     cy.wait(1000);
 
-    cy.window().its('scrollY').should('be.closeTo', 15761, 1000);
+    cy.window().its('scrollY').should('be.closeTo', 17100, 1400);
   });
 
   it('Should scroll to report when clicking on a report in the timeline', () => {
@@ -45,7 +45,7 @@ describe('Cite pages', () => {
 
     cy.wait(1000);
 
-    cy.window().its('scrollY').should('be.closeTo', 4946, 400);
+    cy.window().its('scrollY').should('be.closeTo', 6215.5, 1400);
   });
 
   // Meanwhile there is not reproducible environment skip tests with admin permissions
