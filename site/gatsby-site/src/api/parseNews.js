ba1d75c2c9b47ebcd5591b3548bf07d6e65b14ad
Cesar Varela <mail@cesarvarela.com>
2022-03-01 19:57:38 -0300

    Move parsing endpoint to a Gatsby function

---
+++ site/gatsby-site/src/api/parseNews.js
@@ -0,0 +1,19 @@
+import Mercury from '@postlight/mercury-parser';
+import { format, parseISO } from 'date-fns';
+
+export default async function handler(req, res) {
+  const { url } = req.query;
+
+  const article = await Mercury.parse(url);
+
+  const response = {
+    title: article.title,
+    authors: [article.author],
+    date_published: format(parseISO(article.date_published), 'yyyy-MM-dd'),
+    date_downloaded: format(new Date(), 'yyyy-MM-dd'),
+    image_url: article.lead_image_url,
+    text: article.content,
+  };
+
+  res.status(200).json(response);
+}
