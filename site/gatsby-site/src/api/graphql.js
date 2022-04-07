e99a1f191b0a63a157b7cc4402389a35346d4514
Cesar Varela <mail@cesarvarela.com>
2022-04-07 03:14:03 -0300

    Stitch Realm graphQL onto local graphQL (#507)
    * Setup umzug
    
    * Use its own connection string
    
    * Add report_number increment migration
    
    * Skip migrations if missing env settings
    
    * Remove console log
    
    * Run migrations on build
    
    * Update docs
    
    * run migrations from gatsby build
    
    * Do not exit process
    
    * Skip migrations only in gatsby build
    
    * Skip migraitons if CONTEXT != production
    
    * Run migrations before source plugins load
    
    * Refactor landing page queries to use reports collection
    
    * Normalize incidents collection
    
    * Make migration less verbose
    
    * Update algolia index generator to use new collection
    
    * Update reports schema
    
    * Update report edit form
    
    * null values don't play nice with realm's graphql
    
    * prevent mongo driver to force undefined into null
    
    * Add missing schema rules for new reports collection
    
    * Fix flagging test
    
    * Fix discover tests
    
    * Update schema
    
    * Updates to realm schema
    
    * Prevent undefined to being forced to null
    
    * Handle linking a report to a different Incident
    
    * Add missing awaits to return appropiate incident_id
    
    * Handle new reports collection when promoting reports
    
    * Show toast on incident promotion
    
    * Create new incident when promoting report if id missing
    
    * increment global report number
    
    * Increase default limit of 100
    
    * Fix cite flagging test
    
    * Fix citation edit tests
    
    * Fix discover tests
    
    * Give scroll more time to complete
    
    * Add tests to submitted page
    
    * Reduce number of queries to promote a report
    
    * Add missing incident id
    
    * Add test to promote a submission to report
    
    * Add a test to check submission promotions to existing incidents
    
    * Add tests to dowloadIndex page
    
    * Add a test to edit report form when switching incident ids
    
    * Remove console log
    
    * Stich realm graphql into a local graphql
    
    * Filter out mutations
    
    * Enable api key auth
    
    * Enable cors from observable
    
    * Remove cors whitelist
    
    * Reduce flakiness
    
    * Add docs for the graphql endpoint
    
    * Update test to use deep include
    
    * Update test
    
    * Increase global timeouts to account for netlify slowness
    
    * Remove license
    
    * Improve docs

---
+++ site/gatsby-site/src/api/graphql.js
@@ -0,0 +1,73 @@
+import { introspectSchema, wrapSchema, FilterRootFields } from '@graphql-tools/wrap';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import fetch from 'cross-fetch';
+import { print } from 'graphql';
+import { stitchSchemas } from '@graphql-tools/stitch';
+import Cors from 'cors';
+import { graphqlHTTP } from 'express-graphql';
+import config from '../../config';
+
+const cors = Cors();
+
+// This custom executor is used to execute GraphQL queries against the Realm API
+// https://www.graphql-tools.com/docs/schema-wrapping#schema-wrapping
+
+async function realmExecutor({ document, variables }) {
+  const query = print(document);
+
+  const fetchResult = await fetch(
+    `https://realm.mongodb.com/api/client/v2.0/app/${config.realm.production_db.realm_app_id}/graphql`,
+    {
+      method: 'POST',
+      headers: {
+        apiKey: config.realm.graphqlApiKey,
+      },
+      body: JSON.stringify({ query, variables }),
+    }
+  );
+
+  return fetchResult.json();
+}
+
+// Cache the Graphql middleware to make use of Lambdas's Container reuse
+
+let graphqlMiddleware = null;
+
+export default async function handler(req, res) {
+  if (!graphqlMiddleware) {
+    // We want to expose a read-only api so Mutation operations are filtered out
+
+    const realmSubschema = wrapSchema({
+      schema: await introspectSchema(realmExecutor),
+      executor: realmExecutor,
+      transforms: [new FilterRootFields((operationName) => operationName != 'Mutation')],
+    });
+
+    // Root types (Mutation in this case) can't be empty so we add a dummy type to the definition
+    // https://github.com/graphql/graphql-spec/issues/568
+
+    const dummySchema = makeExecutableSchema({
+      typeDefs: [`type Mutation { _ : Boolean }`],
+    });
+
+    const gatewaySchema = stitchSchemas({
+      subschemas: [realmSubschema, dummySchema],
+    });
+
+    graphqlMiddleware = graphqlHTTP({ schema: gatewaySchema, graphiql: true });
+  }
+
+  // Manually run the cors middleware
+  // https://www.gatsbyjs.com/docs/reference/functions/middleware-and-helpers/#custom-middleware
+
+  await new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        reject(result);
+      }
+      resolve(result);
+    });
+  });
+
+  return graphqlMiddleware(req, res);
+}
