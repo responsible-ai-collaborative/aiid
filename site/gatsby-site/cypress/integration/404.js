3f9ea4f6744e5b229917dec5f434d57ad5746af9
Cesar Varela <mail@cesarvarela.com>
2022-04-11 14:38:11 -0300

    Move netlify plugin to dev

--- site/gatsby-site/cypress/integration/404.js
+++ site/gatsby-site/cypress/integration/404.js
@@ -2,16 +2,12 @@ describe('404 Page', () => {
   it('Successfully loads', () => {
     cy.visit('/');
 
-    cy.get('html')
-      .then(($html) => {
-        return $html.find('meta[content="environment=development"]').length === 0;
-      })
-      .then((isGatsbyBuild) => {
-        if (isGatsbyBuild) {
-          cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
+    cy.get('html').then(($html) => {
+      if ($html.find('meta[content="environment=development"]').length === 0) {
+        cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
 
-          cy.title().should('eq', 'Page not found');
-        }
-      });
+        cy.title().should('eq', 'Page not found');
+      }
+    });
   });
 });
