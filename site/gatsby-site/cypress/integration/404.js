441755a0db8329fb19c92e35de29e9ea2c4707b6
Cesar Varela <mail@cesarvarela.com>
2022-04-11 11:52:53 -0300

    Keep test runing for 404

--- site/gatsby-site/cypress/integration/404.js
+++ site/gatsby-site/cypress/integration/404.js
@@ -1,6 +1,6 @@
 describe('404 Page', () => {
   it('Successfully loads', () => {
-    cy.visit('/something-that-does-not-exist');
+    cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
 
     cy.title().should('eq', 'Page not found');
   });
