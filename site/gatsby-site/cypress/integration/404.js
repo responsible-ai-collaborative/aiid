bd650cf7073a1012cc36ccfa22ffaefd44a5c308
Cesar Varela <mail@cesarvarela.com>
2022-04-11 13:06:45 -0300

    Load root page first

--- site/gatsby-site/cypress/integration/404.js
+++ site/gatsby-site/cypress/integration/404.js
@@ -1,13 +1,17 @@
 describe('404 Page', () => {
   it('Successfully loads', () => {
+    cy.visit('/');
+
     cy.get('html')
       .then(($html) => {
         return $html.find('meta[content="environment=development"]').length === 0;
       })
-      .then(() => {
-        cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
+      .then((isGatsbyBuild) => {
+        if (isGatsbyBuild) {
+          cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
 
-        cy.title().should('eq', 'Page not found');
+          cy.title().should('eq', 'Page not found');
+        }
       });
   });
 });
