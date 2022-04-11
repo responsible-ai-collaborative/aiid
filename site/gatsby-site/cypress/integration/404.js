37b705da3d1b91abaccf93820cc02f31bd828b01
Cesar Varela <mail@cesarvarela.com>
2022-04-11 12:33:10 -0300

    skip test

--- site/gatsby-site/cypress/integration/404.js
+++ site/gatsby-site/cypress/integration/404.js
@@ -1,9 +1,9 @@
-if (process.env.CI) {
-  describe('404 Page', () => {
-    it('Successfully loads', () => {
-      cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
+const runOnCI = process.env.CI ? it : it.skip;
 
-      cy.title().should('eq', 'Page not found');
-    });
+describe('404 Page', () => {
+  runOnCI('Successfully loads', () => {
+    cy.visit('/something-that-does-not-exist', { failOnStatusCode: false });
+
+    cy.title().should('eq', 'Page not found');
   });
-}
+});
