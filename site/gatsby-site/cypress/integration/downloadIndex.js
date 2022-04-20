a1716a4f3f97c85ab7b47adc5ee748014f501e21
Cesar Varela <mail@cesarvarela.com>
2022-04-19 21:51:39 -0300

    Update test

--- site/gatsby-site/cypress/integration/downloadIndex.js
+++ site/gatsby-site/cypress/integration/downloadIndex.js
@@ -1,6 +1,43 @@
 import { gql } from '@apollo/client';
 import path from 'path';
 
+const testClassifications = [
+  'CSET:Harm Distribution Basis:Race',
+  'CSET:Harm Distribution Basis:Religion',
+  'CSET:Harm Distribution Basis:National origin or immigrant status',
+  'CSET:Harm Distribution Basis:Sex',
+  'CSET:Intent:Accident',
+  'CSET:Lives Lost:false',
+  'CSET:Location:Global',
+  'CSET:Named Entities:Microsoft',
+  'CSET:Named Entities:Twitter',
+  'CSET:Named Entities:Tay',
+  'CSET:Named Entities:Xiaoice',
+  'CSET:Near Miss:Harm caused',
+  'CSET:Severity:Minor',
+  'CSET:AI Applications:comprehension',
+  'CSET:AI Applications:language output',
+  'CSET:AI Applications:chatbot',
+  'CSET:AI Techniques:content creation',
+  'CSET:AI Techniques:language recognitionl natural language processing',
+  'CSET:Harm Type:Psychological harm',
+  'CSET:Harm Type:Harm to social or political systems',
+  'CSET:Level of Autonomy:Medium',
+  'CSET:Nature of End User:Amateur',
+  'CSET:Physical System:Software only',
+  'CSET:Problem Nature:Specification',
+  'CSET:Problem Nature:Robustness',
+  'CSET:Problem Nature:Assurance',
+  'CSET:Public Sector Deployment:false',
+  'CSET:Relevant AI functions:Perception',
+  'CSET:Relevant AI functions:Cognition',
+  'CSET:Relevant AI functions:Action',
+  'CSET:Sector of Deployment:Arts, entertainment and recreation',
+  'CSET:System Developer:Microsoft',
+  'CSET:Technology Purveyor:Microsoft',
+  'CSET:Technology Purveyor:Twitter',
+];
+
 describe('Download Algolia Index', () => {
   const url = '/downloadIndex';
 
@@ -51,42 +88,11 @@ describe('Download Algolia Index', () => {
           ref_number: 23,
         });
 
-        expect(report.classifications).to.deep.include([
-          'CSET:Harm Distribution Basis:Race',
-          'CSET:Harm Distribution Basis:Religion',
-          'CSET:Harm Distribution Basis:National origin or immigrant status',
-          'CSET:Harm Distribution Basis:Sex',
-          'CSET:Intent:Accident',
-          'CSET:Lives Lost:false',
-          'CSET:Location:Global',
-          'CSET:Named Entities:Microsoft',
-          'CSET:Named Entities:Twitter',
-          'CSET:Named Entities:Tay',
-          'CSET:Named Entities:Xiaoice',
-          'CSET:Near Miss:Harm caused',
-          'CSET:Severity:Minor',
-          'CSET:AI Applications:comprehension',
-          'CSET:AI Applications:language output',
-          'CSET:AI Applications:chatbot',
-          'CSET:AI Techniques:content creation',
-          'CSET:AI Techniques:language recognitionl natural language processing',
-          'CSET:Harm Type:Psychological harm',
-          'CSET:Harm Type:Harm to social or political systems',
-          'CSET:Level of Autonomy:Medium',
-          'CSET:Nature of End User:Amateur',
-          'CSET:Physical System:Software only',
-          'CSET:Problem Nature:Specification',
-          'CSET:Problem Nature:Robustness',
-          'CSET:Problem Nature:Assurance',
-          'CSET:Public Sector Deployment:false',
-          'CSET:Relevant AI functions:Perception',
-          'CSET:Relevant AI functions:Cognition',
-          'CSET:Relevant AI functions:Action',
-          'CSET:Sector of Deployment:Arts, entertainment and recreation',
-          'CSET:System Developer:Microsoft',
-          'CSET:Technology Purveyor:Microsoft',
-          'CSET:Technology Purveyor:Twitter',
-        ]);
+        expect(report.classifications.length).to.eq(testClassifications.length);
+
+        cy.wrap(report.classifications).each((key) => {
+          expect(testClassifications).to.include(key);
+        });
       });
     });
   });
