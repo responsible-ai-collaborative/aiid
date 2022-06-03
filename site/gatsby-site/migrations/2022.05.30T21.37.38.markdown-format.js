870fd147eecc81d63a1acb2113a95037277947ef
Cesar Varela <mail@cesarvarela.com>
2022-06-03 20:49:59 -0300

    Update documnets in batches so migration runs faster

--- site/gatsby-site/migrations/2022.05.30T21.37.38.markdown-format.js
+++ site/gatsby-site/migrations/2022.05.30T21.37.38.markdown-format.js
@@ -6,15 +6,32 @@
 const formatCollection = async (collection) => {
   const cursor = await collection.find({}, { projection: { _id: 1, text: 1 } });
 
+  let batch = [];
+
+  const batchSize = 10;
+
   while (await cursor.hasNext()) {
     const item = await cursor.next();
 
     // convert single newlines to markdown-compatible double newlines
     const text = item.text.replace(/(^|[^\n])\n(?!\n)/g, '$1\n\n');
 
-    await collection.updateOne({ _id: item._id }, { $set: { text, plain_text: text } });
+    const update = {
+      updateOne: {
+        filter: { _id: item._id },
+        update: { $set: { text, plain_text: text } },
+      },
+    };
 
     console.log(`Updated document ${item._id}`);
+
+    batch.push(update);
+
+    if (batch.length == batchSize || !(await cursor.hasNext())) {
+      await collection.bulkWrite(batch);
+
+      batch = [];
+    }
   }
 };
 
