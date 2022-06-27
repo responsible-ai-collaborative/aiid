b8dc3398de3ab0f50d81ee940dac641f5805f082
Luna McNulty <l.mcnulty@riseup.net>
2022-06-27 15:20:21 -0400

    Factor out card

--- site/gatsby-site/src/components/cite/SimilarIncidents.js
+++ site/gatsby-site/src/components/cite/SimilarIncidents.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { gql, useApolloClient } from '@apollo/client';
 import styled from 'styled-components';
 import { Card, Button } from 'react-bootstrap';
-import { format, parse } from 'date-fns';
+import { formatISO, format, parse } from 'date-fns';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlag, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { Image } from '../../utils/cloudinary';
@@ -37,6 +37,23 @@ const similarIncidentIdsQuery = gql`
   }
 `;
 
+const Subtitle = styled.p`
+  font-size: 120%;
+  font-weight: bold;
+  svg {
+    margin-left: 0.5ch;
+  }
+`;
+
+const FlagPrompt = styled.p`
+  margin-top: 1em;
+  margin-bottom: 1em;
+  svg {
+    margin-left: 0.5ch;
+    margin-right: 0.5ch;
+  }
+`;
+
 const SimilarIncidentsList = styled.div`
   margin-top: 2em;
   margin-bottom: 2em;
@@ -56,8 +73,15 @@ const SimilarIncidentsList = styled.div`
     font-size: 200%;
   }
   h3 {
-    margin-top: 0.5em;
+    margin-top: 1rem;
   }
+`;
+
+const IncidentCardImage = styled(Image)`
+  object-fit: cover;
+  width: calc(100% + 2.5em);
+  max-width: calc(100% + 2.5em);
+  margin: -1.25em -1.25em 0em -1.25em !important;
 `;
 
 const CardFooter = styled.div`
@@ -74,30 +98,6 @@ const FlexSeparator = styled.div`
   margin-right: auto;
 `;
 
-const IncidentCardImage = styled(Image)`
-  object-fit: cover;
-  width: calc(100% + 2.5em);
-  max-width: calc(100% + 2.5em);
-  margin: -1.25em -1.25em 0em -1.25em !important;
-`;
-
-const Subtitle = styled.p`
-  font-size: 120%;
-  font-weight: bold;
-  svg {
-    margin-left: 0.5ch;
-  }
-`;
-
-const FlagPrompt = styled.p`
-  margin-top: 1em;
-  margin-bottom: 1em;
-  svg {
-    margin-left: 0.5ch;
-    margin-right: 0.5ch;
-  }
-`;
-
 const FlagButton = styled(Button)`
   padding: 0px !important;
   color: var(--bs-gray-600) !important;
@@ -105,6 +105,39 @@ const FlagButton = styled(Button)`
     color: var(--bs-red) !important;
   }
 `;
+
+const SimilarIncidentCard = ({ incident }) => {
+  const parsedDate = parse(incident.date, 'yyyy-MM-dd', new Date());
+
+  return (
+    <Card>
+      <Card.Body>
+        <a href={'/cite/' + incident.incident_id}>
+          <IncidentCardImage
+            publicID={
+              incident.reports[0].cloudinary_id || `legacy/${md5(incident.reports[0].image_url)}`
+            }
+            alt=""
+            transformation={fill().height(480)}
+          />
+          <h3>{incident.title || incident.reports[0].title}</h3>
+        </a>
+        <CardFooter>
+          <div className="text-muted">
+            <time dateTime={formatISO(parsedDate)}>{format(parsedDate, 'MMM yyyy')}</time> ·{' '}
+            <span>
+              {incident.reports.length} {incident.reports.length == 1 ? 'report' : 'reports'}
+            </span>
+          </div>
+          <FlexSeparator />
+          <FlagButton variant="link">
+            <FontAwesomeIcon icon={faFlag} className="fa-flag" />
+          </FlagButton>
+        </CardFooter>
+      </Card.Body>
+    </Card>
+  );
+};
 
 const SimilarIncidents = ({ incident }) => {
   const client = useApolloClient();
@@ -149,34 +182,8 @@ const SimilarIncidents = ({ incident }) => {
         Did <strong>our</strong> AI mess up? Flag{' '}
         <FontAwesomeIcon icon={faFlag} className="fa-flag" /> unrelated incidents
       </FlagPrompt>
-      {similarIncidents.map((incident) => (
-        <Card key={incident.incident_id}>
-          <Card.Body>
-            <a href={'/cite/' + incident.incident_id}>
-              <IncidentCardImage
-                publicID={
-                  incident.reports[0].cloudinary_id ||
-                  `legacy/${md5(incident.reports[0].image_url)}`
-                }
-                alt=""
-                transformation={fill().height(480)}
-              />
-              <h3>{incident.title || incident.reports[0].title}</h3>
-            </a>
-            <CardFooter>
-              <div className="text-muted">
-                <time>{format(parse(incident.date, 'yyyy-MM-dd', new Date()), 'MMM yyyy')}</time> ·{' '}
-                <span>
-                  {incident.reports.length} {incident.reports.length == 1 ? 'report' : 'reports'}
-                </span>
-              </div>
-              <FlexSeparator />
-              <FlagButton variant="link">
-                <FontAwesomeIcon icon={faFlag} className="fa-flag" />
-              </FlagButton>
-            </CardFooter>
-          </Card.Body>
-        </Card>
+      {similarIncidents.map((similarIncident) => (
+        <SimilarIncidentCard incident={similarIncident} key={similarIncident.incident_id} />
       ))}
     </SimilarIncidentsList>
   ) : (
