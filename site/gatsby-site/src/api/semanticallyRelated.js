1ced06eb20379e00232d163dd8c453c7e5a2d4b8
Luna McNulty <l.mcnulty@riseup.net>
2022-06-27 09:29:24 -0400

    Add semanticallyRelated.js

---
+++ site/gatsby-site/src/api/semanticallyRelated.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+export default async function handler(req, res) {
+  const { text } = req.query;
+
+  // Example result
+  // {
+  //    "isBase64Encoded": false,
+  //    "statusCode": 200,
+  //    "headers": {"Content-Type": "application/json"},
+  //    "multiValueHeaders": {},
+  //    "body": {
+  //      "warnings": [],
+  //      "msg": "[(0.9972602725028992, 10), (0.996686577796936, 73), (0.9966337084770203, 134)]",
+  //      "best_url": "https://incidentdatabase.ai/apps/discover?display=details&incident_id=10"
+  //  }}
+  const url =
+    'https://q3z6vr2qvj.execute-api.us-west-2.amazonaws.com/text-to-db-similar?text=' +
+    encodeURIComponent(text);
+
+  axios
+    .get(url, { timeout: 30000 })
+    .then((lambdaResponse) =>
+      res.status(200).json({
+        // See: https://github.com/responsible-ai-collaborative/nlp-lambdas/issues/9
+        incidents: JSON.parse(
+          lambdaResponse.data.body.msg.replace(/\(/g, '[').replace(/\)/g, ']')
+        ).map((arr) => ({ incident_id: arr[1], similarity: arr[0] })),
+      })
+    )
+    .catch((error) => {
+      console.error(error);
+    });
+}
