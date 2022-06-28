e4e5da9380a734861ff1a499d9c2911991512df6
Luna McNulty <l.mcnulty@riseup.net>
2022-06-28 16:45:00 -0400

    Add buttons for designating similar, dissimilar incidents

--- site/gatsby-site/src/components/RelatedIncidentsArea.js
+++ site/gatsby-site/src/components/RelatedIncidentsArea.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ListGroup, Card, Spinner, Button } from 'react-bootstrap';
+import { ListGroup, Card, Spinner, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import { useFormikContext } from 'formik';
 
 const ListContainer = styled(Card)`
@@ -18,19 +18,39 @@ const ReportRow = styled(ListGroup.Item)`
     flex-shrink: 1;
     margin-right: auto;
   }
-  Button {
+  Button.set-id {
     margin-left: 1ch;
     flex-shrink: 0 !important;
     width: 8em;
   }
-  @media (max-width: 400px) {
-    Button {
-      width: unset;
-    }
-    Button .incident-id {
-      display: block;
-    }
+  @media (max-width: 860px) {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.5em;
   }
+`;
+
+const SimilarSelector = styled(ButtonGroup)`
+  margin-left: 1ch;
+`;
+
+const SimilarityButton = styled(Button)`
+  transition: 0.2s ease-in all !important;
+  opacity: 0.8 !important;
+  width: 2.5em;
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+  text-align: center;
+
+  :disabled {
+    opacity: 1 !important;
+    box-shadow: inset 0px 0px 4px 4px rgba(0, 0, 0, 0.05);
+  }
+`;
+
+const ReportToolbar = styled(ButtonToolbar)`
+  flex-shrink: 0;
+  align-items: center;
 `;
 
 const RelatedIncidentsArea = ({ columnKey, header, reports, loading, editable, error }) => {
@@ -38,7 +58,7 @@ const RelatedIncidentsArea = ({ columnKey, header, reports, loading, editable, e
     return null;
   }
 
-  const { setFieldValue } = editable ? useFormikContext() : { setFieldValue: null };
+  const { values, setFieldValue } = editable ? useFormikContext() : { setFieldValue: null };
 
   return (
     <ListContainer data-cy={`related-${columnKey}`}>
@@ -53,13 +73,96 @@ const RelatedIncidentsArea = ({ columnKey, header, reports, loading, editable, e
               <a href={val.url} target="_blank" rel="noreferrer">
                 {val.title}
               </a>
-              {val.incident_id && editable && (
-                <Button
-                  onClick={() => setFieldValue && setFieldValue('incident_id', val.incident_id)}
-                >
-                  Use&nbsp;ID&nbsp;<span className="incident-id">#{val.incident_id}</span>
-                </Button>
-              )}
+              <ReportToolbar>
+                {editable && (
+                  <>
+                    <label htmlFor="similar-selector">Related: </label>
+                    <SimilarSelector id="similar-selector">
+                      {[
+                        {
+                          variant: 'danger',
+                          icon: 'x',
+                          show:
+                            values.editor_dissimilar_incidents &&
+                            values.editor_dissimilar_incidents.includes(val.incident_id),
+                          onClick: () => {
+                            setFieldValue(
+                              'editor_dissimilar_incidents',
+                              (values.editor_dissimilar_incidents || []).concat([val.incident_id])
+                            );
+                            setFieldValue(
+                              'editor_similar_incidents',
+                              (values.editor_similar_incidents || []).filter(
+                                (id) => id != val.incident_id
+                              )
+                            );
+                          },
+                        },
+                        {
+                          variant: 'secondary',
+                          icon: '?',
+                          show:
+                            !values.editor_similar_incidents ||
+                            !values.editor_dissimilar_incidents ||
+                            (!values.editor_similar_incidents.includes(val.incident_id) &&
+                              !values.editor_dissimilar_incidents.includes(val.incident_id)),
+                          onClick: () => {
+                            setFieldValue(
+                              'editor_similar_incidents',
+                              (values.editor_similar_incidents || []).filter(
+                                (id) => id != val.incident_id
+                              )
+                            );
+                            setFieldValue(
+                              'editor_dissimilar_incidents',
+                              (values.editor_dissimilar_incidents || []).filter(
+                                (id) => id != val.incident_id
+                              )
+                            );
+                          },
+                        },
+                        {
+                          variant: 'success',
+                          icon: '✓',
+                          show:
+                            values.editor_similar_incidents &&
+                            values.editor_similar_incidents.includes(val.incident_id),
+                          onClick: () => {
+                            setFieldValue(
+                              'editor_similar_incidents',
+                              (values.editor_similar_incidents || []).concat([val.incident_id])
+                            );
+                            setFieldValue(
+                              'editor_dissimilar_incidents',
+                              (values.editor_dissimilar_incidents || []).filter(
+                                (id) => id != val.incident_id
+                              )
+                            );
+                          },
+                        },
+                      ].map((button) => (
+                        <SimilarityButton
+                          variant={button.show ? button.variant : 'secondary'}
+                          aria-pressed={button.show}
+                          disabled={button.show}
+                          onClick={button.onClick}
+                          key={button.icon}
+                        >
+                          {button.icon}
+                        </SimilarityButton>
+                      ))}
+                    </SimilarSelector>
+                  </>
+                )}
+                {val.incident_id && editable && (
+                  <Button
+                    className="set-id"
+                    onClick={() => setFieldValue && setFieldValue('incident_id', val.incident_id)}
+                  >
+                    Use&nbsp;ID&nbsp;<span className="incident-id">#{val.incident_id}</span>
+                  </Button>
+                )}
+              </ReportToolbar>
             </ReportRow>
           ))}
         {!loading && reports?.length == 0 && (
