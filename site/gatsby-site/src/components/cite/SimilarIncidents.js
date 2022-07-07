fb318777dd04265857a3f47846de1b373f1975fd
Luna McNulty <l.mcnulty@riseup.net>
2022-07-07 10:52:10 -0400

    Return to bottom layout

--- site/gatsby-site/src/components/cite/SimilarIncidents.js
+++ site/gatsby-site/src/components/cite/SimilarIncidents.js
@@ -19,13 +19,16 @@ const SimilarIncidentsList = styled.div`
   margin-bottom: 2em;
 
   .card {
-    margin-top: 1.5em;
-    margin-bottom: 1.5em;
     overflow: hidden;
     box-shadow: 0 2px 5px 0px #e3e5ec;
 
     a:not(:hover) {
       color: inherit;
+    }
+    h3 {
+      margin: 1rem 1rem 0.5rem 1rem;
+      font-size: 16pt;
+      hyphens: auto;
     }
   }
   p + .card {
@@ -34,16 +37,12 @@ const SimilarIncidentsList = styled.div`
   h2 {
     font-size: 200%;
   }
-  h3 {
-    margin-top: 1rem;
-  }
 `;
 
 const IncidentCardImage = styled(Image)`
   object-fit: cover;
-  width: calc(100% + 2.5em);
-  max-width: calc(100% + 2.5em);
-  margin: -1.25em -1.25em 0em -1.25em !important;
+  width: 100%;
+  aspect-ratio: 16 / 9;
 `;
 
 const CardFooter = styled.div`
@@ -52,6 +51,7 @@ const CardFooter = styled.div`
   flex-direction: row;
   align-items: center;
   font-weight: 500;
+  margin: 0rem 1rem 1rem 1rem;
 `;
 
 const FlexSeparator = styled.div`
@@ -88,6 +88,25 @@ const FlagButton = styled(Button)`
   }
 `;
 
+const CardSet = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  margin-bottom: 2em;
+  @media (min-width: 1300px) {
+    flex-direction: row;
+
+    > * {
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+  }
+
+  > * {
+    max-width: 700px;
+  }
+`;
+
 const SimilarIncidentCard = ({ incident, flaggable = true, flagged, parentIncident }) => {
   const parsedDate = incident.date ? parse(incident.date, 'yyyy-MM-dd', new Date()) : null;
 
@@ -101,64 +120,62 @@ const SimilarIncidentCard = ({ incident, flaggable = true, flagged, parentIncide
 
   return (
     <Card data-cy="similar-incident-card">
-      <Card.Body>
-        <a href={'/cite/' + incident.incident_id} data-cy="cite-link">
-          <IncidentCardImage
-            publicID={
-              incident.reports[0].cloudinary_id || `legacy/${md5(incident.reports[0].image_url)}`
-            }
-            alt=""
-            transformation={fill().height(480)}
-          />
-          <h3>{incident.title || incident.reports[0].title}</h3>
-        </a>
-        <CardFooter>
-          <div className="text-muted">
-            {parsedDate && (
-              <>
-                <time dateTime={formatISO(parsedDate)}>{format(parsedDate, 'MMM yyyy')}</time> ·{' '}
-              </>
-            )}
-            <span>
-              {incident.reports.length} {incident.reports.length == 1 ? 'report' : 'reports'}
-            </span>
-          </div>
-          <FlexSeparator />
-
-          {flaggable && (
-            <FlagButton
-              variant="link"
-              className={isFlagged ? ' flagged' : ''}
-              data-cy="flag-similar-incident"
-              onClick={async () => {
-                await updateIncident({
-                  variables: {
-                    query: { incident_id: parentIncident.incident_id },
-                    set: {
-                      flagged_dissimilar_incidents: isFlagged
-                        ? parentIncident.flagged_dissimilar_incidents.filter(
-                            (e) => e != incident.incident_id
-                          )
-                        : parentIncident.flagged_dissimilar_incidents
-                            .filter((e) => e != incident.incident_id)
-                            .concat([incident.incident_id]),
-                    },
-                  },
-                });
-                addToast({
-                  message: isFlagged
-                    ? `Flag reverted.`
-                    : `Incident flagged successfully. Our editors will remove it from this list if it not relevant.`,
-                  severity: SEVERITY.success,
-                });
-                setFlagged(!isFlagged);
-              }}
-            >
-              <FontAwesomeIcon icon={faFlag} />
-            </FlagButton>
+      <a href={'/cite/' + incident.incident_id} data-cy="cite-link">
+        <IncidentCardImage
+          publicID={
+            incident.reports[0].cloudinary_id || `legacy/${md5(incident.reports[0].image_url)}`
+          }
+          transformation={fill().height(480)}
+          alt=""
+        />
+        <h3>{incident.title || incident.reports[0].title}</h3>
+      </a>
+      <CardFooter>
+        <div className="text-muted">
+          {parsedDate && (
+            <>
+              <time dateTime={formatISO(parsedDate)}>{format(parsedDate, 'MMM yyyy')}</time> ·{' '}
+            </>
           )}
-        </CardFooter>
-      </Card.Body>
+          <span>
+            {incident.reports.length} {incident.reports.length == 1 ? 'report' : 'reports'}
+          </span>
+        </div>
+        <FlexSeparator />
+
+        {flaggable && (
+          <FlagButton
+            variant="link"
+            className={isFlagged ? ' flagged' : ''}
+            data-cy="flag-similar-incident"
+            onClick={async () => {
+              await updateIncident({
+                variables: {
+                  query: { incident_id: parentIncident.incident_id },
+                  set: {
+                    flagged_dissimilar_incidents: isFlagged
+                      ? parentIncident.flagged_dissimilar_incidents.filter(
+                          (e) => e != incident.incident_id
+                        )
+                      : parentIncident.flagged_dissimilar_incidents
+                          .filter((e) => e != incident.incident_id)
+                          .concat([incident.incident_id]),
+                  },
+                },
+              });
+              addToast({
+                message: isFlagged
+                  ? `Flag reverted.`
+                  : `Incident flagged successfully. Our editors will remove it from this list if it not relevant.`,
+                severity: SEVERITY.success,
+              });
+              setFlagged(!isFlagged);
+            }}
+          >
+            <FontAwesomeIcon icon={faFlag} />
+          </FlagButton>
+        )}
+      </CardFooter>
     </Card>
   );
 };
@@ -194,17 +211,7 @@ const SimilarIncidents = ({
   return (
     <SimilarIncidentsList>
       {(editor_similar_incidents.length > 0 || nlp_only_incidents.length > 0) && (
-        <h2 id="similar-incidents">
-          Similar Incidents
-          {isRole('incident_editor') && (
-            <EditIcon
-              href={`/incidents/edit?incident_id=${parentIncident.incident_id}#similar-incidents`}
-              title="Change the displayed similar incidents"
-            >
-              <FontAwesomeIcon icon={faEdit} />
-            </EditIcon>
-          )}
-        </h2>
+        <h2 id="similar-incidents">Similar Incidents</h2>
       )}
       {editor_similar_incidents.length > 0 && (
         <>
@@ -220,13 +227,15 @@ const SimilarIncidents = ({
               </EditIcon>
             )}
           </Subtitle>
-          {editor_similar_incidents.map((similarIncident) => (
-            <SimilarIncidentCard
-              incident={similarIncident}
-              flaggable={false}
-              key={similarIncident.incident_id}
-            />
-          ))}
+          <CardSet>
+            {editor_similar_incidents.map((similarIncident) => (
+              <SimilarIncidentCard
+                incident={similarIncident}
+                flaggable={false}
+                key={similarIncident.incident_id}
+              />
+            ))}
+          </CardSet>
         </>
       )}
 
@@ -256,14 +265,16 @@ const SimilarIncidents = ({
             Did <strong>our</strong> AI mess up? Flag <FontAwesomeIcon icon={faFlag} /> the
             unrelated incidents
           </FlagPrompt>
-          {nlp_only_incidents.map((similarIncident) => (
-            <SimilarIncidentCard
-              incident={similarIncident}
-              key={similarIncident.incident_id}
-              flagged={flagged_dissimilar_incidents.includes(similarIncident.incident_id)}
-              parentIncident={parentIncident}
-            />
-          ))}
+          <CardSet>
+            {nlp_only_incidents.map((similarIncident) => (
+              <SimilarIncidentCard
+                incident={similarIncident}
+                key={similarIncident.incident_id}
+                flagged={flagged_dissimilar_incidents.includes(similarIncident.incident_id)}
+                parentIncident={parentIncident}
+              />
+            ))}
+          </CardSet>
         </>
       )}
     </SimilarIncidentsList>
