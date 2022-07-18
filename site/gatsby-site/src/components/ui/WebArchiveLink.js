f491993f9d0e63973c0066504943ab530ee95888
Luna McNulty <l.mcnulty@riseup.net>
2022-07-18 15:59:25 -0400

    Make WebArchiveLinks split buttons with original and archived

--- site/gatsby-site/src/components/ui/WebArchiveLink.js
+++ site/gatsby-site/src/components/ui/WebArchiveLink.js
@@ -1,4 +1,24 @@
 import React from 'react';
+import { Dropdown, SplitButton } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const ArchiveOriginalSplit = styled(SplitButton)`
+  > *.dropdown-toggle {
+    padding: 0px !important;
+    margin: 0px !important;
+    margin-left: 2px !important;
+    width: 2ch;
+    color: inherit;
+  }
+  > *:not(.dropdown-toggle) {
+    padding: 0px !important;
+    margin: 0px !important;
+  }
+  a {
+    text-decoration: none;
+    font-weight: inherit;
+  }
+`;
 
 async function getSnapshotURL(url, date) {
   const timestamp = date ? date.replace('-', '') : '';
@@ -10,7 +30,7 @@ async function getSnapshotURL(url, date) {
   return response.archived_snapshots.closest.url.replace('http:', 'https:');
 }
 
-export default function WebArchiveLink({ url, date, children, className, title }) {
+export default function WebArchiveLink({ url, date, children, className }) {
   const onClick = async (e) => {
     e.preventDefault();
     const win = window.open('', '_blank');
@@ -25,15 +45,20 @@ export default function WebArchiveLink({ url, date, children, className, title }
   };
 
   return (
-    <a
-      title={title}
-      className={className}
-      onClick={onClick}
+    <ArchiveOriginalSplit
+      variant="link"
+      title={children}
       href={url}
       target="_blank"
       rel="noopener noreferrer"
+      className={className}
     >
-      {children}
-    </a>
+      <Dropdown.Item eventKey="1" href={url} target="_blank" rel="noopener noreferrer">
+        View the page at its original source
+      </Dropdown.Item>
+      <Dropdown.Item eventKey="2" onClick={onClick} target="_blank" rel="noopener noreferrer">
+        View the page archived by the Wayback Machine
+      </Dropdown.Item>
+    </ArchiveOriginalSplit>
   );
 }
