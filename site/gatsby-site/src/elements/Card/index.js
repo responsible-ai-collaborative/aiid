cc9ba42ded58f637a57bd9e65115e985c24c8f21
Clara Youdale <clara@botsfactory.io>
2022-07-19 13:24:28 -0300

    Add card component

---
+++ site/gatsby-site/src/elements/Card/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Card({ children }) {
+  return (
+    <>
+      <div className="p-10">{children}</div>
+    </>
+  );
+}
--- site/gatsby-site/tailwind.config.js
+++ site/gatsby-site/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/elements/**/*.{js,jsx,ts,tsx}',
     './src/templates/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
