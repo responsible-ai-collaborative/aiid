479c01c985d7b26ca11911612e0f966695eb9acc
Pablo Costa <pablo@botsfactory.io>
2022-07-20 17:40:29 -0300

    Change Cypress port to `9000`

--- site/gatsby-site/cypress.config.js
+++ site/gatsby-site/cypress.config.js
@@ -15,6 +15,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
-    baseUrl: 'http://localhost:8000/',
+    baseUrl: 'http://localhost:9000/',
   },
 });
