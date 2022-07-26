eb70a4fe2ac0e03d95ad401a1d6887c5463518ad
Clara Youdale <clara@botsfactory.io>
2022-07-26 13:39:53 -0300

    Tailwind setup

---
+++ site/gatsby-site/src/elements/Card/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export default function Card(props) {
+  return (
+    <>
+      <div
+        {...props}
+        className={`tw-border tw-rounded-lg tw-flex tw-break-words ${
+          props.className ? props.className : ''
+        } tw-flex-col tw-shadow-card ${props.bg ? 'tw-bg-' + props.bg : ''}`}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+}
+
+Card.Header = function CardHeader(props) {
+  return (
+    <div
+      className={`tw-flex ${
+        props.className ? props.className : ''
+      } tw-bg-light-grey tw-px-4 tw-py-2 tw-border-b tw-border-border-grey`}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+Card.Title = function CardTitle(props) {
+  return (
+    <>
+      <div className={`tw-flex-1 ${props.className ? props.className : ''}`}>
+        <h3 className="tw-text-2xl tw-mb-2">{props.children}</h3>
+      </div>
+    </>
+  );
+};
+
+Card.Body = function CardBody(props) {
+  return (
+    <>
+      <div className={`tw-flex-1 tw-p-4 ${props.className ? props.className : ''}`}>
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+Card.Footer = function CardFooter(props) {
+  return (
+    <>
+      <div
+        className={`tw-bg-light-grey tw-border-border-grey tw-py-2 tw-px-4 ${
+          props.className ? props.className : ''
+        }`}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+Card.Text = function CardText(props) {
+  return (
+    <>
+      <p className={`${props.className ? props.className : ''}`}>{props.children}</p>
+    </>
+  );
+};
--- site/gatsby-site/tailwind.config.js
+++ site/gatsby-site/tailwind.config.js
@@ -1,12 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/elements/**/*.{js,jsx,ts,tsx}',
     './src/templates/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-grey': 'rgba(0,0,0,.03)',
+        'border-grey': 'rgba(0,0,0,.125)',
+        'disable-grey': '#dee2e6',
+        'primary-blue': '#0d6efd',
+        'dark-grey': '#6c757d',
+        'deep-blue': '#0a58ca',
+        'secondary-grey': 'rgba(108,117,125,1)',
+        'text-light-grey': 'rgba(248,249,250,1)',
+        'border-light-grey': '#d9deee',
+        'default-gray': 'rgb(128, 128, 128)',
+        danger: '#dc3545',
+        'gray-900': '#212529',
+      },
+      boxShadow: {
+        card: '0 2px 5px 0px #e3e5ec',
+      },
+      transitionProperty: {
+        btn: 'color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out',
+      },
+      zIndex: {
+        2: '2',
+      },
+      borderWidth: {
+        2.5: '2.5px',
+      },
+      padding: {
+        0.8: '0.8rem',
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    'tw-btn-outline-primary',
+    'tw-btn',
+    'tw-bg-secondary',
+    'tw-tooltip-top',
+    'tw-tooltip-right',
+    'tw-tooltip-bottom',
+    'tw-tooltip-left',
+  ],
 };
