e8b1fac3d1ef858170a1f76040e5e180a0996483
Cesar Varela <mail@cesarvarela.com>
2022-07-26 18:00:30 -0300

    Add custom resolver to pull translations from reports

---
+++ site/realm/functions/reportTranslations.js
@@ -0,0 +1,8 @@
+exports = async (input, source) => {
+  
+    const translations = context.services.get('mongodb-atlas').db('translations').collection("reports_" + input);
+    
+    const translation = await translations.findOne({report_number: source.report_number});
+  
+  return { text: translation.text, title: translation.title };
+};
