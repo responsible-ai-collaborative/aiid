6d090695f809fe03857b82e75b4c43fbb283b998
Luna McNulty <l.mcnulty@riseup.net>
2022-07-26 14:53:13 -0400

    Fix failing test

--- site/gatsby-site/src/components/AiidHelmet.js
+++ site/gatsby-site/src/components/AiidHelmet.js
@@ -2,31 +2,46 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import config from '../../config';
 
-const AiidHelmet = ({ children, metaTitle, metaDescription, canonicalUrl, metaImage }) => (
-  <Helmet>
-    metaImage ||= config.siteMetadata.ogImage;
-    {metaTitle && <title>{metaTitle}</title>}
-    {metaTitle && <meta name="title" content={metaTitle} />}
-    {metaTitle && <meta property="og:title" content={metaTitle} />}
-    {metaTitle && <meta property="twitter:title" content={metaTitle} />}
-    {metaDescription && <meta property="og:description" content={metaDescription} />}
-    {metaDescription && <meta property="twitter:description" content={metaDescription} />}
-    {metaDescription && <meta name="description" content={metaDescription} />}
-    {metaImage && <meta property="twitter:image" content={metaImage} />}
-    {metaImage && <meta property="og:image" content={metaImage} />}
-    {config.siteMetadata.twitterAccount && (
-      <>
-        <meta name="twitter:site" content="@IncidentsDB" />
-        <meta name="twitter:creator" content="@IncidentsDB" />
-      </>
-    )}
-    <meta property="twitter:card" content="summary_large_image" />
-    {config.siteMetadata.favicon && (
-      <link rel="shortcut icon" type="image/svg" href={config.siteMetadata.favicon} />
-    )}
-    {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
-    {children}
-  </Helmet>
-);
+const AiidHelmet = ({
+  children,
+  metaTitle,
+  metaDescription,
+  canonicalUrl,
+  metaImage,
+  metaType,
+}) => {
+  const twitter = config.siteMetadata.twitterAccount;
+
+  metaImage ||= config.siteMetadata.ogImage;
+
+  return (
+    <Helmet>
+      {metaTitle && <title>{metaTitle}</title>}
+      {metaTitle && <meta name="title" content={metaTitle} />}
+      {metaTitle && <meta property="og:title" content={metaTitle} />}
+      {metaTitle && <meta property="twitter:title" content={metaTitle} />}
+      {metaDescription && <meta property="og:description" content={metaDescription} />}
+      {metaDescription && <meta property="twitter:description" content={metaDescription} />}
+      {metaDescription && <meta name="description" content={metaDescription} />}
+      {metaImage && <meta property="twitter:image" content={metaImage} />}
+      {metaImage && <meta property="og:image" content={metaImage} />}
+
+      <meta property="og:type" content={metaType || 'website'} />
+
+      {twitter && <meta name="twitter:site" content={twitter} />}
+      {twitter && <meta name="twitter:creator" content={twitter} />}
+
+      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
+      {canonicalUrl && <meta property="og:url" content={canonicalUrl} />}
+
+      <meta property="twitter:card" content="summary_large_image" />
+      {config.siteMetadata.favicon && (
+        <link rel="shortcut icon" type="image/svg" href={config.siteMetadata.favicon} />
+      )}
+
+      {children}
+    </Helmet>
+  );
+};
 
 export default AiidHelmet;
