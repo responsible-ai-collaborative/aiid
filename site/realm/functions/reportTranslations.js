d1deedeeb2f3d7c3a0d92c72aae181374ec72c05
Cesar Varela <mail@cesarvarela.com>
2022-07-27 19:30:02 -0300

    Handle missing translations

--- site/realm/functions/reportTranslations.js
+++ site/realm/functions/reportTranslations.js
@@ -1,8 +1,13 @@
 exports = async (input, source) => {
-  
-    const translations = context.services.get('mongodb-atlas').db('translations').collection("reports_" + input);
+
+  const translations = context.services.get('mongodb-atlas').db('translations').collection("reports_" + input);
     
-    const translation = await translations.findOne({report_number: source.report_number});
+  const translation = await translations.findOne({report_number: source.report_number});
   
-  return { text: translation.text, title: translation.title };
+  if(translation) {
+  
+    return { text: translation.text, title: translation.title };  
+  }
+  
+  return {text: "", title: ""}
 };
