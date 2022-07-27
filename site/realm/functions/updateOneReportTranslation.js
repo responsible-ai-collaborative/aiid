01f3772726cc7e6eed63005b814723b65deed43c
Cesar Varela <mail@cesarvarela.com>
2022-07-27 19:26:48 -0300

    Add mutation to edit translations

---
+++ site/realm/functions/updateOneReportTranslation.js
@@ -0,0 +1,26 @@
+exports = async (input) => {
+  
+  const translations = context.services.get('mongodb-atlas').db('translations').collection("reports_" + input.language);
+  
+  const update = {};
+  
+  if(input.text) {
+    
+    update.text = input.text;
+  }
+  
+  if(input.title) {
+    
+    update.title = input.title;
+  }
+  
+  await translations.updateOne({report_number: input.report_number}, {$set: {... update}}, {upsert: true});
+  
+  
+  const reports = context.services.get('mongodb-atlas').db('aiidprod').collection("reports");
+
+  const report = await reports.find({report_number: input.report_number}).limit(1).next();
+  
+  
+  return report;
+};
