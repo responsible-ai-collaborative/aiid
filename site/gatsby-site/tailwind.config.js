cb9accd59edfab8d5ed03d84f8c68daf8892afb9
Clara Youdale <clara@botsfactory.io>
2022-08-01 16:05:08 -0300

    Refactor configuration to make usable

--- site/gatsby-site/tailwind.config.js
+++ site/gatsby-site/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
