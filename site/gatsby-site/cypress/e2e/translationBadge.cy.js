b64e91ee8e8daa8e297066d40a74aead64a1ce75
Cesar Varela <mail@cesarvarela.com>
2022-08-04 17:49:36 -0300

    Add tests

---
+++ site/gatsby-site/cypress/e2e/translationBadge.cy.js
@@ -0,0 +1,21 @@
+describe('Translation Badges', () => {
+  it('Should be visible on blog post', () => {
+    cy.visit('/es/blog/using-ai-to-connect-ai-incidents');
+
+    cy.contains('.badge', 'Traducido por IA').should('be.visible');
+  });
+
+  it('Should be visible on the discover app', () => {
+    cy.visit('/es/apps/discover?display=details&incident_id=1&page=1&source_domain=today.com');
+
+    cy.get('[data-cy="5d34b8c29ced494f010ed45c"]')
+      .contains('.badge', 'Traducido por IA')
+      .should('be.visible');
+  });
+
+  it('Should be visible on an incident card on the citation page', () => {
+    cy.visit('/es/cite/1#r1');
+
+    cy.get('#r1').contains('.badge', 'Traducido por IA').should('be.visible');
+  });
+});
