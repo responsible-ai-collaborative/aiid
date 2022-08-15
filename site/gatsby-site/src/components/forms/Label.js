33b8954e3701c23dc36502cb639717577910c183
Cesar Varela <mail@cesarvarela.com>
2022-08-15 15:17:34 -0300

    Translate popovers

--- site/gatsby-site/src/components/forms/Label.js
+++ site/gatsby-site/src/components/forms/Label.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
-import { OverlayTrigger, Form } from 'react-bootstrap';
+import { OverlayTrigger, Form, Popover } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
+import { Trans, useTranslation } from 'react-i18next';
 
-const Label = ({ popover, placement, trigger, label }) => {
+const Label = ({ popover, label }) => {
   const [show, setShow] = useState(false);
 
-  if (!popover) {
+  const { i18n } = useTranslation(['popovers']);
+
+  if (!i18n.exists(popover, { ns: 'popovers' })) {
     return <Form.Label>{label} :</Form.Label>;
   }
+
   return (
     <OverlayTrigger
-      trigger={trigger}
-      placement={placement}
-      overlay={popover}
+      placement={'top'}
+      overlay={
+        <Popover data-cy={`popover-${popover}`}>
+          <Popover.Header as="h3">
+            <Trans ns="popovers" i18nKey={`${popover}.title`} />
+          </Popover.Header>
+          <Popover.Body>
+            <Trans ns="popovers" i18nKey={`${popover}.text`} />
+          </Popover.Body>
+        </Popover>
+      }
       {...(show ? { show } : {})}
     >
-      <Form.Label>
+      <Form.Label data-cy={`label-${popover}`}>
         {label}{' '}
         <FontAwesomeIcon
           icon={faQuestionCircle}
