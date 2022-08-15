d9a2a7063afacab8934ee335a666ad48f59d62b8
Pablo Costa <pablo@botsfactory.io>
2022-08-15 15:07:03 -0300

    Cypress to wait for port 8000

--- site/gatsby-site/cypress.config.js
+++ site/gatsby-site/cypress.config.js
@@ -16,6 +16,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
-    baseUrl: 'http://localhost:9000/',
+    baseUrl: 'http://localhost:8000/',
   },
 });
