6f66f45dfd97bc2df2dec3cdb8e13dc65108474f
Luna McNulty <l.mcnulty@riseup.net>
2022-08-17 11:23:31 -0400

    Add more tests

--- site/gatsby-site/cypress/e2e/tsneVisualization.cy.js
+++ site/gatsby-site/cypress/e2e/tsneVisualization.cy.js
@@ -5,4 +5,27 @@ describe('TSNE Visualization', () => {
     cy.visit(url);
     cy.get('[data-cy="tsne-visualization"] [data-cy="tsne-plotpoint"]').should('exist');
   });
+
+  it('Should highlight source incident when one exists', () => {
+    cy.visit(url + '?incident=1');
+    cy.get('[data-cy="tsne-visualization"] [data-cy="tsne-plotpoint"].current')
+      .should('exist')
+      .should('be.visible');
+  });
+
+  it('Should show an incident card on hover', () => {
+    cy.visit(url);
+    cy.get('[data-cy="tsne-visualization"] #spatial-incident-1').trigger('mouseover');
+    cy.get('[data-cy="tsne-visualization"] #spatial-incident-1 + [data-cy="incident-card"]').should(
+      'be.visible'
+    );
+  });
+
+  it('Incident card should show title', () => {
+    cy.visit(url);
+    cy.get('[data-cy="tsne-visualization"] #spatial-incident-1').trigger('mouseover');
+    cy.get(
+      '[data-cy="tsne-visualization"] #spatial-incident-1 + [data-cy="incident-card"] [data-cy="title"]'
+    ).should('be.visible');
+  });
 });
