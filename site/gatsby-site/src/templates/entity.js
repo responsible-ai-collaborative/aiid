6bdd4b6e03da510205fc825525176394e4df95c4
Cesar Varela <mail@cesarvarela.com>
2022-08-25 20:57:24 -0300

    Add categories to listings

--- site/gatsby-site/src/templates/entity.js
+++ site/gatsby-site/src/templates/entity.js
@@ -6,14 +6,30 @@ import React from 'react';
 const EntityPage = ({ pageContext, data, ...props }) => {
   const { name } = pageContext;
 
-  const { incidents } = data;
+  const { incidentsAsDeployer, incidentsAsDeveloper, incidentsAsBoth } = data;
 
   return (
     <Layout {...props}>
       <h1>{name}</h1>
 
-      <h2>Associated incidents</h2>
-      {incidents.nodes.map((incident) => (
+      <h2>as deployer</h2>
+      {incidentsAsDeployer.nodes.map((incident) => (
+        <div key={incident.incident_id}>
+          <h3 className="tw-pt-6">Incident {incident.incident_id}</h3>
+          <Link to={`/cite/${incident.incident_id}`}>{incident.title}</Link>
+        </div>
+      ))}
+
+      <h2>as developer</h2>
+      {incidentsAsDeveloper.nodes.map((incident) => (
+        <div key={incident.incident_id}>
+          <h3 className="tw-pt-6">Incident {incident.incident_id}</h3>
+          <Link to={`/cite/${incident.incident_id}`}>{incident.title}</Link>
+        </div>
+      ))}
+
+      <h2>as both</h2>
+      {incidentsAsBoth.nodes.map((incident) => (
         <div key={incident.incident_id}>
           <h3 className="tw-pt-6">Incident {incident.incident_id}</h3>
           <Link to={`/cite/${incident.incident_id}`}>{incident.title}</Link>
@@ -24,8 +40,32 @@ const EntityPage = ({ pageContext, data, ...props }) => {
 };
 
 export const query = graphql`
-  query EntityPageQuery($incidents: [Int]) {
-    incidents: allMongodbAiidprodIncidents(filter: { incident_id: { in: $incidents } }) {
+  query EntityPageQuery(
+    $incidentsAsDeployer: [Int]
+    $incidentsAsDeveloper: [Int]
+    $incidentsAsBoth: [Int]
+  ) {
+    incidentsAsDeployer: allMongodbAiidprodIncidents(
+      filter: { incident_id: { in: $incidentsAsDeployer } }
+    ) {
+      nodes {
+        title
+        incident_id
+      }
+    }
+
+    incidentsAsDeveloper: allMongodbAiidprodIncidents(
+      filter: { incident_id: { in: $incidentsAsDeveloper } }
+    ) {
+      nodes {
+        title
+        incident_id
+      }
+    }
+
+    incidentsAsBoth: allMongodbAiidprodIncidents(
+      filter: { incident_id: { in: $incidentsAsBoth } }
+    ) {
       nodes {
         title
         incident_id
