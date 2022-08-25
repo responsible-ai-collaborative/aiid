a509ddf5ef1e1f94e280d18d62981c75b7728f86
Luna McNulty <l.mcnulty@riseup.net>
2022-08-25 14:08:10 -0400

    Add spatial icon

--- site/gatsby-site/src/components/sidebar/treeNode.js
+++ site/gatsby-site/src/components/sidebar/treeNode.js
@@ -26,6 +26,22 @@ const getIcon = (label) =>
     ),
     blog: <FontAwesomeIcon icon={faNewspaper} className="pointer fa tw-mr-1" fixedWidth />,
     taxonomies: <FontAwesomeIcon icon={faChartPie} className="pointer fa tw-mr-1" fixedWidth />,
+    spatial: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        viewBox="0 0 8.4666665 8.4666669"
+        style={{ width: '1.25em', marginRight: '0.25rem' }}
+        fill="currentColor"
+        stroke="none"
+      >
+        <circle id="c1" cx="1.4249661" cy="3.3814588" r="1.2797676" />
+        <circle id="c2" cx="2.6276002" cy="6.9394135" r="1.2797676" />
+        <circle id="c3" cx="4.7233200" cy="4.6742487" r="1.2797676" />
+        <circle id="c4" cx="6.6127176" cy="6.8885708" r="1.2797676" />
+        <circle id="c5" cx="6.1704326" cy="1.5272532" r="1.2797676" />
+      </svg>
+    ),
   }[label]);
 
 const TreeNode = ({ className = '', setCollapsed, navSetting, item }) => {
