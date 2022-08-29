b605ab7c4bd6a15397b9332c48ae169cccf317d3
Luna McNulty <l.mcnulty@riseup.net>
2022-08-29 13:56:14 -0400

    Fix paths to restore sidebar outline

--- site/gatsby-site/src/components/rightSidebar.js
+++ site/gatsby-site/src/components/rightSidebar.js
@@ -4,73 +4,86 @@ import { StaticQuery, graphql } from 'gatsby';
 import config from '../../config';
 import { Sidebar, ListItem } from './styles/Sidebar';
 
-const SidebarLayout = ({ location }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allMdx {
-          edges {
-            node {
-              fields {
-                slug
+import { useLocalization } from 'gatsby-theme-i18n';
+
+const SidebarLayout = ({ location }) => {
+  const { locale } = useLocalization();
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allMdx {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                tableOfContents
               }
-              tableOfContents
             }
           }
         }
-      }
-    `}
-    render={({ allMdx }) => {
-      let finalNavItems;
+      `}
+      render={({ allMdx }) => {
+        let finalNavItems;
 
-      if (allMdx.edges !== undefined && allMdx.edges.length > 0) {
-        allMdx.edges.map((item) => {
-          let innerItems;
+        if (allMdx.edges !== undefined && allMdx.edges.length > 0) {
+          allMdx.edges.map((item) => {
+            let innerItems;
 
-          if (item !== undefined) {
-            if (
-              item.node.fields.slug === location.pathname ||
-              config.gatsby.pathPrefix + item.node.fields.slug === location.pathname
-            ) {
-              if (item.node.tableOfContents.items) {
-                innerItems = item.node.tableOfContents.items.map((innerItem, index) => {
-                  const itemId = innerItem.title
-                    ? innerItem.title.replace(/\s+/g, '').toLowerCase()
-                    : '#';
+            if (item !== undefined) {
+              const indexName = (
+                location.pathname.replace(`/${locale}`, '') +
+                '/index' +
+                (locale == 'en' ? '' : `.${locale}`)
+              ).replace(/\/+/g, '/');
 
-                  return (
-                    <ListItem key={index} to={`#${itemId}`} level={1}>
-                      {innerItem.title}
-                    </ListItem>
-                  );
-                });
+              if (
+                [item.node.fields.slug, config.gatsby.pathPrefix + item.node.fields.slug].includes(
+                  indexName
+                )
+              ) {
+                if (item.node.tableOfContents.items) {
+                  innerItems = item.node.tableOfContents.items.map((innerItem, index) => {
+                    const itemId = innerItem.title
+                      ? innerItem.title.replace(/\s+/g, '').toLowerCase()
+                      : '#';
+
+                    return (
+                      <ListItem key={index} to={`#${itemId}`} level={1}>
+                        {innerItem.title}
+                      </ListItem>
+                    );
+                  });
+                }
               }
             }
-          }
-          if (innerItems) {
-            finalNavItems = innerItems;
-          }
-        });
-      }
+            if (innerItems) {
+              finalNavItems = innerItems;
+            }
+          });
+        }
 
-      if (finalNavItems && finalNavItems.length) {
-        return (
-          <Sidebar>
-            <ul className={'rightSideBarUL'}>
-              <li className={'rightSideTitle'}>CONTENTS</li>
-              {finalNavItems}
-            </ul>
-          </Sidebar>
-        );
-      } else {
-        return (
-          <Sidebar>
-            <ul></ul>
-          </Sidebar>
-        );
-      }
-    }}
-  />
-);
+        if (finalNavItems && finalNavItems.length) {
+          return (
+            <Sidebar>
+              <ul className={'rightSideBarUL'}>
+                <li className={'rightSideTitle'}>CONTENTS</li>
+                {finalNavItems}
+              </ul>
+            </Sidebar>
+          );
+        } else {
+          return (
+            <Sidebar>
+              <ul></ul>
+            </Sidebar>
+          );
+        }
+      }}
+    />
+  );
+};
 
 export default SidebarLayout;
