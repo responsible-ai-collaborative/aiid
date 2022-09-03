cb2b8ea04bda839b64f6c6f603ff58f603bd530b
Pablo Costa <pablo@botsfactory.io>
2022-09-02 21:26:01 -0300

    Apply Flowbite Spinner to `signup` page

--- site/gatsby-site/src/pages/signup.js
+++ site/gatsby-site/src/pages/signup.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Layout from '../components/Layout';
-import { Form, Spinner } from 'react-bootstrap';
-import { useUserContext } from 'contexts/userContext';
+import { Form } from 'react-bootstrap';
+import { Spinner } from 'flowbite-react';
+import { useUserContext } from '../contexts/userContext';
 import useToastContext, { SEVERITY } from '../hooks/useToast';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import Link from 'components/ui/Link';
+import Link from '../components/ui/Link';
 import { Trans, useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
@@ -55,10 +56,10 @@ const SignUp = (props) => {
   return (
     <Layout {...props} className="bootstrap">
       {loading ? (
-        <>
-          <Spinner animation="border" size="sm" role="status" className="mr-2" />
+        <div className="flex flex-wrap gap-2">
+          <Spinner />
           <Trans>Loading...</Trans>
-        </>
+        </div>
       ) : user && user.isLoggedIn && user.profile.email ? (
         <>
           <p>
@@ -165,10 +166,10 @@ const SignUp = (props) => {
                   }
                   className="w-full"
                 >
-                  {isSubmitting && (
-                    <Spinner animation="border" size="sm" role="status" className="mr-2" />
-                  )}
-                  <Trans ns="login">Sign up</Trans>
+                  {isSubmitting && <Spinner />}
+                  <span className="pl-3">
+                    <Trans ns="login">Sign up</Trans>
+                  </span>
                 </Button>
               </Form>
             )}
@@ -184,9 +185,9 @@ const SignUp = (props) => {
             className={'w-full'}
             disabled={displayFacebookSpinner || displayGoogleSpinner}
           >
-            <div className={'flex justify-center items-center'}>
+            <div className={'flex justify-center items-center gap-2'}>
               {displayFacebookSpinner ? (
-                <Spinner animation="border" size="sm" role="status" aria-hidden="true" />
+                <Spinner />
               ) : (
                 <FontAwesomeIcon
                   icon={faFacebook}
@@ -195,9 +196,7 @@ const SignUp = (props) => {
                   title="Sign up with Facebook"
                 />
               )}
-              <div className={'ml-2'}>
-                <Trans ns="login">Sign up with Facebook</Trans>
-              </div>
+              <Trans ns="login">Sign up with Facebook</Trans>
             </div>
           </Button>
 
@@ -207,9 +206,9 @@ const SignUp = (props) => {
             className={'w-full mt-5'}
             disabled={displayFacebookSpinner || displayGoogleSpinner}
           >
-            <div className={'flex justify-center items-center'}>
+            <div className={'flex justify-center items-center gap-2'}>
               {displayGoogleSpinner ? (
-                <Spinner animation="border" size="sm" role="status" aria-hidden="true" />
+                <Spinner />
               ) : (
                 <FontAwesomeIcon
                   icon={faGoogle}
@@ -218,9 +217,7 @@ const SignUp = (props) => {
                   title="Sign up with Google"
                 />
               )}
-              <div className={'ml-2'}>
-                <Trans ns="login">Sign up with Google</Trans>
-              </div>
+              <Trans ns="login">Sign up with Google</Trans>
             </div>
           </Button>
 
