8d5cc45f44fcca20077fe664fb28c7ddf9026dbb
Pablo Costa <pablo@botsfactory.io>
2022-09-05 20:52:55 -0300

    Remove `tw-` prefix from Button element and add `loading` state

--- site/gatsby-site/src/elements/Button/index.js
+++ site/gatsby-site/src/elements/Button/index.js
@@ -1,20 +1,26 @@
+import { Spinner } from 'flowbite-react';
 import React from 'react';
 
 export default function Button(props) {
-  let classNames = 'tw-btn';
+  let classNames = 'btn';
+
+  if (props.title === 'Flag Report') console.log('--- props.loading', props.loading);
 
   classNames += props.className ? ` ${props.className}` : '';
-  classNames += props.variant ? ` tw-btn-${props.variant}` : '';
-  classNames += props.size ? ` tw-btn-${props.size}` : '';
+  classNames += props.variant ? ` btn-${props.variant}` : '';
+  classNames += props.size ? ` btn-${props.size}` : '';
+  classNames += props.loading ? ` flex gap-2 justify-center` : '';
 
   return (
     <>
       {props.href ? (
         <a {...props} className={`${classNames}`} role="button">
+          {props.loading && <Spinner />}
           {props.children}
         </a>
       ) : (
         <button {...props} className={`${classNames}`}>
+          {props.loading && <Spinner />}
           {props.children}
         </button>
       )}
