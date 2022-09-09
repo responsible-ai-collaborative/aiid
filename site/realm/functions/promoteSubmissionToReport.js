4818bff038f6709fca379301fb5732b8e983eb46
Pablo Costa <pablo@botsfactory.io>
2022-09-09 17:31:53 -0300

    Add `projection` to sort queries on `promoteSubmissionToReport` function

--- site/realm/functions/promoteSubmissionToReport.js
+++ site/realm/functions/promoteSubmissionToReport.js
@@ -1,19 +1,19 @@
 exports = async (input) => {
-  
+
   const submissions = context.services.get('mongodb-atlas').db('aiidprod').collection("submissions");
   const incidents = context.services.get('mongodb-atlas').db('aiidprod').collection("incidents");
   const reports = context.services.get('mongodb-atlas').db('aiidprod').collection("reports");
-  
-  const {_id: undefined, ...submission} = await submissions.findOne({_id: input.submission_id});
-  
-  const parentIncidents = await incidents.find({incident_id: {$in: input.incident_ids }}).toArray();
-  
-  const report_number = (await reports.find({}).sort({report_number: -1}).limit(1).next()).report_number + 1;
+
+  const { _id: undefined, ...submission } = await submissions.findOne({ _id: input.submission_id });
+
+  const parentIncidents = await incidents.find({ incident_id: { $in: input.incident_ids } }).toArray();
+
+  const report_number = (await reports.find({}, { report_number: 1 }).sort({ report_number: -1 }).limit(1).next()).report_number + 1;
 
   if (parentIncidents.length == 0) {
-    
-    const lastIncident = await incidents.find({}).sort({incident_id: -1}).limit(1).next();
-    
+
+    const lastIncident = await incidents.find({}, { incident_id: 1 }).sort({ incident_id: -1 }).limit(1).next();
+
     const newIncident = {
       title: submission.title,
       description: submission.description,
@@ -29,24 +29,24 @@ exports = async (input) => {
       editor_dissimilar_incidents: submission.editor_dissimilar_incidents || [],
     }
     if (submission.embedding) {
-      newIncident.embedding = { 
+      newIncident.embedding = {
         vector: submission.embedding.vector,
-        from_reports: [BSON.Int32(report_number)] 
-      } 
+        from_reports: [BSON.Int32(report_number)]
+      }
     }
-    
-    await incidents.insertOne({...newIncident, incident_id: BSON.Int32(newIncident.incident_id)});
-    
+
+    await incidents.insertOne({ ...newIncident, incident_id: BSON.Int32(newIncident.incident_id) });
+
     parentIncidents.push(newIncident);
 
   } else if (submission.embedding) {
 
     for (const parentIncident of parentIncidents) {
-      
+
       const matchingReports = [];
 
       for (const report_number of parentIncident.reports) {
-        matchingReports.push(await reports.findOne({report_number}));
+        matchingReports.push(await reports.findOne({ report_number }));
       }
 
       const embeddings = matchingReports
@@ -55,17 +55,17 @@ exports = async (input) => {
         .concat([submission.embedding]);
 
       const embedding = {
-        vector: 
+        vector:
           embeddings.map(e => e.vector).reduce(
             (sum, vector) => (
               vector.map(
                 (component, i) => component + sum[i]
               )
-            ), 
+            ),
             Array(embeddings[0].vector.length).fill(0)
           ).map(component => component / embeddings.length),
 
-        from_reports: 
+        from_reports:
           matchingReports
             .map(report => BSON.Int32(report.report_number))
             .concat([BSON.Int32(report_number)])
@@ -73,12 +73,11 @@ exports = async (input) => {
 
       await incidents.updateOne(
         { incident_id: BSON.Int32(parentIncident.incident_id) },
-        { $set : { ...parentIncident, embedding }}
+        { $set: { ...parentIncident, embedding } }
       );
     }
   }
-  
-  
+
   const newReport = {
     report_number,
     ref_number: BSON.Int32(parentIncidents[0].reports.length), // this won't make sense with many to many relationships
@@ -102,17 +101,15 @@ exports = async (input) => {
   if (submission.embedding) {
     newReport.embedding = submission.embedding;
   }
-  
-  await reports.insertOne({...newReport, report_number: BSON.Int32(newReport.report_number)});
+
+  await reports.insertOne({ ...newReport, report_number: BSON.Int32(newReport.report_number) });
 
   const incident_ids = parentIncidents.map(incident => incident.incident_id);
-  const report_numbers = [ newReport.report_number];
-  
-  await context.functions.execute('linkReportsToIncidents', {incident_ids, report_numbers });
-  
-  
-  await submissions.deleteOne({_id: input.submission_id});
-  
-  
-  return incidents.find({incident_id: {$in: incident_ids }}).toArray();
+  const report_numbers = [newReport.report_number];
+
+  await context.functions.execute('linkReportsToIncidents', { incident_ids, report_numbers });
+
+  await submissions.deleteOne({ _id: input.submission_id });
+
+  return incidents.find({ incident_id: { $in: incident_ids } }, { incident_id: 1, reports: 1 }).toArray();
 };
