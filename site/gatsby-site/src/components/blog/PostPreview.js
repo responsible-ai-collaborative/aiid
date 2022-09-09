36ffe49611590297950a74e1cf7ae7eb993a742c
Clara Youdale <clara@botsfactory.io>
2022-09-09 16:02:04 -0300

    Apply same styling as landing page for blog cards

--- site/gatsby-site/src/components/blog/PostPreview.js
+++ site/gatsby-site/src/components/blog/PostPreview.js
@@ -6,51 +6,52 @@ import { Trans } from 'react-i18next';
 
 function PostPreview({ post }) {
   return (
-    <div className="flex flex-col w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <LocalizedLink to={post.frontmatter.slug} className="text-primary-blue max-w-full">
-        <Img
-          alt="post-image"
-          className="img-fluid rounded-start h-full  max-w-full max-h-240 rounded-t-lg"
-          imgStyle={{ transition: '0.5s all ease-in-out' }}
-          imgClassName="hover:scale-110 object-cover"
-          image={post.frontmatter.image.childImageSharp.gatsbyImageData}
-        />
-      </LocalizedLink>
-      <div className="p-5 flex flex-col justify-between flex-1">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {post.fields.title}
-        </h5>
-        <div className="font-medium">{format(new Date(post.frontmatter.date), 'MMM d, yyyy')}</div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {post.excerpt}...{' '}
-          <LocalizedLink to={post.frontmatter.slug}>
-            <Trans>(Read More)</Trans>
-          </LocalizedLink>
-        </p>
-        <div className="mt-4 mb-4">
-          <small className="text-muted-gray text-sm">By {post.frontmatter.author}</small>
-        </div>
-        <LocalizedLink
-          to={post.frontmatter.slug}
-          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-start"
-        >
-          Read more
-          <svg
-            aria-hidden="true"
-            className="ml-2 -mr-1 w-4 h-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+    <>
+      <div className="flex flex-col w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <LocalizedLink to={post.frontmatter.slug} className="text-primary-blue max-w-full">
+          <Img
+            alt="post-image"
+            className="img-fluid h-full w-full max-w-full max-h-240 rounded-t-lg"
+            imgStyle={{ transition: '0.5s all ease-in-out' }}
+            imgClassName="hover:scale-110 object-cover"
+            image={post.frontmatter.image.childImageSharp.gatsbyImageData}
+          />
         </LocalizedLink>
+        <div className="p-5">
+          <LocalizedLink to={post.frontmatter.slug} className="">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white relative block hover:text-primary-blue">
+              {post.fields.title}
+            </h5>
+          </LocalizedLink>
+          <p className="text-muted-gray text-sm">By {post.frontmatter.author}</p>
+          <p className="text-sm text-muted-gray">
+            {format(new Date(post.frontmatter.date), 'MMM d, yyyy')}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.excerpt}... </p>
+          <div className="flex justify-end">
+            <a
+              href={post.frontmatter.slug}
+              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <Trans>Read more</Trans>
+              <svg
+                aria-hidden="true"
+                className="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
