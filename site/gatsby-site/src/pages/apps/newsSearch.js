0a35ab70147593999250ce192a324e2fb496a7af
Luna McNulty <l.mcnulty@riseup.net>
2022-09-12 14:04:00 -0400

    Allow dismissing candidates

--- site/gatsby-site/src/pages/apps/newsSearch.js
+++ site/gatsby-site/src/pages/apps/newsSearch.js
@@ -1,12 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from 'components/Layout';
 import { StyledHeading } from 'components/styles/Docs';
 import { Trans, useTranslation } from 'react-i18next';
 import { LocalizedLink } from 'gatsby-theme-i18n';
 import AiidHelmet from 'components/AiidHelmet';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import { Card, Button, Badge } from 'flowbite-react';
 import { format, parse } from 'date-fns';
+import { faPlusCircle, faTrash, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const CandidateCard = ({
+  newsArticle,
+  setDismissedArticles,
+  updateCandidate,
+  dismissed = false,
+}) => (
+  <Card style={{ justifyContent: 'flex-start' }} className="gap-0" key={newsArticle.incident_id}>
+    <div>
+      <a href={newsArticle.url}>
+        <h3 className="text-xl mt-0 mb-0">{newsArticle.title.replace(/\s(-|\|).*/g, '')}</h3>
+      </a>
+      <div className="text-lg text-gray-600 mb-3 mt-1">
+        {format(parse(newsArticle.date_published, 'yyyy-MM-dd', new Date()), 'MMM d')} •{' '}
+        {new URL(newsArticle.url).host.replace('www.', '')}
+      </div>
+      <div className="flex flex-wrap">
+        <span className="mb-1 mr-1">
+          {newsArticle.similarity < 0.997 ? (
+            <Badge color="warning" title={'cosine similarity: ' + newsArticle.similarity}>
+              Weak match
+            </Badge>
+          ) : newsArticle.similarity < 0.9975 ? (
+            <Badge color="success" title={'cosine similarity: ' + newsArticle.similarity}>
+              Match
+            </Badge>
+          ) : (
+            <Badge color="success" title={'cosine similarity: ' + newsArticle.similarity}>
+              Strong match
+            </Badge>
+          )}{' '}
+        </span>
+        {newsArticle.matching_keywords.map((keyword) => (
+          <span className="inline-block mr-1 mb-1" key={keyword}>
+            <Badge>{keyword}</Badge>
+          </span>
+        ))}
+      </div>
+    </div>
+    <div className="mt-auto flex">
+      {dismissed ? (
+        <Button
+          color="light"
+          onClick={() => {
+            setDismissedArticles((dismissedArticles) => {
+              const updatedValue = { ...dismissedArticles };
+
+              updatedValue[newsArticle.url] = false;
+              return updatedValue;
+            });
+            updateCandidate({
+              variables: {
+                query: { url: newsArticle.url },
+                set: { dismissed: false },
+              },
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowUp} className="pointer fa mr-1" fixedWidth />
+          Restore
+        </Button>
+      ) : (
+        <Button
+          color="light"
+          onClick={() => {
+            setDismissedArticles((dismissedArticles) => {
+              const updatedValue = { ...dismissedArticles };
+
+              updatedValue[newsArticle.url] = true;
+              return updatedValue;
+            });
+            updateCandidate({
+              variables: {
+                query: { url: newsArticle.url },
+                set: { dismissed: true },
+              },
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faTrash} className="pointer fa mr-1" fixedWidth />
+          Dismiss
+        </Button>
+      )}
+      <LocalizedLink
+        to={
+          '/apps/submit?' +
+          ['url', 'title', 'text']
+            .map((e) => `${e}=${encodeURIComponent(newsArticle[e])}`)
+            .join('&')
+        }
+        target="_blank"
+        className="inline ml-1"
+      >
+        <Button color="light">
+          <FontAwesomeIcon icon={faPlusCircle} className="pointer fa mr-1" fixedWidth />
+          Submit
+        </Button>
+      </LocalizedLink>
+    </div>
+  </Card>
+);
 
 const NewsSearchPage = (props) => {
   const { t } = useTranslation(['submit']);
@@ -20,11 +123,61 @@ const NewsSearchPage = (props) => {
         matching_keywords
         text
         date_published
+        dismissed
       }
     }
   `);
 
   const newsArticles = loading ? [] : [...newsArticlesData?.candidates];
+
+  const newsArticleUrls = newsArticles.map((newsArticle) => newsArticle.url);
+
+  const { data: submissionsData } = useQuery(
+    gql`
+      query ExistingSubmissions($query: SubmissionQueryInput!) {
+        submissions(query: $query) {
+          url
+        }
+      }
+    `,
+    { variables: { query: { url_in: newsArticleUrls } } }
+  );
+
+  const { data: reportsData } = useQuery(
+    gql`
+      query ExistingReports($query: ReportQueryInput!) {
+        reports(query: $query) {
+          report_number
+          url
+        }
+      }
+    `,
+    { variables: { query: { url_in: newsArticleUrls } } }
+  );
+
+  const existingSubmissions = (submissionsData?.submissions || []).map(
+    (submission) => submission.url
+  );
+
+  const existingReports = (reportsData?.reports || []).map((report) => report.url);
+
+  const [updateCandidate] = useMutation(gql`
+    mutation UpdateCandidate($query: CandidateQueryInput!, $set: CandidateUpdateInput!) {
+      updateOneCandidate(query: $query, set: $set) {
+        url
+      }
+    }
+  `);
+
+  const [dismissedArticles, setDismissedArticles] = useState({});
+
+  console.log(dismissedArticles);
+
+  const displayedDismissed = newsArticles.filter(
+    (newsArticle) =>
+      (newsArticle.dismissed && dismissedArticles[newsArticle.url] === undefined) ||
+      dismissedArticles[newsArticle.url] === true
+  );
 
   return (
     <Layout {...props}>
@@ -45,67 +198,42 @@ const NewsSearchPage = (props) => {
         {newsArticles
           .filter(
             (newsArticle) =>
-              !newsArticle.date_published ||
-              (new Date().getTime() -
-                parse(newsArticle.date_published, 'yyyy-MM-dd', new Date()).getTime()) /
-                (1000 * 60 * 60 * 24) <
-                30
+              ((dismissedArticles[newsArticle.url] == undefined && !newsArticle.dismissed) ||
+                dismissedArticles[newsArticle.url] === false) &&
+              (!newsArticle.date_published ||
+                (new Date().getTime() -
+                  parse(newsArticle.date_published, 'yyyy-MM-dd', new Date()).getTime()) /
+                  (1000 * 60 * 60 * 24) <
+                  30) &&
+              !existingSubmissions.includes(newsArticle.url) &&
+              !existingReports.includes(newsArticle.url)
           )
           .sort((b, a) => a.similarity - b.similarity)
           .map((newsArticle) => (
-            <Card
-              style={{ justifyContent: 'flex-start' }}
-              className="gap-0"
-              key={newsArticle.incident_id}
-            >
-              <div>
-                <a href={newsArticle.url}>
-                  <h3 className="text-xl mt-0 mb-0">{newsArticle.title}</h3>
-                </a>
-                <div className="text-lg text-gray-600 my-2">
-                  {format(
-                    parse(newsArticle.date_published, 'yyyy-MM-dd', new Date()),
-                    'MMM d, yyyy'
-                  )}
-                </div>
-                <div className="flex">
-                  {newsArticle.similarity < 0.997 ? (
-                    <Badge color="warning" title={'cosine similarity: ' + newsArticle.similarity}>
-                      Weak match
-                    </Badge>
-                  ) : newsArticle.similarity < 0.9975 ? (
-                    <Badge color="success" title={'cosine similarity: ' + newsArticle.similarity}>
-                      Match
-                    </Badge>
-                  ) : (
-                    <Badge color="success" title={'cosine similarity: ' + newsArticle.similarity}>
-                      Strong match
-                    </Badge>
-                  )}{' '}
-                  {newsArticle.matching_keywords.map((keyword) => (
-                    <span className="inline-block ml-1 mr-1" key={keyword}>
-                      <Badge>{keyword}</Badge>
-                    </span>
-                  ))}
-                </div>
-              </div>
-              <div className="mt-auto flex justify-end">
-                <LocalizedLink
-                  to={
-                    '/apps/submit?' +
-                    ['url', 'title', 'text']
-                      .map((e) => `${e}=${encodeURIComponent(newsArticle[e])}`)
-                      .join('&')
-                  }
-                  target="_blank"
-                  className="inline"
-                >
-                  <Button>Submit</Button>
-                </LocalizedLink>
-              </div>
-            </Card>
+            <CandidateCard
+              newsArticle={newsArticle}
+              setDismissedArticles={setDismissedArticles}
+              updateCandidate={updateCandidate}
+              key={newsArticle.url}
+            />
           ))}
       </div>
+      {displayedDismissed.length > 0 && (
+        <details>
+          <summary>Dismissed News Reports</summary>
+          <div className="tw-card-set mt-2">
+            {displayedDismissed.map((newsArticle) => (
+              <CandidateCard
+                newsArticle={newsArticle}
+                updateCandidate={updateCandidate}
+                setDismissedArticles={setDismissedArticles}
+                dismissed={true}
+                key={newsArticle.url}
+              />
+            ))}
+          </div>
+        </details>
+      )}
     </Layout>
   );
 };
