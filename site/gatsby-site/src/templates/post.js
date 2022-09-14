02a52c84745491e1643aeed81274949be3e98453
Clara Youdale <clara@botsfactory.io>
2022-09-14 13:13:24 -0300

    Remove max width for post wrapper

--- site/gatsby-site/src/templates/post.js
+++ site/gatsby-site/src/templates/post.js
@@ -51,7 +51,7 @@ export default function Post(props) {
           page="post"
         ></SocialShareButtons>
       </div>
-      <StyledMainWrapper className="max-w-full">
+      <StyledMainWrapper>
         <MDXProvider components={MdxComponents}>
           <MDXRenderer>{mdx.body}</MDXRenderer>
         </MDXProvider>
