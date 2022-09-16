7b34c56c7d6d28d83d95b1a2e792580b41726286
Clara Youdale <clara@botsfactory.io>
2022-09-16 17:00:07 -0300

    Add missing french config

--- site/gatsby-site/src/templates/cite.js
+++ site/gatsby-site/src/templates/cite.js
@@ -342,6 +342,7 @@ export const query = graphql`
     $incident_id: Int
     $report_numbers: [Int]
     $translate_es: Boolean!
+    $translate_fr: Boolean!
     $translate_en: Boolean!
   ) {
     mongodbAiidprodResources(
@@ -437,6 +438,14 @@ export const query = graphql`
     }
     allMongodbTranslationsReportsEs(filter: { report_number: { in: $report_numbers } })
       @include(if: $translate_es) {
+      nodes {
+        title
+        text
+        report_number
+      }
+    }
+    allMongodbTranslationsReportsFr(filter: { report_number: { in: $report_numbers } })
+      @include(if: $translate_fr) {
       nodes {
         title
         text
