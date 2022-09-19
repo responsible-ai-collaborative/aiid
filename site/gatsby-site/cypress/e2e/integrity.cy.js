4b1525c91877c2c872fc124e0cff47af9f10094b
Cesar Varela <mail@cesarvarela.com>
2022-09-19 20:18:38 -0300

    Check that linked reports have incident_report_flag to true

--- site/gatsby-site/cypress/e2e/integrity.cy.js
+++ site/gatsby-site/cypress/e2e/integrity.cy.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client';
 
+const isLinked = (reportNumber, incidents) => {
+  for (const incident of incidents) {
+    for (const linkedReport of incident.reports) {
+      if (linkedReport.report_number == reportNumber) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
 describe('Integrity', () => {
   it('Should have repeated report numbers', () => {
     cy.query({
@@ -47,27 +59,43 @@ describe('Integrity', () => {
         }
       `,
     }).then(({ data: { incidents, reports } }) => {
-      const isLinked = (reportNumber) => {
-        for (const incident of incidents) {
-          for (const linkedReport of incident.reports) {
-            if (linkedReport.report_number == reportNumber) {
-              return true;
-            }
-          }
-        }
-
-        return false;
-      };
-
       const orphanReports = [];
 
       for (const reportNumber of reports.map((r) => r.report_number)) {
-        if (!isLinked(reportNumber)) {
+        if (!isLinked(reportNumber, incidents)) {
           orphanReports.push(reportNumber);
         }
       }
 
       expect(orphanReports.length, `Orphan reports: [${orphanReports.toString()}]`).eq(0);
+    });
+  });
+
+  it(`is_incident_report should be true for reports assigned to incidents and vice versa`, () => {
+    cy.query({
+      query: gql`
+        query {
+          incidents(limit: 9999) {
+            reports {
+              report_number
+            }
+          }
+          reports(limit: 9999) {
+            report_number
+            is_incident_report
+          }
+        }
+      `,
+    }).then(({ data: { incidents, reports } }) => {
+      const invalidReports = [];
+
+      for (const { report_number, is_incident_report } of reports) {
+        if (isLinked(report_number, incidents) !== is_incident_report) {
+          invalidReports.push(report_number);
+        }
+      }
+
+      expect(invalidReports.length, `Invalid reports: [${invalidReports.toString()}]`).eq(0);
     });
   });
 });
