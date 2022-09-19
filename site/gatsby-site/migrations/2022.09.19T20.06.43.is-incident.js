379aa6e3ca99a59b48d0a81978c8eee520725ba0
Cesar Varela <mail@cesarvarela.com>
2022-09-19 17:29:49 -0300

    Migration to add new is_incident_report field

---
+++ site/gatsby-site/migrations/2022.09.19T20.06.43.is-incident.js
@@ -0,0 +1,14 @@
+const config = require('../config');
+
+/**
+ *
+ * @param {{context: {client: import('mongodb').MongoClient}}} context
+ */
+
+exports.up = async ({ context: { client } }) => {
+  await client.connect();
+
+  const reportsCollection = client.db(config.realm.production_db.db_name).collection('reports');
+
+  await reportsCollection.updateMany({}, { $set: { is_incident_report: true } });
+};
