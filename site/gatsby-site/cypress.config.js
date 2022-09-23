35fc50690e10775b85c1d234d34ed217465d8ea7
Cesar Varela <mail@cesarvarela.com>
2022-09-23 16:39:04 -0300

    Disable cypress videos

--- site/gatsby-site/cypress.config.js
+++ site/gatsby-site/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  video: true,
+  video: false,
   videoUploadOnPasses: false,
   chromeWebSecurity: false,
   defaultCommandTimeout: 15000,
