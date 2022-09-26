fe6fd8c9862e2a2c8d79a9a69e72f336068b8329
Clara Youdale <clara@botsfactory.io>
2022-09-26 15:46:01 -0300

    Update LatestReports.js

--- site/gatsby-site/src/components/landing/LatestReports.js
+++ site/gatsby-site/src/components/landing/LatestReports.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LatestIncidentReport from 'components/landing/LatestIncidentReport';
 
 export default function LatestReports({ latestReport }) {
-  return ( 
+  return (
     <>
       <div className="flex flex-col items-center">
         <LatestIncidentReport report={latestReport} />
