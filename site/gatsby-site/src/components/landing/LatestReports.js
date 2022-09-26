69ca4452bf8f4e09d6f78f82fcbda5266bb5af8a
Clara Youdale <clara@botsfactory.io>
2022-09-26 15:42:55 -0300

    Fix dependency

--- site/gatsby-site/src/components/landing/LatestReports.js
+++ site/gatsby-site/src/components/landing/LatestReports.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import LatestIncidentReport from 'components/landing/LatestIncidentReport';
-import { Trans } from 'react-i18next';
 
 export default function LatestReports({ latestReport }) {
-  return (
+  return ( 
     <>
       <div className="flex flex-col items-center">
         <LatestIncidentReport report={latestReport} />
