482ccada1b820e98f471961c85506c1f375adcf1
Luna McNulty <l.mcnulty@riseup.net>
2022-09-26 15:36:40 -0400

    Update landing page carousel style

--- site/gatsby-site/src/components/landing/RandomIncidentsCarousel.js
+++ site/gatsby-site/src/components/landing/RandomIncidentsCarousel.js
@@ -1,80 +1,98 @@
-import React from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
 import md5 from 'md5';
 import { Image } from 'utils/cloudinary';
 import { fill } from '@cloudinary/base/actions/resize';
-import { Carousel } from 'flowbite-react';
+import { Carousel, Spinner } from 'flowbite-react';
+import { gql, useQuery } from '@apollo/client';
 
 const RandomIncidentsCarousel = () => {
-  const generateRandomIncidents = () => {
-    const array = [];
+  const [randomIncidentIds] = useState(
+    Array(5)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 300))
+  );
 
-    for (let index = 0; index < 5; index++) {
-      array.push(Math.floor(Math.random() * 50 + 1));
-    }
-
-    return array;
-  };
-
-  const randomArray = generateRandomIncidents();
-
-  return (
-    <StaticQuery
-      query={graphql`
-        query RandomIncidentsCarousel {
-          allMongodbAiidprodIncidents {
-            nodes {
-              incident_id
-              reports
-              title
-            }
-          }
-
-          allMongodbAiidprodReports(limit: 50, sort: { order: ASC, fields: id }) {
-            nodes {
-              id
-              report_number
-              image_url
-            }
+  const { loading: incidentsLoading, data: incidentsData } = useQuery(
+    gql`
+      query CarouselIncidents($query: IncidentQueryInput!) {
+        incidents(query: $query) {
+          incident_id
+          title
+          reports {
+            report_number
+            title
+            cloudinary_id
+            image_url
           }
         }
-      `}
-      render={({
-        allMongodbAiidprodReports: { nodes: reports },
-        allMongodbAiidprodIncidents: { nodes: incidents },
-      }) => {
-        const randomIncidents = reports
-          .filter(
-            (node, index) => randomArray.includes(index) && node.image_url !== 'placeholder.svg'
-          )
-          .map((report) => ({
-            ...report,
-            ...incidents.find((incident) => incident.reports.includes(report.report_number)),
-          }));
+      }
+    `,
+    { variables: { query: { incident_id_in: randomIncidentIds } } }
+  );
 
-        return (
-          <div className="flex-1 flex justify-center items-center">
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 w-full">
-              <Carousel slideInterval={6000} slide={false}>
-                {randomIncidents.map(({ id, incident_id, title, image_url, cloudinary_id }) => (
-                  <Link to={`/cite/${incident_id}`} key={id} className="h-full">
-                    <h5 className="text-sm sm:text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-                      {title}
-                    </h5>
-                    <Image
-                      publicID={cloudinary_id ? cloudinary_id : `legacy/${md5(image_url)}`}
-                      alt={title}
-                      transformation={fill().height(480)}
-                      plugins={[]}
-                    />
-                  </Link>
-                ))}
-              </Carousel>
-            </div>
+  const randomIncidents =
+    (incidentsData?.incidents &&
+      incidentsData?.incidents.reduce(
+        (reports, incident) =>
+          reports.concat([
+            {
+              ...incident.reports[0],
+              incident_id: incident.incident_id,
+              title: incident.title || reports[0].title,
+            },
+          ]),
+        []
+      )) ||
+    [];
+
+  return (
+    <div className="flex-1 flex justify-center items-center">
+      <div className="h-full w-full relative">
+        <div className="absolute h-3 inset-x-0 top-0 bg-white filler z-50 block w-50">
+          {/* The carousel insists on using rounded corners, *
+           * so we have to cover them up on top.            */}
+        </div>
+        {incidentsLoading && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Spinner size="xl" />
           </div>
-        );
-      }}
-    />
+        )}
+        <Carousel slideInterval={6000} slide={false}>
+          {randomIncidents.map(({ incident_id, title, image_url, cloudinary_id }) => (
+            <Link to={`/cite/${incident_id}`} key={incident_id} className="block h-full relative">
+              <Image
+                publicID={cloudinary_id ? cloudinary_id : `legacy/${md5(image_url)}`}
+                alt={title}
+                transformation={fill().height(800).width(1000)}
+                plugins={[]}
+                className="w-full h-full"
+              />
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h5
+                  className="
+                  text-sm sm:text-lg 
+                  font-bold 
+                  tracking-tight 
+                  text-gray-900 dark:text-white 
+                  w-1/2
+                  backdrop-blur-md
+                  p-6
+                  rounded-md
+                  bg-white/50
+                  top-0
+                  inset-x-0
+                  shadow-lg
+                "
+                >
+                  {title}
+                </h5>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
+      </div>
+    </div>
   );
 };
 
