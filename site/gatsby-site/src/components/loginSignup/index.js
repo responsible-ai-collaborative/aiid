eded6c3b16301aacded8ac30ead595df422281bb
Clara Youdale <clara@botsfactory.io>
2022-09-27 19:30:10 -0300

    Fix infinite Login Loading

--- site/gatsby-site/src/components/loginSignup/index.js
+++ site/gatsby-site/src/components/loginSignup/index.js
@@ -2,6 +2,9 @@ import { useUserContext } from 'contexts/userContext';
 import { Button } from 'flowbite-react';
 import React from 'react';
 import { Trans } from 'react-i18next';
+import Link from 'components/ui/Link';
+import useLocalizePath from '../../components/i18n/useLocalizePath';
+import { useLocalization } from 'gatsby-theme-i18n';
 
 const LoginSignup = ({ className = '', logoutClassName = '', loginClassName = '' }) => {
   const { user } = useUserContext();
@@ -19,26 +22,38 @@ const LoginSignup = ({ className = '', logoutClassName = '', loginClassName = ''
   );
 };
 
-const Login = ({ className = '' }) => (
-  <>
-    <a href="/login" className={`${className}`}>
-      <Trans ns="login">Login</Trans>
-    </a>
-    <span className="mx-2 text-gray-300">
-      <Trans>or</Trans>
-    </span>
-    <Button color={'gray'} href="/signup" size={'sm'}>
-      <Trans ns="login">Sign up</Trans>
-    </Button>
-  </>
-);
+const Login = ({ className = '' }) => {
+  const localizePath = useLocalizePath();
 
-const Logout = ({ className = '', user }) => (
-  <div className="flex flex-col text-xs w-full justify-center items-center">
-    <a href="/login" className={`${className}`}>
-      <Trans ns="login">{user?.profile?.email}</Trans>
-    </a>
-  </div>
-);
+  const { locale } = useLocalization();
+
+  return (
+    <>
+      <Link to={localizePath({ path: '/login', language: locale })} className={`${className}`}>
+        <Trans ns="login">Login</Trans>
+      </Link>
+      <span className="mx-2 text-gray-300">
+        <Trans>or</Trans>
+      </span>
+      <Button color={'gray'} href="/signup" size={'sm'}>
+        <Trans ns="login">Sign up</Trans>
+      </Button>
+    </>
+  );
+};
+
+const Logout = ({ className = '', user }) => {
+  const localizePath = useLocalizePath();
+
+  const { locale } = useLocalization();
+
+  return (
+    <div className="flex flex-col text-xs w-full justify-center items-center">
+      <Link to={localizePath({ path: '/login', language: locale })} className={`${className}`}>
+        <Trans ns="login">{user?.profile?.email}</Trans>
+      </Link>
+    </div>
+  );
+};
 
 export default LoginSignup;
