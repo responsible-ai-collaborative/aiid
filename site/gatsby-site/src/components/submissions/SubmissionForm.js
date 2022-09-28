4dd0c5fc24ab04d831a0df0a32967a0df8388aff
Pablo Costa <pablo@botsfactory.io>
2022-09-27 21:55:02 -0300

    Allow empty `developers`, `deployers` and `harmed_parties` on new submissions

--- site/gatsby-site/src/components/submissions/SubmissionForm.js
+++ site/gatsby-site/src/components/submissions/SubmissionForm.js
@@ -56,33 +56,30 @@ export const schema = yup.object().shape({
       is: (_id) => _id !== undefined,
       then: yup.string().required('*Incident Description required'),
     }),
-  developers: yup
-    .string()
-    .when('incident_id', {
-      is: (incident_id) => incident_id == '' || incident_id === undefined,
-      then: yup.string()
-        .min(3, 'Alleged Developer must have at least 3 characters')
-        .max(200, "Alleged Developers can't be longer than 200 characters")
-        .required('*Developer is required'),
-    }),
-  deployers: yup
-    .string()
-    .when('incident_id', {
-      is: (incident_id) => incident_id == '' || incident_id === undefined,
-      then: yup.string()
-        .min(3, 'Alleged Deployers must have at least 3 characters')
-        .max(200, "Alleged Deployers can't be longer than 200 characters")
-        .required('*Deployer is required'),
-    }),
-  harmed_parties: yup
-    .string()
-    .when('incident_id', {
-      is: (incident_id) => incident_id == '' || incident_id === undefined,
-      then: yup.string()
-        .min(3, 'Harmed Parties must have at least 3 characters')
-        .max(200, "Harmed Parties can't be longer than 200 characters")
-        .required('*Harmed Parties is required'),
-    }),
+  developers: yup.string().when(['_id', 'incident_id'], {
+    is: (_id, incident_id) => _id !== undefined && (incident_id == '' || incident_id === undefined),
+    then: yup
+      .string()
+      .min(3, 'Alleged Developer must have at least 3 characters')
+      .max(200, "Alleged Developers can't be longer than 200 characters")
+      .required('*Developer is required'),
+  }),
+  deployers: yup.string().when(['_id', 'incident_id'], {
+    is: (_id, incident_id) => _id !== undefined && (incident_id == '' || incident_id === undefined),
+    then: yup
+      .string()
+      .min(3, 'Alleged Deployers must have at least 3 characters')
+      .max(200, "Alleged Deployers can't be longer than 200 characters")
+      .required('*Deployer is required'),
+  }),
+  harmed_parties: yup.string().when(['_id', 'incident_id'], {
+    is: (_id, incident_id) => _id !== undefined && (incident_id == '' || incident_id === undefined),
+    then: yup
+      .string()
+      .min(3, 'Harmed Parties must have at least 3 characters')
+      .max(200, "Harmed Parties can't be longer than 200 characters")
+      .required('*Harmed Parties is required'),
+  }),
   authors: yup
     .string()
     .min(3, '*Authors must have at least 3 characters')
@@ -219,9 +216,9 @@ const SubmissionForm = () => {
         const message =
           e.message == 'Parser error'
             ? t(
-              `Error fetching news. Scraping was blocked by {{newsUrl}}. Please enter the text manually.`,
-              { newsUrl }
-            )
+                `Error fetching news. Scraping was blocked by {{newsUrl}}. Please enter the text manually.`,
+                { newsUrl }
+              )
             : t(`Error reaching news info endpoint, please try again in a few seconds.`);
 
         addToast({
