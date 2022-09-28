6d38dec0ddb66c6f841ca2c4f76534c4578c86d0
Pablo Costa <pablo@botsfactory.io>
2022-09-28 18:00:10 -0300

    Add `/account` page

---
+++ site/gatsby-site/src/pages/account.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Spinner } from 'flowbite-react';
+import { useUserContext } from '../contexts/userContext';
+import { Trans, useTranslation } from 'react-i18next';
+import Layout from 'components/Layout';
+import Link from 'components/ui/Link';
+import AiidHelmet from 'components/AiidHelmet';
+import { StyledHeading } from 'components/styles/Docs';
+
+const Account = (props) => {
+  const { user, loading } = useUserContext();
+
+  const { t } = useTranslation(['account']);
+
+  return (
+    <Layout {...props}>
+      <AiidHelmet>
+        <title>{t('Account Details')}</title>
+      </AiidHelmet>
+      <div className={'titleWrapper'}>
+        <StyledHeading>
+          <Trans ns="account">Account Details</Trans>
+        </StyledHeading>
+      </div>
+      {loading ? (
+        <div className="flex flex-wrap gap-2">
+          <Spinner />
+          <Trans>Loading...</Trans>
+        </div>
+      ) : user && user.isLoggedIn && user.profile.email ? (
+        <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <p>
+            <Trans>Email address</Trans>
+            {': '}
+            {user.profile.email}
+          </p>
+          <Link to="/logout">
+            <Trans ns="login">Log out</Trans>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <Link to="/login">
+            <Trans ns="login">Login</Trans>
+          </Link>
+        </>
+      )}
+    </Layout>
+  );
+};
+
+export default Account;
