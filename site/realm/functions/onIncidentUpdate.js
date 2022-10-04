213e466043c9acffa3b6f495c73329752096c687
Pablo Costa <pablo@botsfactory.io>
2022-10-03 21:07:32 -0300

    Move send notifications code to `onIncidentUpdate` function

--- site/realm/functions/onIncidentUpdate.js
+++ site/realm/functions/onIncidentUpdate.js
@@ -1,43 +1,71 @@
-exports = function (changeEvent) {
-    /*
-      A Database Trigger will always call a function with a changeEvent.
-      Documentation on ChangeEvents: https://docs.mongodb.com/manual/reference/change-events/
-  
-      Access the _id of the changed document:
-      const docId = changeEvent.documentKey._id;
-  
-      Access the latest version of the changed document
-      (with Full Document enabled for Insert, Update, and Replace operations):
-      const fullDocument = changeEvent.fullDocument;
-  
-      const updateDescription = changeEvent.updateDescription;
-  
-      See which fields were changed (if any):
-      if (updateDescription) {
-        const updatedFields = updateDescription.updatedFields; // A document containing updated fields
-      }
-  
-      See which fields were removed (if any):
-      if (updateDescription) {
-        const removedFields = updateDescription.removedFields; // An array of removed fields
-      }
-  
-      Functions run by Triggers are run as System users and have full access to Services, Functions, and MongoDB Data.
-  
-      Access a mongodb service:
-      const collection = context.services.get("mongodb-atlas").db("aiidprod").collection("incidents");
-      const doc = collection.findOne({ name: "mongodb" });
-  
-      Note: In Atlas Triggers, the service name is defaulted to the cluster name.
-  
-      Call other named functions if they are defined in your application:
-      const result = context.functions.execute("function_name", arg1, arg2);
-  
-      Access the default http client and execute a GET request:
-      const response = context.http.get({ url: <URL> })
-  
-      Learn more about http client here: https://www.mongodb.com/docs/atlas/app-services/functions/context/#context-http
-    */
+exports = async function (changeEvent) {
 
-    return changeEvent;
+  // Destructure out fields from the change stream event object
+  const { updateDescription, fullDocument, fullDocumentBeforeChange } = changeEvent;
+
+  // Check if the "reports" field was updated
+  const updatedFields = Object.keys(updateDescription.updatedFields);
+
+  const isReportListChanged = updatedFields.some(field => field.match(/reports/));
+
+  if (isReportListChanged) {
+    const { reports: newReports } = fullDocument;
+    const { reports: oldReports } = fullDocumentBeforeChange;
+
+    // If a new report is added, send the email notification to all subscribers
+    if (newReports.length > oldReports.length) {
+
+      const newReportNumber = newReports.filter(report => !oldReports.includes(report))[0];
+
+      const reportsCollection = context.services.get('mongodb-atlas').db('aiidprod').collection("reports");
+      const newReport = await reportsCollection.findOne({ report_number: newReportNumber })
+
+      const incidentId = fullDocument.incident_id;
+      const subscriptionsCollection = context.services.get('mongodb-atlas').db('customData').collection("subscriptions");
+      const subscriptions = await subscriptionsCollection.find({ type: 'incident', incident_id: incidentId }).toArray();
+
+      // Process subscriptions to incident updates
+      if (subscriptions.length > 0) {
+
+        const userIds = subscriptions.map((subscription) => subscription.userId);
+
+        const recipients = [];
+
+        for (const userId of userIds) {
+          const userResponse = await context.functions.execute('getUser', { userId });
+
+          if (userResponse.email) {
+            recipients.push({
+              email: userResponse.email,
+              userId,
+            });
+          }
+        }
+
+        //Send email notification
+        const sendEmailParams = {
+          recipients,
+          subject: 'Incident {{incidentId}} was updated',
+          dynamicData: {
+            incidentId: `${incidentId}`,
+            incidentTitle: fullDocument.title,
+            incidentUrl: `https://incidentdatabase.ai/cite/${incidentId}`,
+            reportUrl: `https://incidentdatabase.ai/cite/${incidentId}#r${newReportNumber}`,
+            reportTitle: newReport.title,
+            reportAuthor: newReport.authors[0],
+          },
+          templateId: 'd-6cebfe690b83416387ec75b21f99108a', // SendGrid Template name: "Incident updated"
+        };
+        const sendEmailresult = await context.functions.execute('sendEmail', sendEmailParams);
+
+        console.log('sendEmailParams', JSON.stringify(sendEmailParams));
+        console.log(JSON.stringify(sendEmailresult));
+        return sendEmailresult;
+      }
+    }
+    else { //if a report was deleted
+      const deletedReportNumber = oldReports.filter(report => !newReports.includes(report))[0];
+      console.log('Report deleted:', deletedReportNumber);
+    }
+  }
 };
