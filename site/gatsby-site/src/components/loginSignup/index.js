30d1150ad8dc618af68373f2e5bb394975edde50
Clara Youdale <clara@botsfactory.io>
2022-10-05 12:10:39 -0300

    Remove spinner and use subscribe instead of login/signup

--- site/gatsby-site/src/components/loginSignup/index.js
+++ site/gatsby-site/src/components/loginSignup/index.js
@@ -1,72 +1,38 @@
 import { useUserContext } from 'contexts/userContext';
-import { Button, Spinner } from 'flowbite-react';
+import { Button } from 'flowbite-react';
 import React from 'react';
 import { Trans } from 'react-i18next';
-import Link from 'components/ui/Link';
-import useLocalizePath from '../../components/i18n/useLocalizePath';
-import { useLocalization } from 'gatsby-theme-i18n';
 
-const LoginSignup = ({ className = '', logoutClassName = '', loginClassName = '' }) => {
+const LoginSignup = ({ className = '' }) => {
   const { user, loading } = useUserContext();
 
-  if (loading)
-    return (
-      <span className="hidden md:flex">
-        <Spinner />
-      </span>
-    );
+  if (loading) return <span></span>;
 
   return (
     <>
       <div className={`flex items-center ${className}`}>
-        {user && user.isLoggedIn && user.profile.email ? (
-          <Logout className={logoutClassName} user={user} />
-        ) : (
-          <Login className={loginClassName} />
-        )}
+        {user && user.isLoggedIn && user.profile.email ? <Subscriptions /> : <Subscribe />}
       </div>
     </>
   );
 };
 
-const Login = ({ className = '' }) => {
-  const localizePath = useLocalizePath();
-
-  const { locale } = useLocalization();
-
+const Subscribe = () => {
   return (
     <>
-      <Link
-        to={localizePath({ path: '/login', language: locale })}
-        className={`${className}`}
-        data-cy="login-link"
-      >
-        <Trans ns="login">Login</Trans>
-      </Link>
-      <span className="mx-2 text-gray-300">
-        <Trans>or</Trans>
-      </span>
       <Button color={'gray'} href="/signup" size={'sm'} data-cy="signup-link">
-        <Trans ns="login">Sign up</Trans>
+        <Trans ns="login">Subscribe</Trans>
       </Button>
     </>
   );
 };
 
-const Logout = ({ className = '', user }) => {
-  const localizePath = useLocalizePath();
-
-  const { locale } = useLocalization();
-
+const Subscriptions = () => {
   return (
     <div className="flex flex-col text-xs w-full justify-center items-center">
-      <Link
-        to={localizePath({ path: '/account', language: locale })}
-        className={`${className}`}
-        data-cy="account-link"
-      >
-        <Trans ns="login">{user?.profile?.email}</Trans>
-      </Link>
+      <Button color={'gray'} href="/account" size={'sm'} data-cy="signup-link">
+        <Trans ns="login">Your Account</Trans>
+      </Button>
     </div>
   );
 };
