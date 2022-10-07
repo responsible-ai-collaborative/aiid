8d0cd5e3e6653830fae2e6cabd9c43873bb49878
Pablo Costa <pablo@botsfactory.io>
2022-10-07 17:17:49 -0300

    Feature/notify incident update (#1208)
    * Add initial version of the `onIncidentUpdate` trigger and function
    
    * Add `"full_document_before_change": true`
    
    * Allow `system` user to send emails
    
    * Move send notifications code to `onIncidentUpdate` function
    
    * Delete unused `sendEmail` util
    
    * Move SendGrid templates to `sendEmail` function
    
    * Update `mongodb-atlas/config.json` to allow record pre images of documents before updates
    
    * Send email notification when an incident is updated
    
    * Use Trigger Match Expression to filter not requested incident updates
    
    * Check if report.authors is defined
    
    * Add param validation to avoid trigger error on DB restore

--- site/realm/functions/onIncidentUpdate.js
+++ site/realm/functions/onIncidentUpdate.js
@@ -3,44 +3,53 @@ exports = async function (changeEvent) {
   // Destructure out fields from the change stream event object
   const { updateDescription, fullDocument, fullDocumentBeforeChange } = changeEvent;
 
-  // Check if the "reports" field was updated
-  const updatedFields = Object.keys(updateDescription.updatedFields);
+  if(!updateDescription || !fullDocumentBeforeChange || !fullDocument) {
+    console.log('Invalid changeEvent:', JSON.stringify(changeEvent));
+    return;
+  }
 
-  const isReportListChanged = updatedFields.some(field => field.match(/reports/));
+  const incidentId = fullDocument.incident_id;
 
-  if (isReportListChanged) {
-    const { reports: newReports } = fullDocument;
-    const { reports: oldReports } = fullDocumentBeforeChange;
+  console.log(`Processing updates on incident ${incidentId}`);
+  console.log('updateDescription', JSON.stringify(updateDescription));
 
-    // If a new report is added, send the email notification to all subscribers
-    if (newReports.length > oldReports.length) {
+  const subscriptionsCollection = context.services.get('mongodb-atlas').db('customData').collection("subscriptions");
+  const subscriptions = await subscriptionsCollection.find({ type: 'incident', incident_id: incidentId }).toArray();
 
-      const newReportNumber = newReports.filter(report => !oldReports.includes(report))[0];
+  // Process subscriptions to incident updates
+  if (subscriptions.length > 0) {
 
-      const reportsCollection = context.services.get('mongodb-atlas').db('aiidprod').collection("reports");
-      const newReport = await reportsCollection.findOne({ report_number: newReportNumber })
+    const userIds = subscriptions.map((subscription) => subscription.userId);
 
-      const incidentId = fullDocument.incident_id;
-      const subscriptionsCollection = context.services.get('mongodb-atlas').db('customData').collection("subscriptions");
-      const subscriptions = await subscriptionsCollection.find({ type: 'incident', incident_id: incidentId }).toArray();
+    const recipients = [];
 
-      // Process subscriptions to incident updates
-      if (subscriptions.length > 0) {
+    for (const userId of userIds) {
+      const userResponse = await context.functions.execute('getUser', { userId });
 
-        const userIds = subscriptions.map((subscription) => subscription.userId);
+      if (userResponse.email) {
+        recipients.push({
+          email: userResponse.email,
+          userId,
+        });
+      }
+    }
 
-        const recipients = [];
+    // Check if the "reports" field was updated
+    const updatedFields = Object.keys(updateDescription.updatedFields);
 
-        for (const userId of userIds) {
-          const userResponse = await context.functions.execute('getUser', { userId });
+    const isReportListChanged = updatedFields.some(field => field.match(/reports/));
 
-          if (userResponse.email) {
-            recipients.push({
-              email: userResponse.email,
-              userId,
-            });
-          }
-        }
+    if (isReportListChanged) {
+      const { reports: newReports } = fullDocument;
+      const { reports: oldReports } = fullDocumentBeforeChange;
+
+      // If a new report is added, send the email notification to all subscribers
+      if (newReports.length > oldReports.length) {
+
+        const newReportNumber = newReports.filter(report => !oldReports.includes(report))[0];
+
+        const reportsCollection = context.services.get('mongodb-atlas').db('aiidprod').collection("reports");
+        const newReport = await reportsCollection.findOne({ report_number: newReportNumber })
 
         //Send email notification
         const sendEmailParams = {
@@ -52,20 +61,42 @@ exports = async function (changeEvent) {
             incidentUrl: `https://incidentdatabase.ai/cite/${incidentId}`,
             reportUrl: `https://incidentdatabase.ai/cite/${incidentId}#r${newReportNumber}`,
             reportTitle: newReport.title,
-            reportAuthor: newReport.authors[0],
+            reportAuthor: newReport.authors[0] ? newReport.authors[0] : '',
           },
-          templateId: 'IncidentUpdate' // Template value from "site/realm/functions/sendEmail.js" EMAIL_TEMPLATES constant
+          templateId: 'NewReportAddedToAnIncident' // Template value from "site/realm/functions/sendEmail.js" EMAIL_TEMPLATES constant
         };
         const sendEmailresult = await context.functions.execute('sendEmail', sendEmailParams);
 
         console.log('sendEmailParams', JSON.stringify(sendEmailParams));
         console.log(JSON.stringify(sendEmailresult));
+
         return sendEmailresult;
       }
+      else { //if a report was deleted
+        const deletedReportNumber = oldReports.filter(report => !newReports.includes(report))[0];
+        console.log('Report deleted:', deletedReportNumber);
+      }
     }
-    else { //if a report was deleted
-      const deletedReportNumber = oldReports.filter(report => !newReports.includes(report))[0];
-      console.log('Report deleted:', deletedReportNumber);
-    }
+
+    //Send email notification
+    const sendEmailParams = {
+      recipients,
+      subject: 'Incident {{incidentId}} was updated',
+      dynamicData: {
+        incidentId: `${incidentId}`,
+        incidentTitle: fullDocument.title,
+        incidentUrl: `https://incidentdatabase.ai/cite/${incidentId}`,
+      },
+      templateId: 'IncidentUpdate' // Template value from "site/realm/functions/sendEmail.js" EMAIL_TEMPLATES constant
+    };
+    const sendEmailresult = await context.functions.execute('sendEmail', sendEmailParams);
+
+    console.log('sendEmailParams', JSON.stringify(sendEmailParams));
+    console.log(JSON.stringify(sendEmailresult));
+
+    return sendEmailresult;
   }
+
+  console.log('No email subscriptions to process');
+  return;
 };
