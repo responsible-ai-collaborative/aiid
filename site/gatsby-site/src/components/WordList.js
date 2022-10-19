0f8e0731533965111ffd9333cf32c598f8ecd7ed
Luna McNulty <l.mcnulty@riseup.net>
2022-10-19 10:20:23 -0400

    Fix margins in ListGroups

--- site/gatsby-site/src/components/WordList.js
+++ site/gatsby-site/src/components/WordList.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Wordlist = ({ content }) => {
   return (
-    <ListGroup>
+    <ListGroup className="ml-0">
       {content.map((value, index) => (
         <div
           className={`p-3 ${index < content.length - 1 ? 'border-b' : ''}`}
           key={`word-${value[0]}`}
         >
           <div className="flex flex-row w-full justify-between">
-            <p className="truncate text-sm font-medium text-gray-900 dark:text-white mb-0">
+            <p className="truncate text-sm font-medium text-gray-900 dark:text-white my-0">
               {value[0]}
             </p>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
