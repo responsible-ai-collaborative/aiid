7bd1665f1bd5615bb292c0b528637e4bbecabafe
Luna McNulty <l.mcnulty@riseup.net>
2022-10-19 10:21:51 -0400

    Remove inapplicable className

--- site/gatsby-site/src/components/WordList.js
+++ site/gatsby-site/src/components/WordList.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Wordlist = ({ content }) => {
   return (
-    <ListGroup className="ml-0">
+    <ListGroup>
       {content.map((value, index) => (
         <div
           className={`p-3 ${index < content.length - 1 ? 'border-b' : ''}`}
