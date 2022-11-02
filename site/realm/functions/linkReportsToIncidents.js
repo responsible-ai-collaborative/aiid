5c62a3b042038179f45d8a2740036cdaee3eb2b0
Cesar Varela <mail@cesarvarela.com>
2022-11-01 22:40:54 -0300

    Update function to handle unlinked reports

--- site/realm/functions/linkReportsToIncidents.js
+++ site/realm/functions/linkReportsToIncidents.js
@@ -1,8 +1,9 @@
 exports = async (input) => {
-  
+
   const incidents = context.services.get('mongodb-atlas').db('aiidprod').collection("incidents");
-  
-  await incidents.updateMany({incident_id: {$in: input.incident_ids}}, { $addToSet: { reports: { $each: input.report_numbers.map(BSON.Int32) } }});
-  
-  return incidents.find({incident_id: {$in:  input.incident_ids }}).toArray();
+
+  await incidents.updateMany({ reports: { $in: input.report_numbers } }, { $pull: { reports: { $in: input.report_numbers.map(BSON.Int32) } } });
+  await incidents.updateMany({ incident_id: { $in: input.incident_ids } }, { $addToSet: { reports: { $each: input.report_numbers.map(BSON.Int32) } } });
+
+  return incidents.find({ incident_id: { $in: input.incident_ids } }).toArray();
 };
