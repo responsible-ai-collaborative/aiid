cabc86baf84beafe1b6c3d4943436eb7c3486fb6
Clara Youdale <clara@botsfactory.io>
2022-12-22 18:38:18 -0300

    Test with waiting only for RelatedReportsByPublishedDate

--- site/gatsby-site/cypress/e2e/integration/submit.cy.js
+++ site/gatsby-site/cypress/e2e/integration/submit.cy.js
@@ -565,15 +565,7 @@ describe('The Submit form', () => {
       cy.get(`input[name="${key}"]`).type(values[key]);
     }
 
-    cy.wait(
-      [
-        '@RelatedReportsByURL',
-        '@RelatedReportsByPublishedDate',
-        '@RelatedReportsByAuthor',
-        '@RelatedReportsByIncidentId',
-      ],
-      { timeout: 10000 }
-    );
+    cy.wait(['@RelatedReportsByPublishedDate'], { timeout: 10000 });
 
     cy.get('[data-cy="empty-message"]').should('be.visible');
 
