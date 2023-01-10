158262137a51d464e9b45e3fdc717f2452099b8d
Pablo Costa <pablo@botsfactory.io>
2023-01-10 11:50:26 -0300

    Move `format` date function to `useEffect`

--- site/gatsby-site/src/components/landing/LatestIncidentReport.js
+++ site/gatsby-site/src/components/landing/LatestIncidentReport.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import md5 from 'md5';
-//import { format } from 'date-fns';
+import { format } from 'date-fns';
 import { Image } from '../../utils/cloudinary';
 import Link from 'components/ui/Link';
 import ReportText from 'components/reports/ReportText';
@@ -8,14 +8,11 @@ import { Trans } from 'react-i18next';
 import { LocalizedLink } from 'gatsby-theme-i18n';
 
 const LatestIncidentReport = ({ report }) => {
-  const {
-    image_url,
-    cloudinary_id,
-    title,
-    text,
-    //epoch_date_submitted,
-    incident_id,
-  } = report;
+  const { image_url, cloudinary_id, title, text, epoch_date_submitted, incident_id } = report;
+
+  const [formattedDate, setFormattedDate] = useState(null);
+
+  useEffect(() => setFormattedDate(format(epoch_date_submitted * 1000, 'MMM d, yyyy')), []);
 
   return (
     <div className="flex flex-col items-center bg-white rounded-lg border  shadow-md md:flex-row dark:border-gray-700 dark:bg-gray-800">
@@ -43,9 +40,7 @@ const LatestIncidentReport = ({ report }) => {
               {title}
             </h5>
           </LocalizedLink>
-          {/* <span className="text-sm text-gray-500 dark:text-gray-400">
-            {format(epoch_date_submitted * 1000, 'MMM d, yyyy')}
-          </span> */}
+          <span className="text-sm text-gray-500 dark:text-gray-400">{formattedDate}</span>
           <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <ReportText maxChars={240} text={text} />
           </div>
