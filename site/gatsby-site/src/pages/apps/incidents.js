65409b2ec43a8314b50411b7f223e1d4dc56803a
Cesar Varela <mail@cesarvarela.com>
2023-01-31 17:04:10 -0300

    Optimize incidents table query to reduce load times

--- site/gatsby-site/src/pages/apps/incidents.js
+++ site/gatsby-site/src/pages/apps/incidents.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import LayoutHideSidebar from '../../components/LayoutHideSidebar';
 import IncidentsTable from '../../components/incidents/IncidentsTable';
-import { FIND_INCIDENTS } from '../../graphql/incidents';
+import { FIND_INCIDENTS_TABLE } from '../../graphql/incidents';
 import { useQuery } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
 import AiidHelmet from '../../components/AiidHelmet';
 import ListSkeleton from 'elements/Skeletons/List';
 
 export default function IncidentsPage(props) {
-  const { data: incidentsData } = useQuery(FIND_INCIDENTS);
+  const { data: incidentsData } = useQuery(FIND_INCIDENTS_TABLE);
 
   const { t } = useTranslation();
 
