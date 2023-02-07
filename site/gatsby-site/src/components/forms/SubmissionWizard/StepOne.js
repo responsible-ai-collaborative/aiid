b1d657d0be816da281e0f6b9f2410f172c0dab09
Clara Youdale <clara@botsfactory.io>
2023-02-07 14:26:34 -0300

    Fix Editor error on second submit

--- site/gatsby-site/src/components/forms/SubmissionWizard/StepOne.js
+++ site/gatsby-site/src/components/forms/SubmissionWizard/StepOne.js
@@ -321,7 +321,11 @@ const FormDetails = ({
               value={values.text}
               onChange={(value) => {
                 setFieldValue('text', value);
-                setFieldTouched('text', true);
+                if (!submissionComplete) {
+                  setFieldTouched('text', true);
+                } else if (value === '') {
+                  setFieldTouched('text', false);
+                }
               }}
             />
           </div>
