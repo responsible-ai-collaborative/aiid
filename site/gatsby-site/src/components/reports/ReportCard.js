10be4da59554ab798a46d44ab4789fd478837109
Clara Youdale <clara@botsfactory.io>
2023-02-21 14:51:35 -0300

    Fix hydration errors

--- site/gatsby-site/src/components/reports/ReportCard.js
+++ site/gatsby-site/src/components/reports/ReportCard.js
@@ -71,7 +71,7 @@ const ReportCard = ({ item, className = '', incidentId }) => {
             }).replace(' ', '.')}
           />
         </div>
-        <p
+        <div
           className="mt-0 cursor-default"
           role="presentation"
           onClick={(e) => e.stopPropagation()}
@@ -115,93 +115,93 @@ const ReportCard = ({ item, className = '', incidentId }) => {
               </div>
             )}
           </div>
+        </div>
 
-          <ReportText text={item.text} maxChars={expanded ? null : 240} />
-          {expanded && hasVariantData(item) && (
-            <div className="flex w-full flex-col mt-3 gap-2">
-              <div className="font-bold flex items-center gap-2">
-                <Trans ns="variants">Input and circumstances</Trans>
-                <Tooltip
-                  content={
-                    <Trans ns="variants">
-                      Provide the relevant details producing the incident. Examples include the
-                      input prompts to a chatbot or a description of the circumstances leading to
-                      injuries sustained from a robot.
-                    </Trans>
-                  }
-                  trigger="click"
-                  placement="right"
-                >
-                  <FontAwesomeIcon
-                    icon={faQuestionCircle}
-                    style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
-                    className="far fa-question-circle"
-                  />
-                </Tooltip>
-              </div>
-              <div data-cy="variant-text_inputs" className="border-1 rounded-lg px-3">
-                <Markdown>{item.text_inputs}</Markdown>
-              </div>
-              <div className="font-bold flex items-center gap-2">
-                <Trans ns="variants">Output and outcomes</Trans>
-                <Tooltip
-                  content={
-                    <Trans ns="variants">
-                      Provide the relevant details surrounding the incident. Examples include output
-                      text from a chatbot or the nature of injuries sustained from a robot.
-                    </Trans>
-                  }
-                  trigger="click"
-                  placement="right"
-                >
-                  <FontAwesomeIcon
-                    icon={faQuestionCircle}
-                    style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
-                    className="far fa-question-circle"
-                  />
-                </Tooltip>
-              </div>
-              <div data-cy="variant-text_outputs" className="border-1 rounded-lg px-3">
-                <Markdown>{item.text_outputs}</Markdown>
-              </div>
-            </div>
-          )}
-          <div className="flex justify-end">
-            <button
-              onClick={toggleReadMore}
-              className="text-blue-700 border ml-1 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs p-1.5 text-center inline-flex items-center mr-2  dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
-              data-cy={`${expanded ? 'collapse' : 'expand'}-report-button`}
-            >
-              <Trans>{expanded ? 'Collapse' : 'Read More'}</Trans>
-              <svg
-                aria-hidden="true"
-                className="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+        <ReportText text={item.text} maxChars={expanded ? null : 240} />
+        {expanded && hasVariantData(item) && (
+          <div className="flex w-full flex-col mt-3 gap-2">
+            <div className="font-bold flex items-center gap-2">
+              <Trans ns="variants">Input and circumstances</Trans>
+              <Tooltip
+                content={
+                  <Trans ns="variants">
+                    Provide the relevant details producing the incident. Examples include the input
+                    prompts to a chatbot or a description of the circumstances leading to injuries
+                    sustained from a robot.
+                  </Trans>
+                }
+                trigger="click"
+                placement="right"
               >
-                {expanded ? (
-                  <path
-                    fillRule="evenodd"
-                    d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                ) : (
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                )}
-              </svg>
-            </button>
-          </div>
-          {expanded && (
-            <div className="flex w-full flex-row justify-around items-center text-dark-gray">
-              <Actions item={item} />
+                <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
+                  className="far fa-question-circle"
+                />
+              </Tooltip>
             </div>
-          )}
-        </p>
+            <div data-cy="variant-text_inputs" className="border-1 rounded-lg px-3">
+              <Markdown>{item.text_inputs}</Markdown>
+            </div>
+            <div className="font-bold flex items-center gap-2">
+              <Trans ns="variants">Output and outcomes</Trans>
+              <Tooltip
+                content={
+                  <Trans ns="variants">
+                    Provide the relevant details surrounding the incident. Examples include output
+                    text from a chatbot or the nature of injuries sustained from a robot.
+                  </Trans>
+                }
+                trigger="click"
+                placement="right"
+              >
+                <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
+                  className="far fa-question-circle"
+                />
+              </Tooltip>
+            </div>
+            <div data-cy="variant-text_outputs" className="border-1 rounded-lg px-3">
+              <Markdown>{item.text_outputs}</Markdown>
+            </div>
+          </div>
+        )}
+        <div className="flex justify-end">
+          <button
+            onClick={toggleReadMore}
+            className="text-blue-700 border ml-1 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs p-1.5 text-center inline-flex items-center mr-2  dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+            data-cy={`${expanded ? 'collapse' : 'expand'}-report-button`}
+          >
+            <Trans>{expanded ? 'Collapse' : 'Read More'}</Trans>
+            <svg
+              aria-hidden="true"
+              className="ml-2 -mr-1 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {expanded ? (
+                <path
+                  fillRule="evenodd"
+                  d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                ></path>
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                ></path>
+              )}
+            </svg>
+          </button>
+        </div>
+        {expanded && (
+          <div className="flex w-full flex-row justify-around items-center text-dark-gray">
+            <Actions item={item} />
+          </div>
+        )}
       </div>
     </>
   );
