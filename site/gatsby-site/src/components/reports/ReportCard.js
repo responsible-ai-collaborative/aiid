ef74cc84ab9ae2411e4dfe2fe3a70e6973daeecd
Clara Youdale <clara@botsfactory.io>
2023-02-22 19:16:53 -0300

    Fix/expand card (#1594)
    * Keep image and wrap text when expanding card
    
    * Update image height on window resize
    
    * Revert object fit to cover
    
    * Add paddings to card image
    
    * Fix hydration errors
    
    * Add expand/collapse function to margins
    
    * Reuse toggleReadMore function
    
    * Fix hydration errors
    
    * Fix bottom buttons clicks

--- site/gatsby-site/src/components/reports/ReportCard.js
+++ site/gatsby-site/src/components/reports/ReportCard.js
@@ -26,137 +26,159 @@ const ReportCard = ({ item, className = '', incidentId }) => {
 
   const ref = useRef(null);
 
+  const imageRef = useRef(null);
+
   const authors = item.authors.join(', ');
 
+  const toggleReadMore = () => {
+    setExpanded(!expanded);
+    if (expanded) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const toggleReadMoreKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      toggleReadMore();
+    }
+  };
+
   return (
-    <div
-      className={`flex flex-col items-center bg-white rounded-lg border  shadow-md md:flex-row dark:border-gray-700 dark:bg-gray-800 ${className}}`}
-      id={`r${item.report_number}`}
-      ref={ref}
-      data-cy="incident-report-card"
-    >
+    <>
       <div
-        className={`${
-          expanded ? 'md:hidden' : ''
-        } flex self-stretch justify-center items-center md:border-r md:w-1/3`}
+        className={`bg-white rounded-lg border  shadow-md dark:border-gray-700 dark:bg-gray-800 ${className} p-4 relative cursor-pointer`}
+        id={`r${item.report_number}`}
+        ref={ref}
+        data-cy="incident-report-card"
+        onClick={toggleReadMore}
+        onKeyDown={toggleReadMoreKeyDown}
+        role="presentation"
       >
-        <Image
-          className="img-fluid rounded-start h-full w-full max-w-full rounded-t-lg md:rounded-l-lg md:rounded-r-none border-r object-cover"
-          publicID={item.cloudinary_id ? item.cloudinary_id : `legacy/${md5(item.image_url)}`}
-          alt={item.title}
-          transformation={fill().height(480)}
-          itemIdentifier={t('Report {{report_number}}', {
-            report_number: item.report_number,
-          }).replace(' ', '.')}
-        />
-      </div>
-      <div
-        className={`flex flex-col justify-between leading-normal p-4 ${expanded ? '' : 'md:w-2/3'}`}
-      >
-        <div className="flex items-center w-full justify-between">
-          <div>
-            <LocalizedLink to={`#r${item.report_number}`} className="max-w-full cursor-pointer">
-              <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white w-full hover:text-primary-blue">
-                <Trans ns="landing">{item.title}</Trans>
-              </h5>
-            </LocalizedLink>
+        <div
+          className={`flex self-stretch justify-center items-center w-1/3 float-left pr-4 cursor-default md:min-h-[130px]`}
+          ref={imageRef}
+          role="presentation"
+          onClick={(e) => e.stopPropagation()}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <Image
+            className={`img-fluid h-full w-full max-w-full object-cover`}
+            publicID={item.cloudinary_id ? item.cloudinary_id : `legacy/${md5(item.image_url)}`}
+            alt={item.title}
+            transformation={fill().height(480)}
+            itemIdentifier={t('Report {{report_number}}', {
+              report_number: item.report_number,
+            }).replace(' ', '.')}
+          />
+        </div>
+        <div
+          className="mt-0 cursor-default"
+          role="presentation"
+          onClick={(e) => e.stopPropagation()}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <LocalizedLink to={`#r${item.report_number}`} className="max-w-full cursor-pointer">
+            <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white w-full hover:text-primary-blue">
+              <Trans ns="landing">{item.title}</Trans>
+            </h5>
+          </LocalizedLink>
+          <div className="flex justify-between">
             <WebArchiveLink url={item.url} className="text-dark-gray">
               {item.source_domain} &middot;{' '}
               {item.date_published ? item.date_published.substring(0, 4) : 'Needs publish date'}
             </WebArchiveLink>
-            <div className="mt-1 flex w-fit">
-              <TranslationBadge className="mx-2" originalLanguage={item.language} />
-              {item.tags && item.tags.includes(RESPONSE_TAG) && (
-                <div className="flex-1">
-                  <Badge color={'success'}>
-                    <Trans>{{ authors }} post-incident response</Trans>
-                  </Badge>
-                </div>
-              )}
-              {hasVariantData(item) && (
-                <div className="flex-1">
-                  <Badge color="success" data-cy="responded-badge">
-                    <Trans ns="variants">Has Variant data</Trans>
-                  </Badge>
-                </div>
-              )}
-            </div>
+            {!loading && isRole('incident_editor') && (
+              <Button
+                data-cy="edit-report"
+                size={'xs'}
+                color="light"
+                href={`/cite/edit?report_number=${item.report_number}&incident_id=${incidentId}`}
+              >
+                <Trans>Edit</Trans>
+              </Button>
+            )}
           </div>
-          {!loading && isRole('incident_editor') && (
-            <Button
-              data-cy="edit-report"
-              size={'xs'}
-              color="light"
-              href={`/cite/edit?report_number=${item.report_number}&incident_id=${incidentId}`}
-            >
-              <Trans>Edit</Trans>
-            </Button>
+          <div className="mt-1 flex w-fit">
+            <TranslationBadge className="mx-2" originalLanguage={item.language} />
+            {item.tags && item.tags.includes(RESPONSE_TAG) && (
+              <div className="flex-1">
+                <Badge color={'success'}>
+                  <Trans>{{ authors }} post-incident response</Trans>
+                </Badge>
+              </div>
+            )}
+            {hasVariantData(item) && (
+              <div className="flex-1">
+                <Badge color="success" data-cy="responded-badge">
+                  <Trans ns="variants">Has Variant data</Trans>
+                </Badge>
+              </div>
+            )}
+          </div>
+        </div>
+        <div
+          role={'presentation'}
+          onClick={(e) => e.stopPropagation()}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <ReportText text={item.text} maxChars={expanded ? null : 240} />
+          {expanded && hasVariantData(item) && (
+            <div className="flex w-full flex-col mt-3 gap-2">
+              <div className="font-bold flex items-center gap-2">
+                <Trans ns="variants">Input and circumstances</Trans>
+                <Tooltip
+                  content={
+                    <Trans ns="variants">
+                      Provide the relevant details producing the incident. Examples include the
+                      input prompts to a chatbot or a description of the circumstances leading to
+                      injuries sustained from a robot.
+                    </Trans>
+                  }
+                  trigger="click"
+                  placement="right"
+                >
+                  <FontAwesomeIcon
+                    icon={faQuestionCircle}
+                    style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
+                    className="far fa-question-circle"
+                  />
+                </Tooltip>
+              </div>
+              <div data-cy="variant-text_inputs" className="border-1 rounded-lg px-3">
+                <Markdown>{item.text_inputs}</Markdown>
+              </div>
+              <div className="font-bold flex items-center gap-2">
+                <Trans ns="variants">Output and outcomes</Trans>
+                <Tooltip
+                  content={
+                    <Trans ns="variants">
+                      Provide the relevant details surrounding the incident. Examples include output
+                      text from a chatbot or the nature of injuries sustained from a robot.
+                    </Trans>
+                  }
+                  trigger="click"
+                  placement="right"
+                >
+                  <FontAwesomeIcon
+                    icon={faQuestionCircle}
+                    style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
+                    className="far fa-question-circle"
+                  />
+                </Tooltip>
+              </div>
+              <div data-cy="variant-text_outputs" className="border-1 rounded-lg px-3">
+                <Markdown>{item.text_outputs}</Markdown>
+              </div>
+            </div>
           )}
         </div>
-        <div>
-          <ReportText text={item.text} maxChars={expanded ? null : 240} />
-        </div>
-        {expanded && hasVariantData(item) && (
-          <div className="flex w-full flex-col mt-3 gap-2">
-            <div className="font-bold flex items-center gap-2">
-              <Trans ns="variants">Input and circumstances</Trans>
-              <Tooltip
-                content={
-                  <Trans ns="variants">
-                    Provide the relevant details producing the incident. Examples include the input
-                    prompts to a chatbot or a description of the circumstances leading to injuries
-                    sustained from a robot.
-                  </Trans>
-                }
-                trigger="click"
-                placement="right"
-              >
-                <FontAwesomeIcon
-                  icon={faQuestionCircle}
-                  style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
-                  className="far fa-question-circle"
-                />
-              </Tooltip>
-            </div>
-            <div data-cy="variant-text_inputs" className="border-1 rounded-lg px-3">
-              <Markdown>{item.text_inputs}</Markdown>
-            </div>
-            <div className="font-bold flex items-center gap-2">
-              <Trans ns="variants">Output and outcomes</Trans>
-              <Tooltip
-                content={
-                  <Trans ns="variants">
-                    Provide the relevant details surrounding the incident. Examples include output
-                    text from a chatbot or the nature of injuries sustained from a robot.
-                  </Trans>
-                }
-                trigger="click"
-                placement="right"
-              >
-                <FontAwesomeIcon
-                  icon={faQuestionCircle}
-                  style={{ color: 'rgb(210, 210, 210)', cursor: 'pointer' }}
-                  className="far fa-question-circle"
-                />
-              </Tooltip>
-            </div>
-            <div data-cy="variant-text_outputs" className="border-1 rounded-lg px-3">
-              <Markdown>{item.text_outputs}</Markdown>
-            </div>
-          </div>
-        )}
-        <div className="flex justify-end mt-4">
+        <div className="flex justify-end">
           <button
-            onClick={() => {
-              setExpanded(!expanded);
-              if (expanded) {
-                ref.current.scrollIntoView({ behavior: 'smooth' });
-              }
-            }}
-            className="text-blue-700 border mt-4 ml-1 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs p-1.5 text-center inline-flex items-center mr-2  dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+            onClick={toggleReadMore}
+            className="text-blue-700 border ml-1 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xs p-1.5 text-center inline-flex items-center mr-2  dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
             data-cy={`${expanded ? 'collapse' : 'expand'}-report-button`}
           >
-            <Trans>{expanded ? 'Collapse' : 'Expand'}</Trans>
+            <Trans>{expanded ? 'Collapse' : 'Read More'}</Trans>
             <svg
               aria-hidden="true"
               className="ml-2 -mr-1 w-4 h-4"
@@ -181,12 +203,17 @@ const ReportCard = ({ item, className = '', incidentId }) => {
           </button>
         </div>
         {expanded && (
-          <div className="flex w-full flex-row justify-around items-center text-dark-gray">
+          <div
+            className="flex w-full flex-row justify-around items-center text-dark-gray"
+            role={'presentation'}
+            onClick={(e) => e.stopPropagation()}
+            onKeyDown={(e) => e.stopPropagation()}
+          >
             <Actions item={item} />
           </div>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
