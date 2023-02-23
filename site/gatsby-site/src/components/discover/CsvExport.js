ee803b77408f0f57d717e119a8a073ed67e26289
Pablo Costa <pablo@botsfactory.io>
2023-02-23 17:42:21 -0300

    Add export CVS tooltip

--- site/gatsby-site/src/components/discover/CsvExport.js
+++ site/gatsby-site/src/components/discover/CsvExport.js
@@ -4,10 +4,13 @@ import CsvDownloadButton from 'react-json-to-csv';
 import { format, fromUnixTime } from 'date-fns';
 import { faFileCsv } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from 'flowbite-react';
+import { Button, Tooltip } from 'flowbite-react';
+import { Trans } from 'react-i18next';
+import Link from 'components/ui/Link';
 
 const convertData = (hits) => {
   return hits.map((hit) => ({
+    'incident id': hit.incident_id,
     'report number': hit.report_number,
     title: hit.title,
     'date published': format(fromUnixTime(hit.epoch_date_published), 'yyyy-MM-dd'),
@@ -20,7 +23,6 @@ const convertData = (hits) => {
     image: hit.image_url,
     authors: hit.authors.join(','),
     submitters: hit.submitters.join(','),
-    text: hit.text,
   }));
 };
 
@@ -36,20 +38,34 @@ const CsvExport = ({ hits, isSearchStalled }) => {
   return (
     <div>
       {!isSearchStalled && (
-        <CsvDownloadButton
-          data={data}
-          filename="discover_incidents.csv"
-          disabled={!data || data.length == 0}
+        <Tooltip
+          content={
+            <Trans>
+              Export items from the current page to a CSV file.
+              <br />
+              To download the full data set, please follow this{' '}
+              <Link to={'/research/snapshots/'}>link</Link>.
+            </Trans>
+          }
+          trigger="hover"
+          placement="top"
         >
-          <Button
-            color={'gray'}
-            size="sm"
+          <CsvDownloadButton
+            data={data}
+            filename="discover_incidents.csv"
             disabled={!data || data.length == 0}
-            data-cy="export-to-csv"
           >
-            <FontAwesomeIcon icon={faFileCsv} />
-          </Button>
-        </CsvDownloadButton>
+            <Button
+              color="gray"
+              size="sm"
+              disabled={!data || data.length == 0}
+              data-cy="export-to-csv"
+            >
+              <FontAwesomeIcon icon={faFileCsv} className="mr-2" />
+              <Trans>Export</Trans>
+            </Button>
+          </CsvDownloadButton>
+        </Tooltip>
       )}
     </div>
   );
