b759b1ff816166172a0e9f9cd2900204f89e4ce4
Luna McNulty <l.mcnulty@riseup.net>
2023-03-02 15:45:18 -0500

    Add db integration to migration

--- site/gatsby-site/migrations/2023.03.01T23.01.01.add-political-bias-labels.js
+++ site/gatsby-site/migrations/2023.03.01T23.01.01.add-political-bias-labels.js
@@ -1,46 +1,99 @@
-//const config = require('../config');
+const config = require('../config');
+
 const axios = require('axios');
 
 const jsdom = require('jsdom');
 
-exports.up = async (/*{ context: { client } }*/) => {
+exports.up = async ({ context: { client } }) => {
   const { JSDOM } = jsdom;
 
+  const publications = [];
+
+  const addBiasLabel = (publicationTitle, publicationDomain, labeler, label) => {
+    const existingPublication = publications.find((p) => p.domain == publicationDomain);
+
+    if (existingPublication) {
+      existingPublication.biasLabels.push({ label, labeler });
+    } else {
+      publications.push({
+        title: publicationTitle,
+        domain: publicationDomain,
+        biasLabels: [{ label, labeler }],
+      });
+    }
+  };
+
   for (const alignment of ['left', 'leftcenter', 'center', 'right-center', 'right']) {
-    console.log(`alignment`, alignment);
     const mbfcResponse = await axios.get('https://mediabiasfactcheck.com/' + alignment);
 
     if (mbfcResponse.status == 200) {
       const mbfcPage = new JSDOM(mbfcResponse.data);
 
-      const sources = [...mbfcPage.window.document.querySelectorAll('#mbfc-table tr')]
-        .map((tr) => {
-          const deepestChild = getDeepestChild(tr);
+      for (const tr of [...mbfcPage.window.document.querySelectorAll('#mbfc-table tr')]) {
+        const deepestChild = getDeepestChild(tr);
 
-          let tokens = deepestChild.textContent.split(' ');
+        let tokens = deepestChild.textContent.split(' ');
 
-          let lastToken = tokens.pop();
+        let lastToken = tokens.pop();
 
-          let domain;
+        let domain;
 
-          if (lastToken[0] == '(') {
-            domain = lastToken.slice(1, lastToken.length - 1); // Remove parentheses
-          } else {
-            tokens.push(lastToken);
-          }
-          const title = tokens.join(' ');
+        if (lastToken[0] == '(') {
+          domain = lastToken.slice(1, lastToken.length - 1); // Remove parentheses
+        } else {
+          tokens.push(lastToken);
+        }
+        const title = tokens.join(' ');
 
-          return { title, domain };
-        })
-        .filter((source) => source?.title?.length > 0);
+        if (domain) {
+          addBiasLabel(
+            title,
+            domain,
+            'mediabiasfactcheck.com',
+            {
+              left: 'left',
+              right: 'right',
 
-      console.log(`sources`, sources);
+              // These occur in most sources, and it's best to normalize them
+              // to a specific spelling / wording across sources
+              // so we can check if different sources agree.
+              leftcenter: 'center-left',
+              'right-center': 'center-right',
+
+              // They use "center" in the url but "least biased" in the site text.
+              // There's a difference between being unbiased
+              // and being biased towards the center.
+              // They seem to be trying to capture the former:
+              //
+              //  > These sources have minimal bias and use very few loaded words
+              //  > (wording that attempts to influence an audience
+              //  >  by using appeal to emotion or stereotypes).
+              //  > The reporting is factual and usually sourced.
+              //  > These are the most credible media sources.
+              //
+              center: 'least biased',
+            }[alignment]
+          );
+        }
+      }
     }
   }
-  throw 'ok';
+  await client.connect();
+
+  const publicationsCollection = await client
+    .db(config.realm.production_db.db_name)
+    .createCollection('publications');
+
+  for (const publication of publications) {
+    publicationsCollection.insertOne(publication);
+  }
 };
 
-exports.down = async () => {};
+exports.down = async ({ context: { client } }) => {
+  await client.connect();
+
+  await client.db(config.realm.production_db.db_name).dropCollection('publications');
+};
 
 var getDeepestChild = (htmlNode) => {
   if (htmlNode.children.length == 0) {
