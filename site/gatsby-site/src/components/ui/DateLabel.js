2700e49bd02991dc771b0c980df4238b75b3b0b8
Cesar Varela <mail@cesarvarela.com>
2023-03-06 20:09:55 -0300

    Use date label component for post previews

---
+++ site/gatsby-site/src/components/ui/DateLabel.js
@@ -0,0 +1,22 @@
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+
+export default function DateLabel({ date, className = '' }) {
+  const [formmated, setFormatted] = useState('');
+
+  useEffect(() => setFormatted(format(new Date(date), 'yyyy-MM-dd')), []);
+
+  if (!formmated) {
+    return (
+      <span role="status" className={className}>
+        <span
+          role="status"
+          style={{ height: '1ch' }}
+          className="animate-pulse inline-block bg-gray-200 rounded-full dark:bg-gray-700 w-24"
+        ></span>
+      </span>
+    );
+  }
+
+  return <span className={className}>{formmated}</span>;
+}
