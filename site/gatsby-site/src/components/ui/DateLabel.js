f253fc5f6d573cf6725b680ac960c2eed69f70a9
Cesar Varela <mail@cesarvarela.com>
2023-03-07 16:46:17 -0300

    Format in utc tz

--- site/gatsby-site/src/components/ui/DateLabel.js
+++ site/gatsby-site/src/components/ui/DateLabel.js
@@ -1,22 +1,8 @@
-import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { formatInTimeZone } from 'date-fns-tz';
 
 export default function DateLabel({ date, className = '' }) {
-  const [formmated, setFormatted] = useState('');
-
-  useEffect(() => setFormatted(format(date, 'yyyy-MM-dd')), []);
-
-  if (!formmated) {
-    return (
-      <span role="status" className={className}>
-        <span
-          role="status"
-          style={{ height: '1ch' }}
-          className="animate-pulse inline-block bg-gray-200 rounded-full dark:bg-gray-700 w-24"
-        ></span>
-      </span>
-    );
-  }
+  const formmated = formatInTimeZone(date, 'UTC', 'yyyy-MM-dd');
 
   return <span className={className}>{formmated}</span>;
 }
