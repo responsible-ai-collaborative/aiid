cd4c055f752c6f52a433055efafa8dea8ecb324d
Clara Youdale <clara@botsfactory.io>
2023-03-07 20:57:48 -0300

    Use LocalizedLink instead of Link in Latest Incident Report

--- site/gatsby-site/src/components/landing/LatestIncidentReport.js
+++ site/gatsby-site/src/components/landing/LatestIncidentReport.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import md5 from 'md5';
 import { format } from 'date-fns';
 import { Image } from '../../utils/cloudinary';
-import Link from 'components/ui/Link';
 import ReportText from 'components/reports/ReportText';
 import { Trans, useTranslation } from 'react-i18next';
 import { LocalizedLink } from 'plugins/gatsby-theme-i18n';
@@ -47,7 +46,7 @@ const LatestIncidentReport = ({ report }) => {
             <ReportText maxChars={240} text={text} />
           </div>
           <div className="flex justify-end">
-            <Link
+            <LocalizedLink
               to={`/cite/${incident_id}`}
               className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
@@ -65,7 +64,7 @@ const LatestIncidentReport = ({ report }) => {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </Link>
+            </LocalizedLink>
           </div>
         </div>
       </div>
