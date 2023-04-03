b458e03025a98eeeecedd1974cc8d83e57f5fb18
Clara Youdale <clara@botsfactory.io>
2023-04-03 17:07:59 -0300

    Fix hydration issue on inputs' names

--- site/gatsby-site/src/components/forms/TextInputGroup.js
+++ site/gatsby-site/src/components/forms/TextInputGroup.js
@@ -91,24 +91,34 @@ const TextAreaInput = ({
   className = '',
   addOnComponent,
   ...props
-}) => (
-  <>
-    <textarea
-      name={name}
-      className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white min-h-[40px] ${className} ${
-        touched[name] && errors[name]
-          ? 'border-red-600 focus:ring-red-500'
-          : 'border-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500'
-      } ${className}`}
-      placeholder={placeholder}
-      value={values[name] || props.defaultValue || ''}
-      {...props}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    ></textarea>
-    {addOnComponent}
-  </>
-);
+}) => {
+  const [inputName, setInputName] = useState('');
+
+  useEffect(() => {
+    if (name) {
+      setInputName(name);
+    }
+  }, [name]);
+
+  return (
+    <>
+      <textarea
+        name={inputName}
+        className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white min-h-[40px] ${className} ${
+          touched[name] && errors[name]
+            ? 'border-red-600 focus:ring-red-500'
+            : 'border-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500'
+        } ${className}`}
+        placeholder={placeholder}
+        value={values[name] || props.defaultValue || ''}
+        {...props}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      ></textarea>
+      {addOnComponent}
+    </>
+  );
+};
 
 const Input = ({
   name,
@@ -122,24 +132,34 @@ const Input = ({
   className = '',
   addOnComponent,
   ...props
-}) => (
-  <div className="w-full flex">
-    <input
-      type={type}
-      name={name}
-      className={`bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white ${
-        errors && touched && touched[name] && errors[name]
-          ? 'border-red-600 focus:ring-red-500'
-          : 'border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500'
-      } ${className}`}
-      placeholder={placeholder}
-      value={values[name] || props.defaultValue || ''}
-      {...props}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {addOnComponent}
-  </div>
-);
+}) => {
+  const [inputName, setInputName] = useState('');
+
+  useEffect(() => {
+    if (name) {
+      setInputName(name);
+    }
+  }, [name]);
+
+  return (
+    <div className="w-full flex">
+      <input
+        type={type}
+        name={inputName}
+        className={`bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white ${
+          errors && touched && touched[name] && errors[name]
+            ? 'border-red-600 focus:ring-red-500'
+            : 'border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500'
+        } ${className}`}
+        placeholder={placeholder}
+        value={values[name] || props.defaultValue || ''}
+        {...props}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {addOnComponent}
+    </div>
+  );
+};
 
 export default TextInputGroup;
