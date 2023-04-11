d1b00ee3f79bff3400b887531766b224e0ed6039
Clara Youdale <clara@botsfactory.io>
2023-04-11 16:52:50 -0300

    Refactor tooltip

--- site/gatsby-site/src/components/visualizations/Timeline.js
+++ site/gatsby-site/src/components/visualizations/Timeline.js
@@ -47,27 +47,64 @@ const AxisLeft = ({ yScale, margin, data }) => {
 const DataPoint = ({ bucket, groupRadius, radius, yScale, setTooltipPosition }) => {
   const gRef = useRef(null);
 
+  const fORef = useRef(null);
+
   const [showTooltip, setShowTooltip] = useState(false);
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (gRef.current && !gRef.current.contains(event.target)) {
+        setShowTooltip(false);
+        setTooltipPosition(0, 0, null);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [gRef]);
+
   const tooltipContent = (
-    <ul className="m-0 p-0">
-      {bucket.slice(1).map((b) => (
-        <li className="text-[12px] mt-[6px] first:mt-[0%]" key={b.mongodb_id}>
-          {timeFormat('%b %d, %Y')(new Date(b.date_published))}
-          <br />
-          {b.isOccurrence ? <>{b.title}</> : <a href={`#r${b.report_number}`}>{b.title}</a>}
-        </li>
-      ))}
-    </ul>
+    <div className="bg-white text-gray-900 absolute opacity-100 z-50 px-2 py-1 border border-gray-900 rounded w-fit">
+      <ul className="m-0 p-0">
+        {bucket.slice(1).map((b) => (
+          <li className="text-[12px] mt-[6px] first:mt-[0%]" key={b.mongodb_id}>
+            <p className="whitespace-nowrap m-0 font-bold mb-1">
+              {timeFormat('%b %d, %Y')(new Date(b.date_published))}
+            </p>
+            {b.isOccurrence ? (
+              <p className="whitespace-nowrap m-0">{b.title}</p>
+            ) : (
+              <a href={`#r${b.report_number}`} className="whitespace-nowrap">
+                {b.title}
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 
-  const calculateTooltipPosition = () => {
+  const toggleTooltip = () => {
     setShowTooltip(!showTooltip);
     if (!showTooltip) {
-      const { x, y, top, left } = gRef.current.getBoundingClientRect();
+      const g = gRef.current;
 
-      console.log('x, y', x, y, top);
-      setTooltipPosition(left - 270, top - 30, tooltipContent);
+      const parent = g.parentNode;
+
+      const element = fORef.current;
+
+      const { left, top } = element.getBoundingClientRect();
+
+      const { left: parentLeft, top: parentTop } = parent.getBoundingClientRect();
+
+      const offsetLeft = left - parentLeft;
+
+      const offsetTop = top - parentTop;
+
+      setTooltipPosition(offsetLeft, offsetTop, tooltipContent);
     } else {
       setTooltipPosition(0, 0, null);
     }
@@ -75,9 +112,10 @@ const DataPoint = ({ bucket, groupRadius, radius, yScale, setTooltipPosition }) 
 
   return (
     <g
-      ref={gRef}
       key={bucket.x0}
       transform={`translate(20,${(yScale(bucket.x0) + yScale(bucket.x1)) / 2})`}
+      className="z-2"
+      ref={gRef}
     >
       {bucket.length > 1 ? (
         <>
@@ -85,11 +123,20 @@ const DataPoint = ({ bucket, groupRadius, radius, yScale, setTooltipPosition }) 
           <text
             textAnchor="middle"
             dominantBaseline="middle"
-            className="fill-white font-bold text-[12px]"
+            className="fill-white font-bold text-[12px] z-40"
           >
             +{bucket.length - 1}
           </text>
-          <foreignObject x={-8} y={-8} width={16} height={16} onClick={calculateTooltipPosition}>
+          <foreignObject
+            x={-8}
+            y={-8}
+            width={16}
+            height={16}
+            onClick={() => {
+              toggleTooltip();
+            }}
+            ref={fORef}
+          >
             <div className="w-[16px] h-[16px] cursor-pointer" />
           </foreignObject>
         </>
@@ -228,21 +275,21 @@ function Timeline({ data }) {
     }
   }, [containerRef]);
 
-  const [tooltipX, setTooltipX] = useState(0);
+  const [tooltipLeft, setTooltipLeft] = useState(0);
 
-  const [tooltipY, setTooltipY] = useState(0);
+  const [tooltipTop, setTooltipTop] = useState(0);
 
   const [tooltipContent, setTooltipContent] = useState(null);
 
-  const setTooltipPosition = (x, y, content) => {
-    setTooltipX(x);
-    setTooltipY(y);
+  const setTooltipPosition = (left, top, content) => {
+    setTooltipLeft(left);
+    setTooltipTop(top);
     setTooltipContent(content);
   };
 
   return (
     <div ref={containerRef} style={{ height: `${size.height}px` }} className="relative">
-      <svg width="100%" viewBox={`0 0 ${size.width} ${size.height}`}>
+      <svg width="100%" viewBox={`0 0 ${size.width} ${size.height}`} className="">
         <AxisLeft data={data} yScale={yScale} margin={margin} size={size} />
         <Reports
           radius={radius}
@@ -255,14 +302,11 @@ function Timeline({ data }) {
           setTooltipPosition={setTooltipPosition}
         />
       </svg>
-      {tooltipContent && (
-        <div
-          className="absolute tooltip bg-white text-black p-4 rounded border border-black"
-          style={{ left: `${tooltipX}px`, top: `${tooltipY}px` }}
-        >
-          {tooltipContent}
-        </div>
-      )}
+      <div
+        style={{ position: 'absolute', left: tooltipLeft + 100, top: tooltipTop, width: 'auto' }}
+      >
+        {tooltipContent}
+      </div>
     </div>
   );
 }
