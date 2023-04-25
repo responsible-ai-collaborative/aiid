6a598b842210bfb1bb20e4c6755172aadedbf4d5
Pablo Costa <pablo@botsfactory.io>
2023-04-25 15:13:29 -0300

    Fix broken layout on mobile (#1925)

--- site/gatsby-site/src/components/entities/AllegedEntities.js
+++ site/gatsby-site/src/components/entities/AllegedEntities.js
@@ -8,7 +8,7 @@ function EntityBadge({ entity }) {
   return (
     <Link
       to={to}
-      className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold whitespace-nowrap px-2.5 py-0.5 my-0.5 rounded dark:bg-blue-200 dark:text-blue-800"
+      className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 my-0.5 rounded dark:bg-blue-200 dark:text-blue-800"
     >
       {entity.name}
     </Link>
@@ -21,7 +21,7 @@ function PartyBadge({ entity }) {
   return (
     <Link
       to={to}
-      className="inline-block bg-green-100 text-green-800 text-xs font-semibold whitespace-nowrap px-2.5 py-0.5 my-0.5 rounded dark:bg-green-200 dark:text-green-800"
+      className="inline-block bg-green-100 text-green-800 text-xs font-semibold px-2.5 py-0.5 my-0.5 rounded dark:bg-green-200 dark:text-green-800"
     >
       {entity.name}
     </Link>
