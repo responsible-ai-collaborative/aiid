357c9e7b0fb1840b00a365a446c3dd7acce7ee2f
Luna McNulty <l.mcnulty@riseup.net>
2023-05-08 17:02:13 -0400

    Update text, add translation keys

--- site/gatsby-site/src/components/landing/NewsletterSignup.js
+++ site/gatsby-site/src/components/landing/NewsletterSignup.js
@@ -36,13 +36,13 @@ export default function NewsletterSignup() {
   return (
     <Card className="h-full w-full self-stretch flex justify-center shadow-md">
       <h5 className="text-2xl font-bold tracking-tight p-4 pb-2 text-gray-900 dark:text-white">
-        <Trans ns="landing">AIID Newsletter</Trans>
+        <Trans ns="landing">The AI Incident Briefing</Trans>
       </h5>
       <div className="h-full flex-shrink flex flex-col items-center justify-center p-8 bg-[#e1effe]">
         <img
           src={Envelope}
           alt={t('An envelope with a neural net diagram on its left')}
-          className="w-1/2 mx-auto drop-shadow-xl mb-6"
+          className="w-2/5 mx-auto drop-shadow-xl mb-6"
         />
         <Formik
           initialValues={{ emailSubscription: emailValue }}
@@ -118,7 +118,10 @@ export default function NewsletterSignup() {
           )}
         </Formik>
         <p>
-          <Trans>Get the latest updates from the AIID. Volume: ~1 / month</Trans>
+          <Trans>
+            Subscribe to the AI Incident Briefing and get incident round-ups delivered straight to
+            your inbox, and major organizational updates.
+          </Trans>
         </p>
       </div>
     </Card>
