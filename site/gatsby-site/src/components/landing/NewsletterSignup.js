07bf5be6402ce358fa3ecfdd573c2891b203fee2
Luna McNulty <l.mcnulty@riseup.net>
2023-05-11 23:31:01 -0400

    Update text

--- site/gatsby-site/src/components/landing/NewsletterSignup.js
+++ site/gatsby-site/src/components/landing/NewsletterSignup.js
@@ -24,8 +24,7 @@ export default function NewsletterSignup() {
   });
 
   const {
-    //    user,
-    //    loading,
+    isLoggedIn,
     actions: { signUp },
   } = useUserContext();
 
@@ -44,85 +43,105 @@ export default function NewsletterSignup() {
           alt={t('An envelope with a neural net diagram on its left')}
           className="w-2/5 mx-auto drop-shadow-xl mb-6"
         />
-        <Formik
-          initialValues={{ emailSubscription: emailValue }}
-          validationSchema={SubscribeSchema}
-          onSubmit={async ({ emailSubscription }, { setSubmitting, resetForm }) => {
-            try {
-              await signUp({ email: emailSubscription, password: '123456', redirectTo });
-              addToast({
-                message: t('Thanks for subscribing to our Newsletter!', {
-                  email: emailSubscription,
-                  ns: 'login',
-                }),
-                severity: SEVERITY.success,
-              });
-              resetForm();
-            } catch (e) {
-              console.error(e);
-              addToast({
-                message: (
-                  <label className="capitalize">
-                    {t(e.error || 'An unknown error has ocurred')}
-                  </label>
-                ),
-                severity: SEVERITY.danger,
-                error: e,
-              });
-            }
+        {isLoggedIn ? (
+          <p>
+            <Trans>
+              Check your inbox for the AI Incident Briefing, which includes incident round-ups along
+              with occasional major database updates. You can manage your subscription status from
+              the links in the email footer.
+            </Trans>
+          </p>
+        ) : (
+          <>
+            <Formik
+              initialValues={{ emailSubscription: emailValue }}
+              validationSchema={SubscribeSchema}
+              onSubmit={async ({ emailSubscription }, { setSubmitting, resetForm }) => {
+                try {
+                  await signUp({ email: emailSubscription, password: '123456', redirectTo });
+                  addToast({
+                    message: t('Thanks for subscribing to our Newsletter!', {
+                      email: emailSubscription,
+                      ns: 'login',
+                    }),
+                    severity: SEVERITY.success,
+                  });
+                  resetForm();
+                } catch (e) {
+                  console.error(e);
+                  addToast({
+                    message: (
+                      <label className="capitalize">
+                        {t(e.error || 'An unknown error has ocurred')}
+                      </label>
+                    ),
+                    severity: SEVERITY.danger,
+                    error: e,
+                  });
+                }
 
-            setSubmitting(false);
-          }}
-        >
-          {({ values, touched, handleChange, handleBlur, handleSubmit, isSubmitting, isValid }) => (
-            <Form
-              onSubmit={handleSubmit}
-              className="w-full flex flex-wrap md:flex-nowrap gap-3 items-end"
+                setSubmitting(false);
+              }}
             >
-              <div id="formBasicEmailSubscription" className="w-full shrink-1">
-                <TextInputGroup
-                  type="email"
-                  placeholder={t('Email address')}
-                  name="emailSubscription"
-                  value={values.emailSubscription}
-                  handleChange={(event) => {
-                    setEmailValue(event.target.value);
-                    handleChange(event);
-                  }}
-                  handleBlur={handleBlur}
-                  values={values}
-                  touched={touched}
-                />
-              </div>
-
-              <div className="flex justify-between gap-3 w-full md:w-min">
-                <Button
-                  type="submit"
-                  className="w-full md:w-min"
-                  disabled={isSubmitting || !isValid}
-                  data-cy="subscribe-to-updates-btn"
+              {({
+                values,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                isValid,
+              }) => (
+                <Form
+                  onSubmit={handleSubmit}
+                  className="w-full flex flex-wrap md:flex-nowrap gap-3 items-end"
                 >
-                  <div className="flex gap-2 items-center">
-                    {isSubmitting ? (
-                      <div>
-                        <Spinner size="sm" />
-                      </div>
-                    ) : (
-                      <FontAwesomeIcon icon={faEnvelope} />
-                    )}
-                    <Trans ns="login">Subscribe</Trans>
+                  <div id="formBasicEmailSubscription" className="w-full shrink-1">
+                    <TextInputGroup
+                      type="email"
+                      placeholder={t('Email address')}
+                      name="emailSubscription"
+                      value={values.emailSubscription}
+                      handleChange={(event) => {
+                        setEmailValue(event.target.value);
+                        handleChange(event);
+                      }}
+                      handleBlur={handleBlur}
+                      values={values}
+                      touched={touched}
+                    />
                   </div>
-                </Button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-        <p>
-          <Trans>
-            Subscribe to the AI Incident Briefing and get incident round-ups delivered straight to
-            your inbox, and major organizational updates.
-          </Trans>
-        </p>
+
+                  <div className="flex justify-between gap-3 w-full md:w-min">
+                    <Button
+                      type="submit"
+                      className="w-full md:w-min"
+                      disabled={isSubmitting || !isValid}
+                      data-cy="subscribe-to-updates-btn"
+                    >
+                      <div className="flex gap-2 items-center">
+                        {isSubmitting ? (
+                          <div>
+                            <Spinner size="sm" />
+                          </div>
+                        ) : (
+                          <FontAwesomeIcon icon={faEnvelope} />
+                        )}
+                        <Trans ns="login">Subscribe</Trans>
+                      </div>
+                    </Button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+            <p>
+              <Trans>
+                Subscribe to the AI Incident Briefing and get monthly incident round-ups along with
+                occasional major database updates.
+              </Trans>
+            </p>
+          </>
+        )}
       </div>
     </Card>
   );
