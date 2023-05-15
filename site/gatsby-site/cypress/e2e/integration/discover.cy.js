6354a92561633b3b1bb12a8f86a5ac7757785db7
Pablo Costa <pablo@botsfactory.io>
2023-05-15 20:16:01 -0300

    Fix bug when we search by text and then filter on Discover (#1996)
    * Fix bug when we search by text and then filter on Discover
    
    * Change test name
    
    * Skipping test to allow deploy and test

--- site/gatsby-site/cypress/e2e/integration/discover.cy.js
+++ site/gatsby-site/cypress/e2e/integration/discover.cy.js
@@ -21,6 +21,8 @@ describe('The Discover app', () => {
       .should('exist')
       .and('be.visible');
 
+    cy.waitForStableDOM();
+
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 28);
   });
 
@@ -40,7 +42,8 @@ describe('The Discover app', () => {
 
     cy.url().should('include', 's=starbucks');
 
-    // a flaky assertion here, should improve once a testing enviqronment is set up
+    cy.waitForStableDOM();
+
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 8);
   });
 
@@ -62,6 +65,8 @@ describe('The Discover app', () => {
     cy.get('[data-cy="incident_id-item"]:contains("34")', { timeout: 8000 }).first().click();
 
     cy.url().should('include', 'incident_id=34');
+
+    cy.waitForStableDOM();
 
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 28);
   });
@@ -85,6 +90,8 @@ describe('The Discover app', () => {
 
     cy.url().should('include', 'language=es');
 
+    cy.waitForStableDOM();
+
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 4);
   });
 
@@ -106,6 +113,8 @@ describe('The Discover app', () => {
     cy.get('[data-cy="tags-item"]:contains("response")', { timeout: 8000 }).first().click();
 
     cy.url().should('include', 'tags=response');
+
+    cy.waitForStableDOM();
 
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 1);
   });
@@ -132,6 +141,8 @@ describe('The Discover app', () => {
       .should('contain.text', '1');
 
     cy.url().should('include', 'incident_id=10');
+
+    cy.waitForStableDOM();
 
     cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 8);
   });
@@ -199,7 +210,7 @@ describe('The Discover app', () => {
     cy.get('@windowOpen').should('be.called');
   });
 
-  it("Let's you filter by type", () => {
+  it('Lets you filter by type', () => {
     cy.visit(url);
 
     cy.waitForStableDOM();
@@ -402,5 +413,44 @@ describe('The Discover app', () => {
 
       cy.get(`[data-cy-report-number="${report_number}"]`).should('be.visible');
     }
+  });
+
+  it('Performs a search and filters results by source', () => {
+    cy.visit(url);
+
+    cy.waitForStableDOM();
+
+    cy.get('form#searchForm').as('form');
+
+    // Search by text first
+    cy.get('@form')
+      .get('[data-cy="search-box"] input[placeholder="Type Here"]')
+      .type('google')
+      .type('{enter}');
+
+    cy.url().should('include', 's=google');
+
+    // Filter by source
+    cy.get('[data-cy=expand-filters]').click();
+
+    cy.waitForStableDOM();
+
+    cy.contains('button', 'Source').click();
+
+    cy.waitForStableDOM();
+
+    cy.get('[data-cy="source_domain"] [placeholder="Type Here"]', { timeout: 8000 })
+      .type('theguardian.com')
+      .type('{enter}');
+
+    cy.get('[data-cy="source_domain-item"]:contains("theguardian.com")', { timeout: 8000 })
+      .first()
+      .click();
+
+    cy.url().should('include', 'source_domain=theguardian.com');
+
+    cy.waitForStableDOM();
+
+    cy.get('div[class^="tw-hits-container"]').children().should('have.length.at.least', 8);
   });
 });
