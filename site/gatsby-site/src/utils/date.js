49e842bf2e9e4528815202fc15d62942a660ff51
Pablo Costa <pablo@botsfactory.io>
2023-05-17 21:26:10 -0300

    Only allow past dates on `Date Published` and `Date Downloaded`

--- site/gatsby-site/src/utils/date.js
+++ site/gatsby-site/src/utils/date.js
@@ -1,3 +1,5 @@
+import { parse } from 'date-fns';
+
 // RegEx for date validation
 export const dateRegExp = /^(\+?\d{4})?\s?-?\s?(\(?\d{2}\)?)\s?-?\s?(\(?\d{2}\)?)\s?-?\s?(\?)?$/;
 
@@ -8,4 +10,15 @@ export const validateDate = (date) => {
     return date * 1000;
   }
   return date;
+};
+
+export const isPastDate = {
+  message: '*Date must be in the past',
+  test(value) {
+    const parsedDate = parse(value, 'yyyy-MM-dd', new Date());
+
+    const today = new Date();
+
+    return parsedDate <= today;
+  },
 };
