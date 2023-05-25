3fd21b3bdefc53fbc7a2b5d05ce10056d5b9e544
Clara Youdale <clara@botsfactory.io>
2023-05-25 16:14:25 -0300

    Avoid scrolling based on useEffect on citaiton page

--- site/gatsby-site/src/components/reports/ReportCard.js
+++ site/gatsby-site/src/components/reports/ReportCard.js
@@ -31,6 +31,11 @@ const ReportCard = ({ item, className = '', incidentId, alwaysExpanded = false }
   const toggleReadMore = () => {
     if (alwaysExpanded) return;
     setExpanded(!expanded);
+    const card = ref.current;
+
+    if (card && expanded) {
+      card.scrollIntoView();
+    }
   };
 
   const toggleReadMoreKeyDown = (e) => {
@@ -101,8 +106,6 @@ const ReportCard = ({ item, className = '', incidentId, alwaysExpanded = false }
           observer.unobserve(card);
         }
       };
-    } else {
-      card.scrollIntoView();
     }
   }, [expanded]);
 
