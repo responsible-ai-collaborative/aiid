d8be6c687614da90220ee464f1570bf098916d91
Pablo Costa <pablo@botsfactory.io>
2023-06-08 11:55:44 -0300

    Update migration: New `epoch_date_modified` field on `submissions` collection

--- site/gatsby-site/migrations/2023.06.06T18.15.45.create-history-database.js
+++ site/gatsby-site/migrations/2023.06.06T18.15.45.create-history-database.js
@@ -70,4 +70,32 @@ exports.up = async ({ context: { client } }) => {
 
     await incidentsHistoryCollection.insertOne(incident);
   }
+
+  // New epoch_date_modified field on submissions collection
+  const submissionsCollection = client
+    .db(config.realm.production_db.db_name)
+    .collection('submissions');
+
+  const submissionsCursor = submissionsCollection.find({});
+
+  while (await submissionsCursor.hasNext()) {
+    const submission = await submissionsCursor.next();
+
+    const epoch_date_modified = getUnixTime(new Date(submission.date_modified));
+
+    console.log(
+      `Updating submission ${submission._id} with epoch_date_modified "${epoch_date_modified}"`
+    );
+
+    await submissionsCollection.updateOne(
+      { _id: submission._id },
+      {
+        $set: {
+          epoch_date_modified: epoch_date_modified,
+        },
+      }
+    );
+
+    submission.epoch_date_modified = epoch_date_modified;
+  }
 };
