f2f77ed4a662e79b269c4470233f1a0f49f0ce93
Clara Youdale <clara@botsfactory.io>
2023-06-19 16:16:24 -0300

    Feature/incidents carousel (#2020)
    * Query latest 5 reports
    
    * Add latest incident reports carousel of 5 elements
    
    * Only use lates incident reports query
    
    * Add ordering to fetch latest incidents reports
    
    * Add auto slide
    
    * Add more visible arrows to carousel
    
    * Only fetch reports from different incidents
    
    * Use same arrows on all carousels
    
    * Increase report limit

--- site/gatsby-site/src/components/landing/LatestReports.js
+++ site/gatsby-site/src/components/landing/LatestReports.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import LatestIncidentReport from 'components/landing/LatestIncidentReport';
+import { Card, Carousel } from 'flowbite-react';
+import { Trans } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
 
-export default function LatestReports({ latestReport }) {
+export default function LatestReports({ latestReports }) {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <LatestIncidentReport report={latestReport} />
-      </div>
+      <Card>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900  dark:text-white w-full hover:text-primary-blue">
+          <Trans ns="landing">Latest Incident Reports</Trans>
+        </h5>
+        <Carousel
+          className="latest-reports-carousel"
+          slideInterval={5000}
+          leftControl={
+            <FontAwesomeIcon
+              icon={faArrowCircleLeft}
+              className="h-8 w-8 text-white bg-gray-500 shadow rounded-full"
+            />
+          }
+          rightControl={
+            <FontAwesomeIcon
+              icon={faArrowCircleRight}
+              className="h-8 w-8 text-white bg-gray-500 shadow rounded-full"
+            />
+          }
+        >
+          {latestReports.map((report) => (
+            <LatestIncidentReport report={report} key={`latest-report-${report.title}`} />
+          ))}
+        </Carousel>
+      </Card>
     </>
   );
 }
