fea623a9a1a15ac8a8213a39281a79a12f70824f
Pablo Costa <pablo@botsfactory.io>
2023-06-21 16:55:47 -0300

    Update migration to don't use `editor` on `incident` and `reports`

--- site/gatsby-site/migrations/2023.06.06T18.15.45.create-history-database.js
+++ site/gatsby-site/migrations/2023.06.06T18.15.45.create-history-database.js
@@ -11,91 +11,45 @@ function getUnixTime(dateString) {
 exports.up = async ({ context: { client } }) => {
   await client.connect();
 
-  // Clone Reports collection from "aiidprod" to "history" DB
+  // Create "reports" collection on "history" DB
   const reportsCollection = client.db(config.realm.production_db.db_name).collection('reports');
 
   const reportsHistoryCollection = client
     .db(config.realm.production_db.db_history_name)
     .collection('reports');
 
-  const reportsCursor = reportsCollection.find({});
+  const reports = await reportsCollection.find({}, { limit: 1 }).toArray();
 
-  while (await reportsCursor.hasNext()) {
-    const report = await reportsCursor.next();
+  const report = reports[0];
 
-    const editor = report.submitters && report.submitters.length > 0 ? report.submitters[0] : '';
+  const reportEditor =
+    report.submitters && report.submitters.length > 0 ? report.submitters[0] : '';
 
-    console.log(`Updating report ${report.report_number} with editor "${editor}"`);
+  console.log(`Inserting report ${report.report_number} with editor "${reportEditor}"`);
 
-    await reportsCollection.updateOne(
-      { report_number: report.report_number },
-      { $set: { editor: editor } }
-    );
+  report.editor = reportEditor;
 
-    report.editor = editor;
+  await reportsHistoryCollection.insertOne(report);
 
-    await reportsHistoryCollection.insertOne(report);
-  }
-
-  // Clone Incidents collection from "aiidprod" to "history" DB
+  // Create "incidents" collection on "history" DB
   const incidentsCollection = client.db(config.realm.production_db.db_name).collection('incidents');
 
   const incidentsHistoryCollection = client
     .db(config.realm.production_db.db_history_name)
     .collection('incidents');
 
-  const incidentsCursor = incidentsCollection.find({});
+  const incidents = await incidentsCollection.find({}, { limit: 1 }).toArray();
 
-  while (await incidentsCursor.hasNext()) {
-    const incident = await incidentsCursor.next();
+  const incident = incidents[0];
 
-    const editor = incident.editors && incident.editors.length > 0 ? incident.editors[0] : '';
+  const incidentEditor = incident.editors && incident.editors.length > 0 ? incident.editors[0] : '';
 
-    const epoch_date_modified = getUnixTime(new Date(incident.date));
+  const epoch_date_modified = getUnixTime(new Date(incident.date));
 
-    console.log(`Updating incident ${incident.incident_id} with editor "${editor}"`);
+  console.log(`Inserting incident ${incident.incident_id} with editor "${incidentEditor}"`);
 
-    await incidentsCollection.updateOne(
-      { incident_id: incident.incident_id },
-      {
-        $set: {
-          editor: editor,
-          epoch_date_modified: epoch_date_modified,
-        },
-      }
-    );
+  incident.editor = incidentEditor;
+  incident.epoch_date_modified = epoch_date_modified;
 
-    incident.editor = editor;
-    incident.epoch_date_modified = epoch_date_modified;
-
-    await incidentsHistoryCollection.insertOne(incident);
-  }
-
-  // New epoch_date_modified field on submissions collection
-  const submissionsCollection = client
-    .db(config.realm.production_db.db_name)
-    .collection('submissions');
-
-  const submissionsCursor = submissionsCollection.find({});
-
-  while (await submissionsCursor.hasNext()) {
-    const submission = await submissionsCursor.next();
-
-    const epoch_date_modified = getUnixTime(new Date(submission.date_modified));
-
-    console.log(
-      `Updating submission ${submission._id} with epoch_date_modified "${epoch_date_modified}"`
-    );
-
-    await submissionsCollection.updateOne(
-      { _id: submission._id },
-      {
-        $set: {
-          epoch_date_modified: epoch_date_modified,
-        },
-      }
-    );
-
-    submission.epoch_date_modified = epoch_date_modified;
-  }
+  await incidentsHistoryCollection.insertOne(incident);
 };
