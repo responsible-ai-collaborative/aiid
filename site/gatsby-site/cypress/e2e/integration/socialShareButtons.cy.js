544ec609f39179a6c3dd8346801f714acc1f6dff
Cesar Varela <mail@cesarvarela.com>
2023-07-03 15:15:00 -0300

    Twitter now asks to login first (#2137)
    Co-authored-by: Sean McGregor <gitcommit@seanbmcgregor.com>

--- site/gatsby-site/cypress/e2e/integration/socialShareButtons.cy.js
+++ site/gatsby-site/cypress/e2e/integration/socialShareButtons.cy.js
@@ -23,7 +23,7 @@ describe('Social Share buttons on pages', { retries: { runMode: 4 } }, () => {
     },
   ];
 
-  urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
+  urlsToTest.forEach(({ page, url, shareButtonSections }) => {
     it(`${page} page should have ${shareButtonSections} Social Share button sections`, () => {
       cy.visit(url);
 
@@ -51,10 +51,6 @@ describe('Social Share buttons on pages', { retries: { runMode: 4 } }, () => {
 
       cy.get('[data-cy=btn-share-twitter]').first().click();
       cy.get('@popup_twitter', { timeout: 8000 }).should('be.called');
-      cy.url().should(
-        'contain',
-        `https://twitter.com/intent/tweet?text=${encodeURI(title)}&url=${canonicalUrl}`
-      );
     });
 
     // LinkedIn share
