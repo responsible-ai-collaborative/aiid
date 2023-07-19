85da0182f3f0093492eb202044ec58d2d6f9cca9
Cesar Varela <mail@cesarvarela.com>
2023-07-18 23:45:27 -0300

    Refactor to maybe fix hydration issue

--- site/gatsby-site/src/components/ui/Header.js
+++ site/gatsby-site/src/components/ui/Header.js
@@ -11,10 +11,123 @@ import {
 import LoginSignup from 'components/loginSignup';
 import logoImg from '../images/logo.svg';
 import Link from './Link';
-import config from '../../../config.js';
 
 import Sidebar from '../sidebar';
 import LanguageSwitcher from 'components/i18n/LanguageSwitcher';
+
+const Divider = ({ className = '' }) => (
+  <span className={`divider hidden md:inline-block w-px h-[30px] bg-gray-400 ${className}`} />
+);
+
+const SocialMediaIcons = ({ githubUrl, facebookUrl, linkedInUrl }) => {
+  return (
+    <div className="hidden md:flex wrap-0 gap-2 items-center">
+      <a href={'https://twitter.com/IncidentsDB'} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon
+          titleId="twitter"
+          icon={faTwitterSquare}
+          color={'white'}
+          className="pointer fa fa-twitter-square fa-lg"
+          title="Open Twitter"
+        />
+      </a>
+      <a href={'/rss.xml'} target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon
+          titleId="rss"
+          icon={faRssSquare}
+          color={'white'}
+          className="pointer fa fa-rss-square fa-lg"
+          title="Open RSS Feed"
+        />
+      </a>
+      <a
+        className="paddingAround hiddenMobile"
+        href={facebookUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          titleId="facebook"
+          icon={faFacebookSquare}
+          color={'white'}
+          className="pointer fa fa-rss-square fa-lg"
+          title="Open RSS Feed"
+        />
+      </a>
+      <a
+        className="paddingAround hiddenMobile"
+        href={linkedInUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          titleId="linkedin"
+          icon={faLinkedin}
+          color={'white'}
+          className="pointer fa fa-rss-square fa-lg"
+          title="Open RSS Feed"
+        />
+      </a>
+      <a href={githubUrl} target="_blank" rel="noreferrer" className="pr-2">
+        <FontAwesomeIcon
+          titleId="github"
+          icon={faGithubSquare}
+          color={'white'}
+          className="pointer fa fa-github-square fa-lg -mr-2"
+          title="Open GitHub"
+        />
+      </a>
+    </div>
+  );
+};
+
+const SkipToContent = ({ className }) => (
+  <a
+    href="#content"
+    className={`
+      ${className}
+      relative 
+      overflow-hidden
+      text-white
+      bg-transparent 
+      text-xs
+      opacity-0 focus:opacity-100
+      w-0       focus:w-[unset]
+      h-0       focus:h-[unset]
+                focus:px-[1ch]   
+    `}
+  >
+    Skip to Content
+  </a>
+);
+
+const HeaderLink = ({ className, headerTitle, logo }) => {
+  const finalLogoLink = logo.link !== '' ? logo.link : 'https://incidentdatabase.ai/';
+
+  return (
+    <Link to={finalLogoLink} className={`hover:no-underline flex items-center ${className}`}>
+      <div className="md:w-64 text-center">
+        <img
+          className={'hidden md:inline ml-[10px] mr-[10px] w-[200px]'}
+          src={logo.image !== '' ? logo.image : logoImg}
+          alt={'logo'}
+          loading="lazy"
+        />
+        <img
+          className="md:hidden w-[50px]"
+          src={logo.mobile !== '' ? logo.mobile : logoImg}
+          alt={'logo'}
+          loading="lazy"
+        />
+      </div>
+      <Divider />
+      <span
+        className="inline-block ml-4 md:ml-10 font-semibold text-xs  md:text-base md:uppercase"
+        dangerouslySetInnerHTML={{ __html: headerTitle }}
+      />
+    </Link>
+  );
+};
 
 const Header = ({ location = null }) => {
   const [navCollapsed, setNavCollapsed] = useState(true);
@@ -22,7 +135,7 @@ const Header = ({ location = null }) => {
   return (
     <StaticQuery
       query={graphql`
-        query headerTitleQuery {
+        query {
           site {
             siteMetadata {
               headerTitle
@@ -47,119 +160,12 @@ const Header = ({ location = null }) => {
           },
         } = data;
 
-        const finalLogoLink = logo.link !== '' ? logo.link : 'https://incidentdatabase.ai/';
-
-        var SocialMediaIcons = () =>
-          config.header.social && (
-            <div className="hidden md:flex wrap-0 gap-2 items-center">
-              <a href={'https://twitter.com/IncidentsDB'} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon
-                  titleId="twitter"
-                  icon={faTwitterSquare}
-                  color={'white'}
-                  className="pointer fa fa-twitter-square fa-lg"
-                  title="Open Twitter"
-                />
-              </a>
-              <a href={'/rss.xml'} target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon
-                  titleId="rss"
-                  icon={faRssSquare}
-                  color={'white'}
-                  className="pointer fa fa-rss-square fa-lg"
-                  title="Open RSS Feed"
-                />
-              </a>
-              <a
-                className="paddingAround hiddenMobile"
-                href={facebookUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon
-                  titleId="facebook"
-                  icon={faFacebookSquare}
-                  color={'white'}
-                  className="pointer fa fa-rss-square fa-lg"
-                  title="Open RSS Feed"
-                />
-              </a>
-              <a
-                className="paddingAround hiddenMobile"
-                href={linkedInUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon
-                  titleId="linkedin"
-                  icon={faLinkedin}
-                  color={'white'}
-                  className="pointer fa fa-rss-square fa-lg"
-                  title="Open RSS Feed"
-                />
-              </a>
-              <a href={githubUrl} target="_blank" rel="noreferrer" className="pr-2">
-                <FontAwesomeIcon
-                  titleId="github"
-                  icon={faGithubSquare}
-                  color={'white'}
-                  className="pointer fa fa-github-square fa-lg -mr-2"
-                  title="Open GitHub"
-                />
-              </a>
-            </div>
-          );
-
-        var HeaderLink = ({ className }) => (
-          <Link to={finalLogoLink} className={`hover:no-underline flex items-center ${className}`}>
-            <div className="md:w-64 text-center">
-              <img
-                className={'hidden md:inline ml-[10px] mr-[10px] w-[200px]'}
-                src={logo.image !== '' ? logo.image : logoImg}
-                alt={'logo'}
-                loading="lazy"
-              />
-              <img
-                className="md:hidden w-[50px]"
-                src={logo.mobile !== '' ? logo.mobile : logoImg}
-                alt={'logo'}
-                loading="lazy"
-              />
-            </div>
-            <Divider />
-            <span
-              className="inline-block ml-4 md:ml-10 font-semibold text-xs  md:text-base md:uppercase"
-              dangerouslySetInnerHTML={{ __html: headerTitle }}
-            />
-          </Link>
-        );
-
-        var SkipToContent = ({ className }) => (
-          <a
-            href="#content"
-            className={`
-              ${className}
-              relative 
-              overflow-hidden
-              text-white
-              bg-transparent 
-              text-xs
-              opacity-0 focus:opacity-100
-              w-0       focus:w-[unset]
-              h-0       focus:h-[unset]
-                        focus:px-[1ch]   
-            `}
-          >
-            Skip to Content
-          </a>
-        );
-
         return (
           <nav id="navBarDefault" className="bg-[#001934] shadow">
             <div className=" text-white flex flex-row items-center w-full p-4 md:pl-0 h-[80px]">
               <SkipToContent className="-order-1 mx-2" />
 
-              <HeaderLink className="-order-3" />
+              <HeaderLink className="-order-3" headerTitle={headerTitle} logo={logo} />
 
               <div className="mx-auto -order-2" />
 
@@ -167,7 +173,7 @@ const Header = ({ location = null }) => {
 
               <Divider className="mx-4" />
 
-              <SocialMediaIcons />
+              <SocialMediaIcons {...{ facebookUrl, githubUrl, linkedInUrl }} />
 
               <div className="block md:hidden">
                 <FontAwesomeIcon
@@ -203,9 +209,5 @@ const Header = ({ location = null }) => {
     />
   );
 };
-
-var Divider = ({ className }) => (
-  <span className={`divider hidden md:inline-block w-px h-[30px] bg-gray-400 ${className}`} />
-);
 
 export default Header;
