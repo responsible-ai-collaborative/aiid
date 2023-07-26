739cd9c45626f45f198e92b997d834c0c0874418
Pablo Costa <pablo@botsfactory.io>
2023-07-25 22:09:35 -0300

    Refactor reports to use LogHistoryContext

--- site/gatsby-site/src/contexts/LogHistoryContext.js
+++ site/gatsby-site/src/contexts/LogHistoryContext.js
@@ -1,11 +1,19 @@
 import React, { createContext, useContext } from 'react';
 import { useMutation } from '@apollo/client/react/hooks';
 import { transformIncidentData } from '../utils/cite';
+import { transformReportData } from '../utils/reports';
 import { LOG_INCIDENT_HISTORY } from '../graphql/incidents';
+import { LOG_REPORT_HISTORY } from '../graphql/reports';
+import { stripMarkdown } from '../utils/typography';
 
 const LogHistoryContext = createContext({
   logIncidentHistory(incident, user) {
     incident;
+    user;
+  },
+  logReportHistory(report, updates, user) {
+    report;
+    updates;
     user;
   },
 });
@@ -21,10 +29,31 @@ export function LogHistoryContextProvider({ children }) {
     await logIncidentHistoryMutation({ variables: { input: updatedIncident } });
   };
 
+  const [logReportHistoryMutation] = useMutation(LOG_REPORT_HISTORY);
+
+  const logReportHistory = async (report, updates, user) => {
+    const transformedReport = transformReportData(report, user);
+
+    const updatedReport = {
+      ...transformedReport,
+      ...updates,
+    };
+
+    if (updates.text) {
+      updatedReport.plain_text = await stripMarkdown(updates.text);
+    }
+
+    // Set the user as the last modifier
+    updatedReport.modifiedBy = user && user.providerType != 'anon-user' ? user.id : '';
+
+    await logReportHistoryMutation({ variables: { input: updatedReport } });
+  };
+
   return (
     <LogHistoryContext.Provider
       value={{
         logIncidentHistory,
+        logReportHistory,
       }}
     >
       {children}
