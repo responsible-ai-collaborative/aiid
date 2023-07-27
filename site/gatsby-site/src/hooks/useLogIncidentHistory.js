cfe05caa39deef8b55f02f1aa30aeb3a14770d02
Pablo Costa <pablo@botsfactory.io>
2023-07-27 18:20:33 -0300

    Remove commented code

--- site/gatsby-site/src/hooks/useLogIncidentHistory.js
+++ site/gatsby-site/src/hooks/useLogIncidentHistory.js
@@ -13,33 +13,3 @@ export function useLogIncidentHistory() {
 
   return { logIncidentHistory };
 }
-
-// export function LogHistoryContextProvider({ children }) {
-//   const [logIncidentHistoryMutation] = useMutation(LOG_INCIDENT_HISTORY);
-
-//   const logIncidentHistory = async (incident, user) => {
-//     const updatedIncident = transformIncidentData(incident, user);
-
-//     await logIncidentHistoryMutation({ variables: { input: updatedIncident } });
-//   };
-
-//   const [logReportHistoryMutation] = useMutation(LOG_REPORT_HISTORY);
-
-//   const logReportHistory = async (report, updates, user) => {
-//     const transformedReport = transformReportData(report, user);
-
-//     const updatedReport = {
-//       ...transformedReport,
-//       ...updates,
-//     };
-
-//     if (updates.text) {
-//       updatedReport.plain_text = await stripMarkdown(updates.text);
-//     }
-
-//     // Set the user as the last modifier
-//     updatedReport.modifiedBy = user && user.providerType != 'anon-user' ? user.id : '';
-
-//     await logReportHistoryMutation({ variables: { input: updatedReport } });
-//   };
-// }
