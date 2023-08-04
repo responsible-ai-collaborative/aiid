71b60794763e3330459eca6066b818161b19271d
Cesar Varela <mail@cesarvarela.com>
2023-08-04 17:21:55 -0300

    Add cypress coverage support

--- site/gatsby-site/cypress.config.js
+++ site/gatsby-site/cypress.config.js
@@ -14,7 +14,11 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      require('@cypress/code-coverage/task')(on, config);
+
+      require('./cypress/plugins/index.js')(on, config);
+
+      return config;
     },
     baseUrl: 'http://localhost:8000/',
   },
