162d8f0e6895334c73360ff07efb2b5b40fefd0d
Cesar Varela <mail@cesarvarela.com>
2023-09-04 18:02:37 -0300

    Add taxonomy description to taxonomy modal

--- site/gatsby-site/src/components/systems/AddTaxonomyModal.js
+++ site/gatsby-site/src/components/systems/AddTaxonomyModal.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
 
 const FIND_TAXONOMY_NAMES = gql`
   {
@@ -32,31 +33,48 @@ export default function AddTaxonomyModal({ onClose, onTaxonomySelected }) {
       </Modal.Header>
       <Modal.Body>
         {loading && <Spinner />}
-        {!loading && data?.taxas && (
-          <Dropdown
-            label={selectedTaxonomy || t('Select a taxonomy')}
-            color={'light'}
-            className="min-w-max mr-4"
-          >
-            {data.taxas.map(({ namespace }) => (
-              <Dropdown.Item
-                key={namespace}
-                onClick={() => setSelectedTaxonomy(namespace)}
-                className={`${namespace === selectedTaxonomy ? 'bg-blue-100' : ''}`}
-              >
-                <span>{namespace}</span>
-              </Dropdown.Item>
-            ))}
-          </Dropdown>
-        )}
 
+        {!loading && data?.taxas && (
+          <>
+            <Dropdown
+              label={selectedTaxonomy || t('Select a taxonomy')}
+              color={'light'}
+              className="min-w-max mr-4"
+            >
+              {data.taxas.map(({ namespace }) => (
+                <Dropdown.Item
+                  key={namespace}
+                  onClick={() => setSelectedTaxonomy(namespace)}
+                  className={`${namespace === selectedTaxonomy ? 'bg-blue-100' : ''}`}
+                >
+                  <span>{namespace}</span>
+                </Dropdown.Item>
+              ))}
+            </Dropdown>
+            {data.taxas
+              .filter(({ namespace }) => namespace === selectedTaxonomy)
+              .map(({ namespace, description }) => (
+                <div
+                  key={namespace}
+                  className="mt-2 max-h-240 overflow-y-scroll border rounded-sm p-2"
+                >
+                  <ReactMarkdown className="react-markdown prose max-w-full">
+                    {description}
+                  </ReactMarkdown>
+                </div>
+              ))}
+          </>
+        )}
+      </Modal.Body>
+
+      <Modal.Footer>
         <Button
           onClick={() => handleClickAddTaxonomy(selectedTaxonomy)}
           disabled={!selectedTaxonomy}
         >
           <Trans>Add</Trans>
         </Button>
-      </Modal.Body>
+      </Modal.Footer>
     </Modal>
   );
 }
