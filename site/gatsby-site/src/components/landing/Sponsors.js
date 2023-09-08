99b7b9f60e74bd509ab7645fbde360ac3f6698ef
Clara Youdale <clara@botsfactory.io>
2023-09-08 16:40:27 -0300

    Fix sponsors modal with Prismic

--- site/gatsby-site/src/components/landing/Sponsors.js
+++ site/gatsby-site/src/components/landing/Sponsors.js
@@ -144,11 +144,11 @@ export default function Sponsors({ sponsors = [] }) {
                           <div
                             key={`sponsor-item-${item.name}`}
                             className="flex-1 max-w-xs w-full max-h-[90px] ml-0 mr-0 text-center"
-                            data-cy={`${item.name.text}-image`}
+                            data-cy={`${item.name}-image`}
                           >
                             <StyledImage
                               src={`${item.logo?.url ? item.logo.url : '/images/' + item.logo}`}
-                              onClick={() => setModalState(item.name.text)}
+                              onClick={() => setModalState(item.name)}
                               data-cy={`${item.name}-modal-click`}
                               className="max-h-[90px] ml-0 mr-0 mb-0 inline-flex"
                             />
