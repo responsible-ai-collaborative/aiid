284ac149c03665c660c9ec9183b0143a15dee6b0
Pablo Costa <pablo@botsfactory.io>
2023-09-18 16:59:31 -0300

    Assume that the default user already exists in the database

--- site/gatsby-site/migrations/2023.09.14T18.44.03.create-default-user.js
+++ site/gatsby-site/migrations/2023.09.14T18.44.03.create-default-user.js
@@ -1,5 +1,3 @@
-const { ObjectId } = require('bson');
-
 const config = require('../config');
 
 const userId = '65031f49ec066d7c64380f5c'; // Default user. For more information refer to the wiki page: https://github.com/responsible-ai-collaborative/aiid/wiki/Special-non%E2%80%90secret-values
@@ -10,45 +8,34 @@ const userId = '65031f49ec066d7c64380f5c'; // Default user. For more information
  */
 
 exports.up = async ({ context: { client } }) => {
-  const first_name = 'Anonymous';
+    const first_name = 'Anonymous';
 
-  const last_name = 'User';
+    const last_name = 'User';
 
-  await client.connect();
+    await client.connect();
 
-  const usersCollection = client.db(config.realm.production_db.db_custom_data).collection('users');
+    const usersCollection = client.db(config.realm.production_db.db_custom_data).collection('users');
 
-  const defaultUser = await usersCollection.findOne({ userId: userId });
+    const defaultUser = await usersCollection.findOne({ userId: userId });
 
-  if (defaultUser) {
-    // If the default user already exist in the database, we update the first and last name
-    const updateResult = await usersCollection.updateOne(
-      { _id: defaultUser._id },
-      {
-        $set: {
-          first_name,
-          last_name,
-        },
-      }
-    );
+    if (defaultUser) {
+        // If the default user already exist in the database, we update the first and last name
+        const updateResult = await usersCollection.updateOne(
+            { _id: defaultUser._id },
+            {
+                $set: {
+                    first_name,
+                    last_name,
+                },
+            }
+        );
 
-    console.log(
-      `Update userId: ${defaultUser.userId}, with '${first_name} ${last_name}' name, ${updateResult.modifiedCount}`
-    );
-  } else {
-    // if the default user does not exist in the database, we insert it
-    const insertResult = await usersCollection.insertOne({
-      _id: new ObjectId(userId),
-      userId: userId,
-      roles: ['subscriber'],
-      first_name,
-      last_name,
-    });
-
-    console.log(
-      `Insert userId: ${userId}, with '${first_name} ${last_name}' name, ${insertResult.insertedId}`
-    );
-  }
+        console.log(
+            `Update userId: '${defaultUser.userId}', with '${first_name} ${last_name}' name, ${updateResult.modifiedCount}`
+        );
+    } else {
+        console.log(`User with userId '${userId}' not found`);
+    }
 };
 
 /**
@@ -56,24 +43,24 @@ exports.up = async ({ context: { client } }) => {
  * @param {{context: {client: import('mongodb').MongoClient}}} context
  */
 exports.down = async ({ context: { client } }) => {
-  await client.connect();
+    await client.connect();
 
-  const usersCollection = client.db(config.realm.production_db.db_custom_data).collection('users');
+    const usersCollection = client.db(config.realm.production_db.db_custom_data).collection('users');
 
-  const defaultUser = await usersCollection.findOne({ userId: userId });
+    const defaultUser = await usersCollection.findOne({ userId: userId });
 
-  if (defaultUser) {
-    // Unset the first and last name
-    const updateResult = await usersCollection.updateOne(
-      { _id: defaultUser._id },
-      {
-        $unset: {
-          first_name: '',
-          last_name: '',
-        },
-      }
-    );
+    if (defaultUser) {
+        // Unset the first and last name
+        const updateResult = await usersCollection.updateOne(
+            { _id: defaultUser._id },
+            {
+                $unset: {
+                    first_name: '',
+                    last_name: '',
+                },
+            }
+        );
 
-    console.log(`Rollback userId: ${defaultUser.userId}, ${updateResult.modifiedCount}`);
-  }
+        console.log(`Rollback userId: ${defaultUser.userId}, ${updateResult.modifiedCount}`);
+    }
 };
