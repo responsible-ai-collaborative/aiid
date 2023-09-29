695d0703410d25e41b9fa4f9ba667cb67c4477e1
Pablo Costa <pablo@botsfactory.io>
2023-09-29 19:59:25 -0300

    Add test to the empty Classifications table

--- site/gatsby-site/cypress/e2e/integration/apps/classifications.cy.js
+++ site/gatsby-site/cypress/e2e/integration/apps/classifications.cy.js
@@ -1,17 +1,15 @@
-import { maybeIt } from '../../../support/utils';
+import { conditionalIt } from '../../../support/utils';
 import taxa from '../../../fixtures/call/taxa.json';
 import classifications from '../../../fixtures/call/classifications.json';
 
 describe('Classifications App', () => {
   const url = '/apps/classifications';
 
-  before('before', function () {
-    Cypress.env('isEmptyEnvironment') && this.skip();
+  it('Should successfully load', () => {
+    cy.visit(url);
   });
 
-  maybeIt('Successfully edit a CSET classification', () => {
-    cy.login(Cypress.env('e2eUsername'), Cypress.env('e2ePassword'));
-
+  it('Should successfully load the table', () => {
     cy.conditionalIntercept(
       '**/graphql',
       (req) => req.body.operationName == 'FindTaxa',
@@ -30,14 +28,44 @@ describe('Classifications App', () => {
 
     cy.wait(['@FindTaxa', '@FindClassifications']);
 
-    cy.get('select[data-cy="taxonomy"]').select('CSET');
-
     cy.waitForStableDOM();
 
-    cy.get('a[href="/cite/2/?edit_taxonomy=CSET').click();
+    cy.get('[data-cy="row"]').should('have.length', 2);
   });
 
-  it('Should switch taxonomies', () => {
+  conditionalIt(
+    !Cypress.env('isEmptyEnvironment'),
+    'Successfully edit a CSET classification',
+    () => {
+      cy.login(Cypress.env('e2eUsername'), Cypress.env('e2ePassword'));
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'FindTaxa',
+        'FindTaxa',
+        taxa
+      );
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'FindClassifications',
+        'FindClassifications',
+        classifications
+      );
+
+      cy.visit(url);
+
+      cy.wait(['@FindTaxa', '@FindClassifications']);
+
+      cy.get('select[data-cy="taxonomy"]').select('CSET');
+
+      cy.waitForStableDOM();
+
+      cy.get('a[href="/cite/2/?edit_taxonomy=CSET').click();
+    }
+  );
+
+  conditionalIt(!Cypress.env('isEmptyEnvironment'), 'Should switch taxonomies', () => {
     cy.login(Cypress.env('e2eUsername'), Cypress.env('e2ePassword'));
 
     cy.conditionalIntercept(
