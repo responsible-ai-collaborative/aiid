3a75a2232b5b1252f9bb5b732392fee9919d1a21
Pablo Costa <pablo@botsfactory.io>
2023-09-29 13:15:00 -0300

    Move RSS tests to a separate `rss.cy.js` file

--- site/gatsby-site/cypress/e2e/integration/pages.cy.js
+++ site/gatsby-site/cypress/e2e/integration/pages.cy.js
@@ -1,5 +1,4 @@
 import { switchLocalizedPath } from '../../../i18n';
-import { conditionalIt } from '../../support/utils';
 import config from '../../../config';
 
 describe('Pages', () => {
@@ -150,40 +149,6 @@ describe('Pages', () => {
               cy.log(`No images found on page, skipping image test for path ${path}`);
             }
           });
-      });
-    });
-  });
-
-  conditionalIt(!Cypress.env('isEmptyEnvironment'), 'Should generate a valid RSS feed', () => {
-    cy.request('/rss.xml').then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.headers['content-type']).to.include('application/xml');
-
-      const xml = response.body;
-
-      const parsedXml = Cypress.$.parseXML(xml);
-
-      const hasChannelTag = Cypress.$(parsedXml).find('channel').length > 0;
-
-      expect(hasChannelTag).to.be.true;
-
-      const items = Cypress.$(parsedXml).find('channel > item').slice(0, 20);
-
-      expect(items.length).to.be.greaterThan(0);
-
-      items.each((_index, item) => {
-        const description = Cypress.$(item).find('description');
-
-        const title = Cypress.$(item).find('title');
-
-        const pubDate = Cypress.$(item).find('pubDate');
-
-        const guid = Cypress.$(item).find('guid');
-
-        expect(description.length).to.equal(1);
-        expect(title.length).to.equal(1);
-        expect(pubDate.length).to.equal(1);
-        expect(guid.length).to.equal(1);
       });
     });
   });
