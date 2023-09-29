d9f19a7c647e4ef8deb431f1ee0f57cb6bd68d63
Cesar Varela <mail@cesarvarela.com>
2023-09-29 16:24:04 -0300

    Add tests

--- site/gatsby-site/cypress/e2e/integration/report.cy.js
+++ site/gatsby-site/cypress/e2e/integration/report.cy.js
@@ -7,6 +7,18 @@ describe('Report pages', () => {
     cy.visit(url);
   });
 
+  it('Should Display associated Incidents', () => {
+    cy.visit(`/reports/1`);
+
+    cy.contains('Associated Incidents').should('be.visible');
+
+    cy.contains('Google’s YouTube Kids App Presents Inappropriate Content')
+      .should('be.visible')
+      .should('have.attr', 'href', '/cite/1/');
+
+    cy.get('[data-cy="incident-report-card"]').should('have.length', 1);
+  });
+
   it('Should always be expanded', () => {
     cy.visit(url);
 
