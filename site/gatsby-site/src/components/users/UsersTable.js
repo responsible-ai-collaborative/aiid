1b4c41f3529617b824b834f4dba918ff0f5b4530
Clara Youdale <clara@botsfactory.io>
2023-10-13 14:57:47 -0300

    Fetch each user data on load

--- site/gatsby-site/src/components/users/UsersTable.js
+++ site/gatsby-site/src/components/users/UsersTable.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFilters, usePagination, useSortBy, useTable } from 'react-table';
 import Table, { DefaultColumnFilter, DefaultColumnHeader } from 'components/ui/Table';
 import { Badge, Button } from 'flowbite-react';
 import UserEditModal from './UserEditModal';
 import { UserCreationDateCell, UserEmailCell, UserLastAuthDateCell } from './UserInfoCells';
+import { useApolloClient } from '@apollo/client';
+import { FIND_USER } from '../../graphql/users';
 
 function RolesCell({ cell }) {
   return (
@@ -18,6 +20,8 @@ function RolesCell({ cell }) {
 export default function UsersTable({ data, className = '', ...props }) {
   const [userEditId, setUserEditId] = useState(null);
 
+  const [updatedData, setUpdatedData] = useState(data);
+
   const defaultColumn = React.useMemo(
     () => ({
       className: 'w-[120px]',
@@ -27,13 +31,71 @@ export default function UsersTable({ data, className = '', ...props }) {
     []
   );
 
+  const client = useApolloClient();
+
+  useEffect(() => {
+    const fetchUserAdminData = async () => {
+      if (data) {
+        data.forEach(async (user) => {
+          try {
+            const result = await client.query({
+              query: FIND_USER,
+              variables: { query: { userId: user.userId } },
+            });
+
+            if (result.data && result.data.user && result.data.user.adminData) {
+              const adminDataUserIndex = updatedData.findIndex(
+                (updatedUser) =>
+                  updatedUser.userId === result.data.user.userId && !updatedUser.adminData
+              );
+
+              if (adminDataUserIndex !== -1) {
+                const updatedNode = {
+                  ...updatedData[adminDataUserIndex],
+                  ...result.data.user,
+                };
+
+                setUpdatedData((prev) => {
+                  const updatedData = [...prev];
+
+                  updatedData[adminDataUserIndex] = updatedNode;
+                  return updatedData;
+                });
+              }
+            }
+          } catch (error) {
+            console.error('Error querying user admin data:', error);
+          }
+        });
+      }
+    };
+
+    fetchUserAdminData();
+  }, [data]);
+
   const columns = React.useMemo(() => {
     const columns = [
       {
         title: 'Email',
         accessor: 'adminData.email',
         className: 'min-w-[240px]',
-        Cell: ({ row: { values } }) => <UserEmailCell userId={values.userId} />,
+        Cell: ({ row: { values } }) => {
+          const user = updatedData.find((user) => user.userId === values.userId);
+
+          return <UserEmailCell email={user?.adminData?.email} />;
+        },
+        filter: (rows, id, filterValue) => {
+          return rows.filter((row) => {
+            const rowValue = row.values;
+
+            const user = updatedData.find((user) => user.userId === rowValue.userId);
+
+            if (user?.adminData?.email) {
+              return user.adminData.email.toLowerCase().includes(filterValue.toLowerCase());
+            }
+            return false;
+          });
+        },
       },
       {
         title: 'Id',
@@ -55,12 +117,24 @@ export default function UsersTable({ data, className = '', ...props }) {
       {
         title: 'Creation Date',
         accessor: 'adminData.creationDate',
-        Cell: ({ row: { values } }) => <UserCreationDateCell userId={values.userId} />,
+        Cell: ({ row: { values } }) => {
+          const user = updatedData.find((user) => user.userId === values.userId);
+
+          return <UserCreationDateCell creationDate={user?.adminData?.creationDate} />;
+        },
       },
       {
         title: 'Last Login Date',
         accessor: 'adminData.lastAuthenticationDate',
-        Cell: ({ row: { values } }) => <UserLastAuthDateCell userId={values.userId} />,
+        Cell: ({ row: { values } }) => {
+          const user = updatedData.find((user) => user.userId === values.userId);
+
+          return (
+            <UserLastAuthDateCell
+              lastAuthenticationDate={user?.adminData?.lastAuthenticationDate}
+            />
+          );
+        },
       },
       {
         id: 'actions',
@@ -79,7 +153,7 @@ export default function UsersTable({ data, className = '', ...props }) {
     ];
 
     return columns;
-  }, [setUserEditId]);
+  }, [setUserEditId, updatedData, setUpdatedData]);
 
   const table = useTable(
     {
