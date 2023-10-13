2dfe1f57cc34f68e03dfd5688189be3c4c8a8103
Clara Youdale <clara@botsfactory.io>
2023-10-13 16:44:47 -0300

    Use new reports query link to fetch leaderboards (#2327)
    * Use new reports query link to fetch leaderboards
    
    * Fix query for unique submitters
    
    * Fix original leaderboard query
    
    * Fix query to return accurate count

--- site/gatsby-site/src/components/leaderboards/UniqueSubmittersLeaderboard.js
+++ site/gatsby-site/src/components/leaderboards/UniqueSubmittersLeaderboard.js
@@ -4,24 +4,19 @@ import { Leaderboard } from './Leaderboard';
 
 const UniqueSubmittersLeaderboard = ({ limit = 0, className = '' }) => {
   const {
-    allMongodbAiidprodReports: { nodes: reports },
     allMongodbAiidprodIncidents: { nodes: incidents },
   } = useStaticQuery(graphql`
     {
-      allMongodbAiidprodIncidents {
+      allMongodbAiidprodIncidents(
+        filter: { reports: { elemMatch: { is_incident_report: { eq: true } } } }
+      ) {
         nodes {
           incident_id
           reports {
             report_number
+            submitters
+            date_submitted
           }
-        }
-      }
-
-      allMongodbAiidprodReports(filter: { is_incident_report: { eq: true } }) {
-        nodes {
-          report_number
-          submitters
-          date_submitted
         }
       }
     }
@@ -29,23 +24,21 @@ const UniqueSubmittersLeaderboard = ({ limit = 0, className = '' }) => {
 
   const submitters = {};
 
-  for (const report of reports) {
-    const { incident_id: id } = incidents.find((incident) => {
-      return incident.reports.some((r) => r.report_number === report.report_number);
+  incidents.forEach((incident) => {
+    incident.reports.forEach((report) => {
+      const {
+        submitters: [submitter],
+      } = report;
+
+      if (!submitters[submitter]) {
+        submitters[submitter] = [];
+      }
+
+      if (!submitters[submitter].includes(incident.incident_id)) {
+        submitters[submitter].push(incident.incident_id);
+      }
     });
-
-    const {
-      submitters: [submitter],
-    } = report;
-
-    if (!submitters[submitter]) {
-      submitters[submitter] = [];
-    }
-
-    if (!submitters[submitter].includes(id)) {
-      submitters[submitter].push(id);
-    }
-  }
+  });
 
   const hash = {};
 
