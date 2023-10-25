60562096131616abbce8a96f27beee8715fa645b
Pablo Costa <pablo@botsfactory.io>
2023-10-24 21:53:21 -0300

    Revert "Remove `gatsby-source-s3` plugin"
    This reverts commit 295f25839327e020ce0d0ad551d6fe567f3ccf72.

--- site/gatsby-site/gatsby-config.js
+++ site/gatsby-site/gatsby-config.js
@@ -221,6 +221,20 @@ const plugins = [
     },
   },
   {
+    resolve: `gatsby-source-s3`,
+    options: {
+      aws: {
+        // This AWS IAM user has been provisioned no permissions, but the plugin requires a user to
+        // get a listing of the public S3 bucket. User: backupindexpublic
+        accessKeyId: 'AKIA25BP4AERUFDGAJUJ',
+        secretAccessKeyId: 'backupindexpublic',
+        secretAccessKey: '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',
+      },
+      buckets: ['aiid-backups-public'],
+      // expiration: 120,
+    },
+  },
+  {
     resolve: `gatsby-theme-i18n`,
     options: {
       defaultLang: config.i18n.defaultLanguage,
