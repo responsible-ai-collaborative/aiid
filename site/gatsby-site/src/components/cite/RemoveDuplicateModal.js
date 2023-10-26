2ae7832854fc8130fc01f14d241b8fe7525799f5
Luna McNulty <l.mcnulty@riseup.net>
2023-10-26 19:07:28 -0400

    Transfer subscriptions

--- site/gatsby-site/src/components/cite/RemoveDuplicateModal.js
+++ site/gatsby-site/src/components/cite/RemoveDuplicateModal.js
@@ -3,6 +3,7 @@ import { useMutation, useQuery } from '@apollo/client/react/hooks';
 import { FIND_INCIDENT, UPDATE_INCIDENT } from '../../graphql/incidents';
 import { INSERT_DUPLICATE } from '../../graphql/duplicates';
 import { UPSERT_CLASSIFICATION, FIND_CLASSIFICATION } from '../../graphql/classifications';
+import { UPSERT_SUBSCRIPTION, FIND_FULL_SUBSCRIPTIONS } from '../../graphql/subscriptions';
 import IncidentsField from 'components/incidents/IncidentsField';
 import { Formik, Form } from 'formik';
 import { Button, Modal } from 'flowbite-react';
@@ -18,9 +19,16 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
 
   const [updateClassification] = useMutation(UPSERT_CLASSIFICATION);
 
+  const [updateSubscription] = useMutation(UPSERT_SUBSCRIPTION);
+
   const { data: classificationsData, loading: classificationsLoading } = useQuery(
     FIND_CLASSIFICATION,
     { variables: { query: { incidents: { incident_id: incident.incident_id } } } }
+  );
+
+  const { data: subscriptionsData, loading: subscriptionsLoading } = useQuery(
+    FIND_FULL_SUBSCRIPTIONS,
+    { variables: { query: { incident_id: { incident_id: incident.incident_id } } } }
   );
 
   return (
@@ -43,6 +51,8 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
               values.duplicateIncidentId.length == 0 ||
               classificationsLoading ||
               !classificationsData ||
+              !subscriptionsData ||
+              subscriptionsLoading ||
               isSubmitting;
 
             const submit = async () => {
@@ -51,12 +61,16 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
                 duplicateIncidentData.incident.reports.concat(incident.reports)
               ).map((report) => report.report_number);
 
+              const duplicate_incident_number = incident.incident_id;
+
+              const true_incident_number = values.duplicateIncidentId[0];
+
               try {
                 await insertDuplicate({
                   variables: {
                     duplicate: {
-                      duplicate_incident_number: incident.incident_id,
-                      true_incident_number: values.duplicateIncidentId[0],
+                      duplicate_incident_number,
+                      true_incident_number,
                     },
                   },
                 });
@@ -72,13 +86,13 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
               try {
                 await updateIncident({
                   variables: {
-                    query: { incident_id: values.duplicateIncidentId[0] },
+                    query: { incident_id: true_incident_number },
                     set: { reports: { link: reportIds } },
                   },
                 });
               } catch (e) {
                 addToast({
-                  message: `Could not transfer reports to incident ${values.duplicateIncidentId[0]}.`,
+                  message: `Could not transfer reports to incident ${true_incident_number}.`,
                   severity: SEVERITY.danger,
                   error: e,
                 });
@@ -94,8 +108,8 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
                         incidents: {
                           link: classification.incidents
                             .map((incident) => incident.incident_id)
-                            .filter((incident_id) => incident_id != incident.incident_id)
-                            .concat(values.duplicateIncidentId[0]),
+                            .filter((incident_id) => incident_id != duplicate_incident_number)
+                            .concat(true_incident_number),
                         },
                         reports: {
                           link: classification.reports.map((report) => report.report_number),
@@ -106,11 +120,32 @@ export default function RemoveDuplicateModal({ incident, show, onClose }) {
                 }
               } catch (e) {
                 addToast({
-                  message: `Could not transfer classifications to incident ${values.duplicateIncidentId[0]}.`,
+                  message: `Could not transfer classifications to incident ${true_incident_number}.`,
                   severity: SEVERITY.danger,
                   error: e,
                 });
               }
+              try {
+                for (const subscription of subscriptionsData.subscriptions) {
+                  await updateSubscription({
+                    variables: {
+                      query: { _id: subscription._id },
+                      subscription: {
+                        userId: { link: subscription.userId.userId },
+                        incident_id: { link: true_incident_number },
+                        type: subscription.type,
+                      },
+                    },
+                  });
+                }
+              } catch (e) {
+                addToast({
+                  message: `Could not transfer subscription to incident ${true_incident_number}.`,
+                  severity: SEVERITY.danger,
+                  error: e,
+                });
+              }
+
               setSubmitting(false);
               onClose();
               addToast({
