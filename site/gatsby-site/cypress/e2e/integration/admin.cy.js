897d3f910a93c9f19d36a6efd3a2af92767932e9
Clara Youdale <clara@botsfactory.io>
2023-10-31 16:57:26 -0300

    Fix admin test to use mock

--- site/gatsby-site/cypress/e2e/integration/admin.cy.js
+++ site/gatsby-site/cypress/e2e/integration/admin.cy.js
@@ -1,6 +1,5 @@
 import { conditionalIt } from '../../support/utils';
 import users from '../../fixtures/users/users.json';
-const { gql } = require('@apollo/client');
 
 describe('Admin', () => {
   const baseUrl = '/admin';
@@ -17,149 +16,161 @@ describe('Admin', () => {
     () => {
       cy.login(Cypress.env('e2eUsername'), Cypress.env('e2ePassword'));
 
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'FindUsers',
+        'findUsers',
+        users
+      );
+
+      users.data.users.forEach((user) => {
+        cy.conditionalIntercept(
+          '**/graphql',
+          (req) => {
+            return (
+              req.body.operationName == 'FindUser' &&
+              req.body?.variables?.query.userId == user.userId
+            );
+          },
+          'findUser',
+          { data: { user } }
+        );
+      });
+
       cy.visit(baseUrl);
 
-      cy.query({
-        query: gql`
-          {
-            users {
-              userId
-              roles
-              adminData {
-                email
-              }
-            }
-          }
-        `,
-        timeout: 60000, // mongodb admin api is extremely slow
-      }).then(({ data: { users } }) => {
-        cy.waitForStableDOM();
+      cy.waitForStableDOM();
 
-        for (const user of users.slice(0, 5)) {
-          cy.get('[data-cy="input-filter-Id"]').clear();
-          cy.get('[data-cy="input-filter-Id"]').type(user.userId);
+      cy.wait('@findUsers', { timeout: 60000 });
 
-          if (user.adminData.email) {
-            cy.contains('[data-cy="cell"]', user.userId)
-              .parent()
-              .within(() => {
-                cy.contains(user.adminData.email).scrollIntoView().should('be.visible');
+      cy.wait('@findUser');
 
-                for (const role of user.roles) {
-                  cy.contains(role).scrollIntoView().should('be.visible');
-                }
-              });
-          } else {
-            cy.contains('[data-cy="cell"]', user.userId)
-              .parent()
-              .within(() => {
-                cy.contains('Not found').scrollIntoView().should('be.visible');
-
-                for (const role of user.roles) {
-                  cy.contains(role).scrollIntoView().should('be.visible');
-                }
-              });
-          }
-        }
-
-        const user = users.find((user) => user.adminData.email == Cypress.env('e2eUsername'));
-
+      for (const user of users.data.users) {
         cy.get('[data-cy="input-filter-Id"]').clear();
         cy.get('[data-cy="input-filter-Id"]').type(user.userId);
 
-        cy.contains('[data-cy="cell"]', user.adminData.email)
-          .parent()
-          .within(() => {
-            cy.contains('Edit').click();
-          });
+        if (user.adminData.email) {
+          cy.contains('[data-cy="cell"]', user.userId)
+            .parent()
+            .within(() => {
+              cy.contains(user.adminData.email).scrollIntoView().should('be.visible');
 
-        cy.waitForStableDOM();
+              for (const role of user.roles) {
+                cy.contains(role).scrollIntoView().should('be.visible');
+              }
+            });
+        } else {
+          cy.contains('[data-cy="cell"]', user.userId)
+            .parent()
+            .within(() => {
+              cy.contains('Not found').scrollIntoView().should('be.visible');
 
-        cy.conditionalIntercept(
-          '**/graphql',
-          (req) => req.body.operationName == 'UpdateUserRoles',
-          'UpdateUserRoles',
-          {
-            data: {
-              updateOneUser: {
+              for (const role of user.roles) {
+                cy.contains(role).scrollIntoView().should('be.visible');
+              }
+            });
+        }
+      }
+
+      const user = users.data.users.find(
+        (user) => user.adminData.email == Cypress.env('e2eUsername')
+      );
+
+      cy.get('[data-cy="input-filter-Id"]').clear();
+      cy.get('[data-cy="input-filter-Id"]').type(user.userId);
+
+      cy.contains('[data-cy="cell"]', user.adminData.email)
+        .parent()
+        .within(() => {
+          cy.contains('Edit').click();
+        });
+
+      cy.waitForStableDOM();
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'UpdateUserRoles',
+        'UpdateUserRoles',
+        {
+          data: {
+            updateOneUser: {
+              __typename: 'User',
+              roles: ['subscriber', 'bananas', 'ban', 'admin', 'banana'],
+              userId: '6423479655e4bb918a233bda',
+            },
+          },
+        }
+      );
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'UpdateUserProfile',
+        'UpdateUserProfile',
+        {
+          data: {
+            users: [
+              {
                 __typename: 'User',
-                roles: ['subscriber', 'bananas', 'ban', 'admin', 'banana'],
+                adminData: {
+                  __typename: 'UserAdminDatum',
+                  creationDate: '2023-03-28T20:01:26Z',
+                  disabled: false,
+                  email: 'mail@cesarvarela.com',
+                  lastAuthenticationDate: '2023-05-04T22:03:06Z',
+                },
+                first_name: 'TestEdited',
+                last_name: 'User',
+                roles: ['subscriber', 'admin', 'banana'],
                 userId: '6423479655e4bb918a233bda',
               },
-            },
-          }
-        );
-
-        cy.conditionalIntercept(
-          '**/graphql',
-          (req) => req.body.operationName == 'UpdateUserProfile',
-          'UpdateUserProfile',
-          {
-            data: {
-              users: [
-                {
-                  __typename: 'User',
-                  adminData: {
-                    __typename: 'UserAdminDatum',
-                    creationDate: '2023-03-28T20:01:26Z',
-                    disabled: false,
-                    email: 'mail@cesarvarela.com',
-                    lastAuthenticationDate: '2023-05-04T22:03:06Z',
-                  },
-                  first_name: 'TestEdited',
-                  last_name: 'User',
-                  roles: ['subscriber', 'admin', 'banana'],
-                  userId: '6423479655e4bb918a233bda',
+              {
+                __typename: 'User',
+                adminData: {
+                  __typename: 'UserAdminDatum',
+                  creationDate: '2023-03-30T19:04:55Z',
+                  disabled: false,
+                  email: 'aiidsubscriber@cesarvarela.com',
+                  lastAuthenticationDate: '2023-04-28T00:33:58Z',
                 },
-                {
-                  __typename: 'User',
-                  adminData: {
-                    __typename: 'UserAdminDatum',
-                    creationDate: '2023-03-30T19:04:55Z',
-                    disabled: false,
-                    email: 'aiidsubscriber@cesarvarela.com',
-                    lastAuthenticationDate: '2023-04-28T00:33:58Z',
-                  },
-                  first_name: 'cesarito',
-                  last_name: 'test',
-                  roles: ['subscriber', 'incident_editor'],
-                  userId: '6425dd57ea44a80bf1443b33',
+                first_name: 'cesarito',
+                last_name: 'test',
+                roles: ['subscriber', 'incident_editor'],
+                userId: '6425dd57ea44a80bf1443b33',
+              },
+              {
+                __typename: 'User',
+                adminData: {
+                  __typename: 'UserAdminDatum',
+                  creationDate: null,
+                  disabled: null,
+                  email: null,
+                  lastAuthenticationDate: null,
                 },
-                {
-                  __typename: 'User',
-                  adminData: {
-                    __typename: 'UserAdminDatum',
-                    creationDate: null,
-                    disabled: null,
-                    email: null,
-                    lastAuthenticationDate: null,
-                  },
-                  first_name: null,
-                  last_name: null,
-                  roles: ['subscriber', 'admin'],
-                  userId: '6425dd79c505cc40eb65661e',
-                },
-              ],
-            },
-          }
-        );
+                first_name: null,
+                last_name: null,
+                roles: ['subscriber', 'admin'],
+                userId: '6425dd79c505cc40eb65661e',
+              },
+            ],
+          },
+        }
+      );
 
-        cy.get('[data-cy="edit-user-modal"]').within(() => {
-          cy.get('[id="roles"]', { timeout: 30000 }).type('banana{enter}');
+      cy.get('[data-cy="edit-user-modal"]').within(() => {
+        cy.get('[id="roles"]', { timeout: 30000 }).type('banana{enter}');
 
-          cy.get('[name="first_name"]').clear().type('Edited');
+        cy.get('[name="first_name"]').clear().type('Edited');
 
-          cy.contains('Submit').click();
+        cy.contains('Submit').click();
 
-          cy.wait('@UpdateUserRoles').then((xhr) => {
-            expect(xhr.request.body.variables.roles).includes('banana');
-            expect(xhr.request.body.variables.userId).eq(user.userId);
-          });
+        cy.wait('@UpdateUserRoles').then((xhr) => {
+          expect(xhr.request.body.variables.roles).includes('banana');
+          expect(xhr.request.body.variables.userId).eq(user.userId);
+        });
 
-          cy.wait('@UpdateUserProfile').then((xhr) => {
-            expect(xhr.request.body.variables.first_name).eq('Edited');
-            expect(xhr.request.body.variables.userId).eq(user.userId);
-          });
+        cy.wait('@UpdateUserProfile').then((xhr) => {
+          expect(xhr.request.body.variables.first_name).eq('Edited');
+          expect(xhr.request.body.variables.userId).eq(user.userId);
         });
       });
     }
