4cd49f2cd83a915f8702309668317a1b3c28104f
Luna McNulty <l.mcnulty@riseup.net>
2023-12-05 11:56:43 -0500

    Test that adding tag results in an api request

--- site/gatsby-site/cypress/e2e/integration/apps/checklistsForm.cy.js
+++ site/gatsby-site/cypress/e2e/integration/apps/checklistsForm.cy.js
@@ -109,4 +109,22 @@ describe('Checklists App Form', () => {
       cy.wait(['@upsertChecklist']);
     });
   });
+
+  maybeIt('Should trigger GraphQL upsert query on adding tag', () => {
+    withLogin(({ user }) => {
+      interceptFindChecklist({ ...defaultChecklist, owner_id: user.userId });
+      interceptUpsertChecklist({});
+
+      cy.visit(url);
+
+      cy.get('#tags_goals_input').type('Code Generation');
+      cy.get('#tags_goals').contains('Code Generation').click();
+
+      cy.wait(['@upsertChecklist']).then((xhr) => {
+        expect(xhr.request.body.variables.checklist).to.deep.nested.include({
+          tags_goals: ['GMF:Known AI Goal:Code Generation'],
+        });
+      });
+    });
+  });
 });
