20a0b182c0a23473fb7f39e1a683dd1ceea57d62
Clara Youdale <clara@botsfactory.io>
2024-01-18 13:37:22 -0300

    Fix triggers service name

--- site/realm/functions/onNewEntryFunction_translations.js
+++ site/realm/functions/onNewEntryFunction_translations.js
@@ -3,6 +3,6 @@ exports = function (changeEvent) {
     const newDocument = changeEvent.fullDocument;
     const collectionName = changeEvent.ns.coll; // Get collection name from the event
     newDocument.created_at = new Date();
-    const collection = context.services.get("AIIDDev").db("translations").collection(collectionName);
+    const collection = context.services.get("mongodb-atlas").db("translations").collection(collectionName);
     collection.updateOne({ _id: newDocument._id }, { $set: { created_at: newDocument.created_at } });
 };
