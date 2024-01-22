eaeaf79e0348798569066d8795fe481482baede3
Pablo Costa <pablo@botsfactory.io>
2024-01-22 19:47:51 -0300

    Fix `pages.cy.js` `rel=alternate` element count

--- site/gatsby-site/cypress/e2e/integration/pages.cy.js
+++ site/gatsby-site/cypress/e2e/integration/pages.cy.js
@@ -101,7 +101,7 @@ describe('Pages', () => {
 
         cy.get('[rel="canonical"]').invoke('attr', 'href').should('equal', url);
 
-        cy.get('[rel="alternate"]').should('have.length', 5);
+        cy.get('[rel="alternate"]').should('have.length', 6);
 
         cy.get('[rel="alternate"][hrefLang="x-default"]')
           .invoke('attr', 'href')
