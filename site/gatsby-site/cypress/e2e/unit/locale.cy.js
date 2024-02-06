63bc7050d4ec3417218b0c55538f4cc892d65851
Clara Youdale <clara@botsfactory.io>
2024-02-06 13:48:20 -0300

    Add test to check that a defined language is configured

--- site/gatsby-site/cypress/e2e/unit/locale.cy.js
+++ site/gatsby-site/cypress/e2e/unit/locale.cy.js
@@ -12,4 +12,19 @@ describe('Locale', () => {
       });
     }
   });
+
+
+  it('should have a configuration for each available language', () => {
+    cy.task('getLocaleConfigurations').then((configurations) => {
+      // Ensure the configurations were loaded
+      expect(configurations).to.not.be.null;
+
+      const availableLanguages = Cypress.env('availableLanguages').split(',');
+
+      availableLanguages.forEach((locale) => {
+        const hasConfig = configurations.some(config => config.code === locale);
+        expect(hasConfig, `Locale ${locale} should have configuration`).to.be.true;
+      });
+    });
+  });
 });
