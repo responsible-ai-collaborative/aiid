db73ca0f48331e9d532194f7a6ce2e6f7a643edf
Clara Youdale <clara@botsfactory.io>
2024-02-08 10:16:31 -0300

    Fix test to use readFile instead of task

--- site/gatsby-site/cypress/e2e/unit/locale.cy.js
+++ site/gatsby-site/cypress/e2e/unit/locale.cy.js
@@ -1,22 +1,53 @@
 describe('Locale', () => {
-  it('Locale folder should exist for all specified languages', () => {
+  it('Locale folder should contain specific JSON files for all specified languages', () => {
     let availableLanguages = Cypress.env('availableLanguages');
+
+    // Define the list of files you expect to find in each locale directory
+    const expectedFiles = [
+      'account.json',
+      'actions.json',
+      'entities.json',
+      'footer.json',
+      'incidents.json',
+      'landing.json',
+      'leaderboard.json',
+      'login.json',
+      'popovers.json',
+      'sponsors.json',
+      'submit.json',
+      'submitted.json',
+      'translation.json',
+      'validation.json',
+      'variants.json',
+      'wordcount.json',
+    ];
 
     if (availableLanguages) {
       availableLanguages = availableLanguages.split(',');
 
       availableLanguages.forEach((locale) => {
-        cy.task('checkDirectoryExists', { locale }).then((exists) => {
-          expect(exists).to.be.true;
+        expectedFiles.forEach((fileName) => {
+          const filePath = `./i18n/locales/${locale}/${fileName}`;
+
+          cy.readFile(filePath).then((content) => {
+            if (content) {
+              expect(true).to.be.true; // File exists
+            } else {
+              assert.fail(`File not found: ${filePath}`); // File does not exist
+            }
+          });
         });
       });
     }
   });
 
   it('should have a configuration for each available language', () => {
-    cy.task('getLocaleConfigurations').then((configurations) => {
+    const configPath = './i18n/config.json'; // Adjust the path as needed
+
+    // Use Cypress.readFile to read and parse the JSON file
+    cy.readFile(configPath).then((configurations) => {
       // Ensure the configurations were loaded
-      expect(configurations).to.not.be.null;
+      expect(configurations).to.be.an('array');
 
       const availableLanguages = Cypress.env('availableLanguages').split(',');
 
--- site/gatsby-site/cypress/plugins/index.js
+++ site/gatsby-site/cypress/plugins/index.js
@@ -13,39 +13,11 @@
 // the project's config changing)
 
 require('dotenv').config();
-const fs = require('fs');
-
-const path = require('path');
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  on('task', {
-    checkDirectoryExists({ locale }) {
-      const dirPath = path.resolve('./i18n/locales', locale);
-
-      if (fs.existsSync(dirPath)) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-
-    getLocaleConfigurations: () => {
-      const configPath = path.resolve('./i18n/config.json');
-
-      if (fs.existsSync(configPath)) {
-        const configFile = fs.readFileSync(configPath, 'utf8');
-
-        const configJson = JSON.parse(configFile);
-
-        return configJson;
-      }
-      return null;
-    },
-  });
-
   config.env.e2eUsername = process.env.E2E_ADMIN_USERNAME;
   config.env.e2ePassword = process.env.E2E_ADMIN_PASSWORD;
   config.env.realmAppId = process.env.GATSBY_REALM_APP_ID;
