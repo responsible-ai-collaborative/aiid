e193e2185c1673ba11403a72c4ba407f3dad047c
Cesar Varela <mail@cesarvarela.com>
2024-02-15 17:01:43 -0300

    Add LRU cache

--- site/gatsby-site/src/api/lookupbyurl.js
+++ site/gatsby-site/src/api/lookupbyurl.js
@@ -1,6 +1,7 @@
 import fetch from 'cross-fetch';
 import Cors from 'cors';
 import siteConfig from '../../config';
+import { LRUCache } from 'lru-cache';
 
 const cors = Cors();
 
@@ -9,25 +10,43 @@ query Lookup($urls: [String]){
   lookupByUrl(input:{urls: $urls}){
     results {
       url
+      incidents {
+        incident_id
+        title
+        permalink
+      }
+      reports {
+        report_number
+        url
+        title
+      }
     }
   }
 }`;
 
+const cache = new LRUCache({ max: 1000 });
+
 export default async function handler(req, res) {
-  const variables = { urls: req.query.urls.split(',') };
+  let data = null;
 
-  const response = await fetch(
-    `https://realm.mongodb.com/api/client/v2.0/app/${siteConfig.realm.production_db.realm_app_id}/graphql`,
-    {
-      method: 'POST',
-      headers: {
-        apiKey: siteConfig.realm.graphqlApiKey,
-      },
-      body: JSON.stringify({ query, variables }),
-    }
-  );
+  if (cache.has(req.query.urls) && req.query.urls.length > 0) {
+    data = cache.get(req.query.urls);
+  } else {
+    const variables = { urls: req.query.urls.split(',') };
 
-  const data = await response.json();
+    const response = await fetch(
+      `https://realm.mongodb.com/api/client/v2.0/app/${siteConfig.realm.production_db.realm_app_id}/graphql`,
+      {
+        method: 'POST',
+        headers: {
+          apiKey: siteConfig.realm.graphqlApiKey,
+        },
+        body: JSON.stringify({ query, variables }),
+      }
+    );
+
+    data = await response.json();
+  }
 
   // Manually run the cors middleware
   // https://www.gatsbyjs.com/docs/reference/functions/middleware-and-helpers/#custom-middleware
