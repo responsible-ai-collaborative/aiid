52e7b0ab618c82255fc840fbcf248ab87becfe20
Cesar Varela <mail@cesarvarela.com>
2024-02-27 16:09:36 -0300

    Avoid hydration error

--- site/gatsby-site/src/pages/api-spec.js
+++ site/gatsby-site/src/pages/api-spec.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const ApiSpec = () => {
+  useEffect(() => {
+    window.SwaggerUIBundle({
+      url: '/spec.json',
+      dom_id: '#swagger-ui',
+    });
+  }, []);
+
   return (
-    <>
+    <div>
       <div id="swagger-ui" />
-      <script
-        src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"
-        crossOrigin
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.onload = () => {
-          window.ui = SwaggerUIBundle({
-            url: '/spec.json',
-            dom_id: '#swagger-ui',
-          });
-        };
-    `,
-        }}
-      ></script>
-    </>
+    </div>
   );
 };
 
 export const Head = () => {
-  return <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />;
+  return (
+    <>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />;
+      <script
+        src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
 };
 
 export default ApiSpec;
