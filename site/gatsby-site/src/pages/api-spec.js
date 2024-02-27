1b04d1cbf1d035b665e2bdef692089617202b473
Cesar Varela <mail@cesarvarela.com>
2024-02-26 22:26:35 -0300

    Use js bundle mode to prevent build form crashing  🙏

--- site/gatsby-site/src/pages/api-spec.js
+++ site/gatsby-site/src/pages/api-spec.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
+import React, { useEffect } from 'react';
 import 'swagger-ui-react/swagger-ui.css';
 import spec from '../api/spec.json';
+import SwaggerUI from 'swagger-ui';
 
 const ApiSpec = () => {
-  return <SwaggerUI spec={spec} />;
+  useEffect(() => {
+    SwaggerUI({
+      dom_id: '#swagger-ui',
+      spec,
+    });
+  }, []);
+
+  return <div id="swagger-ui" />;
 };
 
 export default ApiSpec;
