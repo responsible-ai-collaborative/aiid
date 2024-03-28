04038c77f98d68e246ef44791063760ee1e029e9
Pablo Costa <pablo@botsfactory.io>
2024-03-27 21:44:50 -0300

    Add test when editing Entity fails

--- site/gatsby-site/cypress/e2e/integration/entityEdit.cy.js
+++ site/gatsby-site/cypress/e2e/integration/entityEdit.cy.js
@@ -59,4 +59,64 @@ describe('Edit Entity', () => {
       cy.get('.tw-toast').contains('Entity updated successfully.').should('exist');
     }
   );
+
+  conditionalIt(
+    !Cypress.env('isEmptyEnvironment') && Cypress.env('e2eUsername') && Cypress.env('e2ePassword'),
+    'Should display an error message when editing Entity fails',
+    () => {
+      cy.login(Cypress.env('e2eUsername'), Cypress.env('e2ePassword'));
+
+      cy.visit(url);
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'FindEntity',
+        'FindEntity',
+        entity
+      );
+
+      cy.wait(['@FindEntity']);
+
+      const values = {
+        name: 'Google new',
+      };
+
+      Object.keys(values).forEach((key) => {
+        cy.get(`[name=${key}]`).clear().type(values[key]);
+      });
+
+      cy.conditionalIntercept(
+        '**/graphql',
+        (req) => req.body.operationName == 'UpdateEntity',
+        'UpdateEntity',
+        {
+          data: null,
+          errors: [
+            {
+              message: 'Dummy error message',
+            },
+          ],
+        }
+      );
+
+      const now = new Date();
+
+      cy.clock(now);
+
+      cy.contains('button', 'Save').click();
+
+      const updatedEntity = {
+        name: values.name,
+        date_modified: now.toISOString(),
+      };
+
+      cy.wait('@UpdateEntity').then((xhr) => {
+        expect(xhr.request.body.operationName).to.eq('UpdateEntity');
+        expect(xhr.request.body.variables.query.entity_id).to.eq(entity_id);
+        expect(xhr.request.body.variables.set).to.deep.eq(updatedEntity);
+      });
+
+      cy.get('.tw-toast').contains('Error updating Entity.').should('exist');
+    }
+  );
 });
