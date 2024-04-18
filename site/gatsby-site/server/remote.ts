58b410fc99b8dffeee16f3d682b2ce714bb2ce33
Cesar Varela <mail@cesarvarela.com>
2024-04-18 20:19:46 -0300

    Fix ts error

--- site/gatsby-site/server/remote.ts
+++ site/gatsby-site/server/remote.ts
@@ -1,24 +1,26 @@
-import { buildHTTPExecutor } from '@graphql-tools/executor-http'
+import { buildHTTPExecutor, HeadersConfig } from '@graphql-tools/executor-http'
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { FilterObjectFields, FilterTypes, wrapSchema } from '@graphql-tools/wrap';
 import remoteTypeDefs from './remoteTypeDefs';
 import config from './config';
+import { ExecutionRequest } from '@graphql-tools/utils';
 
 const userExecutor = buildHTTPExecutor({
     endpoint: `https://realm.mongodb.com/api/client/v2.0/app/${config.REALM_APP_ID}/graphql`,
-    headers(executorRequest) {
-
+    headers(executorRequest?: ExecutionRequest<any, any, any, Record<string, any>, any> | undefined): HeadersConfig {
         if (
             !executorRequest?.context.req.headers.authorization && executorRequest?.info?.operation.operation == 'query'
         ) {
             return {
                 apiKey: config.REALM_GRAPHQL_API_KEY,
+                authorization: '',
             };
         }
 
         return {
+            apiKey: '',
             authorization: executorRequest?.context.req.headers.authorization!,
-        }
+        };
     },
 });
 
