a6d2d2643f92637794c84bdf99b2bf2e36b7566e
Cesar Varela <mail@cesarvarela.com>
2024-04-26 18:29:52 -0300

    Remove custom timeout

--- site/gatsby-site/cypress/e2e/integration/apps/submitted.cy.js
+++ site/gatsby-site/cypress/e2e/integration/apps/submitted.cy.js
@@ -1066,7 +1066,7 @@ describe('Submitted reports', () => {
 
       cy.contains('[data-cy="toast"]', '*Date is not valid, must be `YYYY-MM-DD`').should('exist');
 
-      cy.wait('@promotionInvoked', { timeout: 2000 });
+      cy.wait('@promotionInvoked');
     }
   );
 
