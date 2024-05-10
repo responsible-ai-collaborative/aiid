f7d314dd79596d65a45f7a49de940b6fc45d9086
Cesar Varela <mail@cesarvarela.com>
2024-05-10 18:05:38 -0300

    Remove unused package

--- site/gatsby-site/server/config.ts
+++ site/gatsby-site/server/config.ts
@@ -1,29 +1,13 @@
-import { z } from 'zod';
-import { createEnv } from './utils';
-
-const config = createEnv({
-    server: {
-        REALM_APP_ID: z.string().min(1),
-        REALM_API_APP_ID: z.string().min(1),
-        REALM_API_GROUP_ID: z.string().min(1),
-        REALM_API_PUBLIC_KEY: z.string().min(1),
-        REALM_API_PRIVATE_KEY: z.string().min(1),
-        REALM_GRAPHQL_API_KEY: z.string().min(1),
-        MONGODB_CONNECTION_STRING: z.string().min(1),
-        E2E_ADMIN_USERNAME: z.string().min(1),
-        E2E_ADMIN_PASSWORD: z.string().min(1),
-    },
-    runtimeEnvStrict: {
-        E2E_ADMIN_PASSWORD: process.env.E2E_ADMIN_PASSWORD,
-        E2E_ADMIN_USERNAME: process.env.E2E_ADMIN_USERNAME,
-        REALM_API_APP_ID: process.env.REALM_API_APP_ID,
-        REALM_API_GROUP_ID: process.env.REALM_API_GROUP_ID,
-        REALM_API_PRIVATE_KEY: process.env.REALM_API_PRIVATE_KEY,
-        REALM_API_PUBLIC_KEY: process.env.REALM_API_PUBLIC_KEY,
-        REALM_GRAPHQL_API_KEY: process.env.REALM_GRAPHQL_API_KEY,
-        REALM_APP_ID: process.env.REALM_APP_ID,
-        MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING,
-    },
-});
+const config = {
+    E2E_ADMIN_PASSWORD: process.env.E2E_ADMIN_PASSWORD!,
+    E2E_ADMIN_USERNAME: process.env.E2E_ADMIN_USERNAME!,
+    REALM_API_APP_ID: process.env.REALM_API_APP_ID!,
+    REALM_API_GROUP_ID: process.env.REALM_API_GROUP_ID!,
+    REALM_API_PRIVATE_KEY: process.env.REALM_API_PRIVATE_KEY!,
+    REALM_API_PUBLIC_KEY: process.env.REALM_API_PUBLIC_KEY!,
+    REALM_GRAPHQL_API_KEY: process.env.REALM_GRAPHQL_API_KEY!,
+    REALM_APP_ID: process.env.REALM_APP_ID!,
+    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING!,
+}
 
 export default config;
