ee0fab316013b3f62d90ab4254b27b404d2107f5
Cesar Varela <mail@cesarvarela.com>
2024-05-30 22:17:14 -0300

    increase timeout

--- site/gatsby-site/playwright.config.ts
+++ site/gatsby-site/playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './playwright',
-  globalTimeout: 30000,
+  globalTimeout: 60000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
