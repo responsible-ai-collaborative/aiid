11a73f0b7794de11330f07c195ceee295fb7c418
Cesar Varela <mail@cesarvarela.com>
2024-06-04 23:08:30 -0300

    Add cite page tests

--- site/gatsby-site/playwright/utils.ts
+++ site/gatsby-site/playwright/utils.ts
@@ -1,5 +1,8 @@
+import { ApolloClient, HttpLink, InMemoryCache, OperationVariables, QueryOptions } from '@apollo/client';
 import { Page, Route, test, Request } from '@playwright/test';
 import { minimatch } from 'minimatch'
+import config from './config';
+import assert from 'node:assert';
 
 declare module '@playwright/test' {
     interface Request {
@@ -12,6 +15,8 @@ export function conditionalIt(condition: boolean, description: string, fn: any) 
         test(description, fn);
     }
 }
+
+export const maybeIt = config.E2E_ADMIN_USERNAME && config.E2E_ADMIN_PASSWORD ? test : test.skip;
 
 
 // SEE: https://playwright.dev/docs/api/class-page#page-wait-for-request
@@ -42,6 +47,8 @@ export async function conditionalIntercept(
 
     if (alias) {
 
+        assert(!waitForRequestMap.has(alias), `Alias ${alias} already exists`);
+
         waitForRequestMap.set(alias, page.waitForRequest((req) => minimatch(req.url(), url) && condition(req)));
     }
 }
@@ -52,7 +59,7 @@ export async function waitForRequest(alias: string) {
 
     waitForRequestMap.delete(alias);
 
-    return promise;
+    return promise!;
 }
 
 
@@ -76,3 +83,57 @@ export async function mockDate(page: Page, date: Date) {
       Date.now = () => __DateNow() + __DateNowOffset;
     }`);
 }
+
+export const getApolloClient = () => {
+
+    const client = new ApolloClient({
+        link: new HttpLink({
+            uri: `http://localhost:8000/api/graphql`,
+
+            fetch: async (uri, options) => {
+                options.headers.email = config.E2E_ADMIN_USERNAME!;
+                options.headers.password = config.E2E_ADMIN_PASSWORD!;
+
+                return fetch(uri, options);
+            },
+        }),
+        cache: new InMemoryCache(),
+    });
+
+    return client;
+};
+
+const client = getApolloClient();
+
+export function query(data: QueryOptions<OperationVariables, any>) {
+
+    const { query, variables } = data
+
+    return client.query({ query, variables });
+}
+
+const loginSteps = async (page: Page, email: string, password: string) => {
+    await page.goto('/login');
+
+    await page.fill('input[name="email"]', email);
+    await page.fill('input[name="password"]', password);
+    await page.click('[data-cy="login-btn"]');
+
+    await page.waitForURL(url => !url.toString().includes('/login'));
+};
+
+export async function login(page: Page, email: string, password: string, options: { skipSession?: boolean } = { skipSession: false }) {
+    if (options.skipSession) {
+        await loginSteps(page, email, password);
+    } else {
+        const sessionState = await page.context().storageState();
+        if (!sessionState || sessionState.cookies.length === 0) {
+            await page.context().clearCookies();
+            await loginSteps(page, email, password);
+            await page.context().storageState({ path: 'session.json' });
+        } else {
+            await page.context().addCookies(sessionState.cookies);
+        }
+    }
+}
+
