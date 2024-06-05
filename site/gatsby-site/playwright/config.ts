03d4eac4c138fc4764fc197dac4fdc28ad14a493
Cesar Varela <mail@cesarvarela.com>
2024-06-05 15:20:14 -0300

    Add missing env vars

--- site/gatsby-site/playwright/config.ts
+++ site/gatsby-site/playwright/config.ts
@@ -5,7 +5,7 @@ type ConfigType = {
 const config: ConfigType = {
     E2E_ADMIN_PASSWORD: process.env.E2E_ADMIN_PASSWORD!,
     E2E_ADMIN_USERNAME: process.env.E2E_ADMIN_USERNAME!,
-    IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT!,
+    IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT ?? '',
 }
 
 Object.keys(config).forEach((key) => {
