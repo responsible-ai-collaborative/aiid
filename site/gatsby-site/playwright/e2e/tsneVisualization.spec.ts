882cec7d8cf548d5adb754a47d7a14d83c857efc
Luna McNulty <l.mcnulty@riseup.net>
2024-06-12 12:30:22 -0400

    Add playwright tests for TSNE visualization

---
+++ site/gatsby-site/playwright/e2e/tsneVisualization.spec.ts
@@ -0,0 +1,80 @@
+import { test, expect, Page, Download } from '@playwright/test';
+import { conditionalIntercept, conditionalIt, mockDate, waitForRequest } from '../utils';
+import { getUnixTime } from 'date-fns';
+import { deleteReportTypenames, transformReportData } from '../../src/utils/reports';
+import fs from 'fs/promises';
+import path from 'path';
+import config from '../../config';
+
+test.describe('TSNE visualization page', () => {
+
+  const url = '/summaries/spatial';
+
+  test('Successfully loads', async ({ page }) => {
+      await page.goto(url);
+  });
+
+  conditionalIt(
+    process.env.isEmptyEnvironment !== 'true',
+    'Should show an incident card on hover', 
+    async ({ page }) => {
+      await page.goto(url);
+
+      page.locator('[data-cy="tsne-visualization"] #spatial-incident-1').dispatchEvent('mouseover');
+
+      await expect(
+        page.locator(
+          '[data-cy="tsne-visualization"] #spatial-incident-1 + [data-cy="incident-card"]'
+        )
+      ).toBeVisible();
+
+  });
+
+  conditionalIt(
+    process.env.isEmptyEnvironment !== 'true',
+    'Should highlight source incident when one exists', 
+    async ({ page }) => {
+      await page.goto(url + '?incident=1');
+
+      page.locator('[data-cy="tsne-visualization"] #spatial-incident-1').dispatchEvent('mouseover');
+
+      await expect(
+        page.locator(
+          '[data-cy="tsne-visualization"] [data-cy="tsne-plotpoint"].current'
+        )
+      ).toBeVisible();
+
+  });
+
+  conditionalIt(
+    process.env.isEmptyEnvironment !== 'true',
+    'Incident card should show title', 
+    async ({ page }) => {
+      await page.goto(url);
+
+      page.locator('[data-cy="tsne-visualization"] #spatial-incident-1').dispatchEvent('mouseover');
+
+      await expect(
+        page.locator(
+          '[data-cy="tsne-visualization"] #spatial-incident-1 + [data-cy="incident-card"] [data-cy="title"]'
+        )
+      ).toBeVisible();
+  });
+
+  conditionalIt(
+    process.env.isEmptyEnvironment !== 'true',
+    'Should change the plotpoint color when the axis selection changes', 
+    async ({ page }) => {
+      await page.goto(url);
+
+      const initialBackground = await page.$eval('[data-cy="tsne-visualization"] #spatial-incident-1', el => getComputedStyle(el).backgroundColor);
+
+      await page.locator('#color-axis-select').selectOption('CSETv0::Harm Distribution Basis');
+
+      await expect(
+        JSON.stringify(
+          await page.$eval('[data-cy="tsne-visualization"] #spatial-incident-1', el => getComputedStyle(el).backgroundColor)
+        ) == JSON.stringify(initialBackground)
+      ).toBeFalsy();
+  });
+});
