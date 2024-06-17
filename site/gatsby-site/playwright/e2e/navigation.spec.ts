3ded18fdc1048accadfce413f71c98f769a40b6c
Clara Youdale <clara@botsfactory.io>
2024-06-17 12:43:13 -0300

    Migrate navigation.cy to playwright

---
+++ site/gatsby-site/playwright/e2e/navigation.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.describe('Navigation', () => {
+  test('Check menu links work (English)', async ({ page }) => {
+    await page.goto('/');
+    await checkLinks(page);
+  });
+
+  test('Check menu links work (Spanish)', async ({ page }) => {
+    await page.goto('/es/');
+    await checkLinks(page);
+  });
+
+  test('Check menu links work (French)', async ({ page }) => {
+    await page.goto('/fr/');
+    await checkLinks(page);
+  });
+
+  const checkLinks = async (page: Page) => {
+    const links = await page.locator('.item.active').evaluateAll((nodes: any[]) => nodes.map((node: { getAttribute: (arg0: string) => any; }) => node.getAttribute('href')));
+    for (const href of links) {
+      await page.goto(href);
+      const activeHref = await page.locator('.active[data-cy="sidebar-link-active"]').first().getAttribute('href');
+      expect(href.endsWith(activeHref)).toBe(true);
+    }
+  };
+
+  test('Check right sidebar "Contents" layout', async ({ page }) => {
+    await page.setViewportSize({ width: 1280, height: 800 });
+    await page.goto('/');
+    await page.getByText('About', { exact: true }).click();
+    await expect(page.locator('.rightSideTitle')).toHaveText('CONTENTS');
+    const sidebarElementsCount = await page.locator('.rightSideBarUL li').count();
+    await expect(sidebarElementsCount).toBeGreaterThan(0);
+    await page.locator('[data-cy="sidebar-desktop"] > #sidebar > [data-cy="sidebar-tree"] > [data-cy="sidebar-welcome"] > [data-cy="sidebar-link"]').click();
+    await expect(page.locator('.rightSideTitle')).not.toBeVisible();
+  });
+});
