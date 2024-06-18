6b4d4425b8106ab1c7bf3785478eb15750ec04d6
Cesar Varela <mail@cesarvarela.com>
2024-06-18 15:43:03 -0300

    Update tests (#2885)

--- site/gatsby-site/playwright/e2e/header.spec.ts
+++ site/gatsby-site/playwright/e2e/header.spec.ts
@@ -1,62 +1,62 @@
-import { conditionalIntercept, waitForRequest, query, login, maybeIt, mockDate } from '../utils';
+import { conditionalIntercept, waitForRequest, query, mockDate } from '../utils';
 import { test, expect } from '@playwright/test';
 import config from '../config';
 
 test.describe('Header', () => {
 
-  test('Global search input appears on hover', async ({ page }) => {
-      await page.goto('/');
+    test('Global search input appears on hover', async ({ page }) => {
+        await page.goto('/');
 
-      await expect(page.locator('#header-search-bar')).toBeHidden();
+        await expect(page.locator('#header-search-bar')).toBeHidden();
 
-      await page.locator('#header-search-focus').hover();
+        await page.locator('#header-search-focus').hover();
 
-      await expect(page.locator('#header-search-bar')).toBeVisible();
-  });
+        await expect(page.locator('#header-search-bar')).toBeVisible();
+    });
 
-  test('Global search input appears on click', async ({ page }) => {
-      await page.goto('/');
+    test('Global search input appears on click', async ({ page }) => {
+        await page.goto('/');
 
-      await expect(page.locator('#header-search-bar')).toBeHidden();
+        await expect(page.locator('#header-search-bar')).toBeHidden();
 
-      await page.locator('#header-search-focus').click();
+        await page.locator('#header-search-focus').click();
 
-      await expect(page.locator('#header-search-bar')).toBeVisible();
-  });
+        await expect(page.locator('#header-search-bar')).toBeVisible();
+    });
 
-  test('Can initiate search by clicking button', async ({ page }) => {
-      await page.goto('/');
+    test('Can initiate search by clicking button', async ({ page }) => {
+        await page.goto('/');
 
-      await page.locator('#header-search-focus').click();
+        await page.locator('#header-search-focus').click();
 
-      await page.locator('#header-search-bar').fill('something');
+        await page.locator('#header-search-bar').fill('something');
 
-      await page.locator('#header-search-submit').click();
+        await page.locator('#header-search-submit').click();
 
-      await expect(page).toHaveURL(/.*s=something.*/);
-  });
+        await expect(page).toHaveURL(/.*s=something.*/);
+    });
 
-  test('Can initiate search by pressing enter', async ({ page }) => {
-      await page.goto('/');
+    test('Can initiate search by pressing enter', async ({ page }) => {
+        await page.goto('/');
 
-      await page.locator('#header-search-focus').click();
+        await page.locator('#header-search-focus').click();
 
-      await page.locator('#header-search-bar').fill('something');
+        await page.locator('#header-search-bar').fill('something');
 
-      await page.locator('#header-search-bar').press('Enter');
+        await page.locator('#header-search-bar').press('Enter');
 
-      await expect(page).toHaveURL(/.*s=something.*/);
-  });
+        await expect(page).toHaveURL(/.*s=something.*/);
+    });
 
-  test('Global search works on discover page', async ({ page }) => {
-      await page.goto('/apps/discover/');
+    test('Global search works on discover page', async ({ page }) => {
+        await page.goto('/apps/discover/');
 
-      await page.locator('#header-search-focus').click();
+        await page.locator('#header-search-focus').click();
 
-      await page.locator('#header-search-bar').fill('something');
+        await page.locator('#header-search-bar').fill('something');
 
-      await page.locator('#header-search-bar').press('Enter');
+        await page.locator('#header-search-bar').press('Enter');
 
-      await expect(page).toHaveURL(/.*s=something.*/);
-  });
+        await expect(page).toHaveURL(/.*s=something.*/);
+    });
 })
