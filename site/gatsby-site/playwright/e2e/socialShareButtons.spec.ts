9819e348d9a5b13670436918a2e0f4705bc8e3d7
Clara Youdale <clara@botsfactory.io>
2024-07-02 13:19:59 -0300

    Remove timeouts from socialShare

--- site/gatsby-site/playwright/e2e/socialShareButtons.spec.ts
+++ site/gatsby-site/playwright/e2e/socialShareButtons.spec.ts
@@ -4,7 +4,7 @@ import { test } from '../utils';
 
 const incidentId = 10;
 const incidentUrl = `/cite/${incidentId}/`;
-const blogPostUrl = `/blog/join-raic`;
+const blogPostUrl = `/blog/join-raic/`;
 const shareButtonsPerSection = 4;
 const urlsToTest = [
   {
@@ -26,8 +26,8 @@ if (!config.IS_EMPTY_ENVIRONMENT) {
 
 urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
   test(`${page} page should have ${shareButtonSections} Social Share button sections`, async ({ page }) => {
-    await page.goto(url);
-    const buttons = page.locator('[data-cy=social-share-buttons] button');
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    const buttons = page.locator('[data-cy="social-share-buttons"] button');
     await expect(buttons).toHaveCount(shareButtonSections * shareButtonsPerSection);
   });
 
@@ -41,8 +41,7 @@ urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
       window.open = (url: string) => { window.location.href = url; return null; };
     });
     await twitterButton.first().click();
-    await page.waitForTimeout(8000);
-    await expect(page.url()).toContain(`https://x.com/intent/post?text=${encodeURIComponent(title).replace(/%20/g, '+')}&url=${encodeURIComponent(canonicalUrl)}`);
+    await page.waitForURL(`https://x.com/intent/post?text=${encodeURIComponent(title).replace(/%20/g, '+')}&url=${encodeURIComponent(canonicalUrl)}`);
   });
 
   test(`${page} page should have a LinkedIn share button`, async ({ page }) => {
@@ -53,8 +52,7 @@ urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
       window.open = (url: string) => { window.location.href = url; return null; };
     });
     await linkedInButton.first().click();
-    await page.waitForTimeout(8000);
-    await expect(page.url()).toContain('https://www.linkedin.com/');
+    await page.waitForURL(/https:\/\/www\.linkedin\.com*/);
   });
 
   test(`${page} page should have an Email share button`, async ({ page }) => {
@@ -70,8 +68,7 @@ urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
       };
     }, canonicalUrl);
     await emailButton.first().click();
-    await page.waitForTimeout(8000); // Wait to ensure the action is triggered
-    await expect(page.url()).toBe(canonicalUrl);
+    await page.waitForURL(canonicalUrl);
 
   });
 
@@ -83,7 +80,6 @@ urlsToTest.forEach(({ page, url, title, shareButtonSections }) => {
       window.open = (url: string) => { window.location.href = url; return null; };
     });
     await facebookButton.first().click();
-    await page.waitForTimeout(8000);
-    await expect(page.url()).toMatch(/https:\/\/www\.facebook\.com\/(login\.php|sharer\/sharer\.php\?u=)/);
+    await page.waitForURL(/https:\/\/www\.facebook\.com\/(login\.php|sharer\/sharer\.php\?u=)/);
   });
 });
