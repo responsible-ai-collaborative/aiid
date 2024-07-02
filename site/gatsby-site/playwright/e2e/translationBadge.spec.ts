96cd44da3d78517b0659608d06905f8cd6e6bc79
Clara Youdale <clara@botsfactory.io>
2024-07-02 14:14:04 -0300

    Add missing skipOnEmptyEnvironment checks for 2 tests

--- site/gatsby-site/playwright/e2e/translationBadge.spec.ts
+++ site/gatsby-site/playwright/e2e/translationBadge.spec.ts
@@ -9,12 +9,12 @@ test.describe('Translation Badges', () => {
     await expect(page.locator('a', { hasText: 'Ver Original' })).toHaveAttribute('href', '/blog/using-ai-to-connect-ai-incidents/');
   });
 
-  test('Should be visible on the discover app', async ({ page }) => {
+  test('Should be visible on the discover app', async ({ page, skipOnEmptyEnvironment }) => {
     await page.goto('/es/apps/discover?display=details&incident_id=1&page=1&source_domain=today.com');
     await expect(page.locator('[data-cy="5d34b8c29ced494f010ed45c"]').locator('[data-cy="translation-badge"]').getByText('Traducido por IA')).toBeVisible();
   });
 
-  test('Should be visible on an incident card on the citation page', async ({ page }) => {
+  test('Should be visible on an incident card on the citation page', async ({ page, skipOnEmptyEnvironment }) => {
     await page.goto('/es/cite/1#r1');
     await expect(page.locator('#r1').locator('[data-cy="translation-badge"]').getByText('Traducido por IA')).toBeVisible();
   });
