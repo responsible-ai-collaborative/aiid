083cf288c3b27ab7a699820d7196b025470422cc
Clara Youdale <clara@botsfactory.io>
2024-07-10 14:39:38 -0300

    Fix date_modified time check

--- site/gatsby-site/playwright/e2e/integration/entityEdit.spec.ts
+++ site/gatsby-site/playwright/e2e/integration/entityEdit.spec.ts
@@ -1,14 +1,14 @@
 import { conditionalIntercept, waitForRequest, test } from '../../utils';
 import entity from '../../fixtures/entities/entity.json';
 import updateOneEntity from '../../fixtures/entities/updateOneEntity.json';
-import { expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 test.describe('Edit Entity', () => {
   const entity_id = 'google';
   const url = `/entities/edit?entity_id=${entity_id}`;
 
   test('Should successfully edit Entity fields',
-    async ({ page, login, skipOnEmptyEnvironment  }) => {
+    async ({ page, login, skipOnEmptyEnvironment }) => {
       await login(process.env.E2E_ADMIN_USERNAME, process.env.E2E_ADMIN_PASSWORD);
       await page.goto(url);
 
@@ -36,23 +36,22 @@ test.describe('Edit Entity', () => {
         'UpdateEntity'
       );
 
-      const now = new Date();
-
       await page.addInitScript(() => {
         Date.now = () => now.getTime();
       });
 
-      await page.locator(':has-text("Save")').click();
+      await page.getByText("Save", { exact: true }).click();
+      const now = new Date();
 
       const updatedEntity = {
         name: values.name,
         date_modified: now.toISOString(),
       };
-
       const updateEntityRequest = await waitForRequest('UpdateEntity');
       const variables = updateEntityRequest.postDataJSON().variables;
       expect(variables.query.entity_id).toBe(entity_id);
-      expect(variables.set).toEqual(updatedEntity);
+      expect(variables.set.name).toBe(values.name);
+      expect(new Date(variables.set.date_modified).getTime()).toBeCloseTo(new Date(updatedEntity.date_modified).getTime(), -2);
 
       await expect(page.locator('.tw-toast')).toContainText('Entity updated successfully.');
     }
@@ -100,15 +99,14 @@ test.describe('Edit Entity', () => {
         Date.now = () => now.getTime();
       });
 
-      await page.locator(':has-text("Save")').click();
+      await page.getByText("Save", { exact: true }).click();
 
       const updateEntityRequest = await waitForRequest('UpdateEntity');
       const variables = updateEntityRequest.postDataJSON().variables;
       expect(variables.query.entity_id).toBe(entity_id);
-      expect(variables.set).toEqual({
-        name: values.name,
-        date_modified: now.toISOString(),
-      });
+      expect(variables.set.name).toBe(values.name);
+      expect(new Date(variables.set.date_modified).getTime()).toBeCloseTo(now.getTime(), -2);
+
 
       await expect(page.locator('.tw-toast')).toContainText('Error updating Entity.');
     }
