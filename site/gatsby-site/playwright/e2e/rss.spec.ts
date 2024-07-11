3cb44f2c6a5faefbadeeda22e1eaa81030091258
Clara Youdale <clara@botsfactory.io>
2024-07-11 15:41:35 -0300

    Use fast-xml-parser instead

--- site/gatsby-site/playwright/e2e/rss.spec.ts
+++ site/gatsby-site/playwright/e2e/rss.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
+import config from '../config';
 import { test } from '../utils';
-import { parse } from 'node-html-parser';
+import { XMLParser } from 'fast-xml-parser';
 
 test.describe('RSS', () => {
   test('Should generate a valid RSS feed', async ({ page }) => {
@@ -9,40 +10,43 @@ test.describe('RSS', () => {
     expect(response.headers()['content-type']).toContain('application/xml');
 
     const xml = await response.text();
-    const parsedXml = parse(xml);
+    const parser = new XMLParser();
+    const parsedXml = parser.parse(xml);
 
-    const hasChannelTag = parsedXml.querySelectorAll('channel').length > 0;
+    const hasChannelTag = parsedXml.rss.channel !== undefined;
     expect(hasChannelTag).toBe(true);
 
-    const items = Array.from(parsedXml.querySelectorAll('channel > item')).slice(0, 20);
+    const items = parsedXml.rss.channel.item.slice(0, 20);
     expect(items.length).toBeGreaterThan(0);
   });
 
-  test('Should generate a valid RSS feed data', async ({ page, skipOnEmptyEnvironment }) => {
-    const response = await page.request.get('/rss.xml');
-    expect(response.status()).toBe(200);
-    expect(response.headers()['content-type']).toContain('application/xml');
+  if (!config.isEmptyEnvironment) {
+    test('Should generate a valid RSS feed data', async ({ page }) => {
+      const response = await page.request.get('/rss.xml');
+      expect(response.status()).toBe(200);
+      expect(response.headers()['content-type']).toContain('application/xml');
 
-    const xml = await response.text();
-    const parsedXml = parse(xml);
+      const xml = await response.text();
+      const parser = new XMLParser();
+      const parsedXml = parser.parse(xml);
 
-    const hasChannelTag = parsedXml.querySelectorAll('channel').length > 0;
-    expect(hasChannelTag).toBe(true);
+      const hasChannelTag = parsedXml.rss.channel !== undefined;
+      expect(hasChannelTag).toBe(true);
 
-    const items = Array.from(parsedXml.querySelectorAll('channel > item')).slice(0, 20);
-    expect(items.length).toBeGreaterThan(0);
+      const items = parsedXml.rss.channel.item.slice(0, 20);
+      expect(items.length).toBeGreaterThan(0);
 
-    items.forEach((item) => {
-      const description = item.querySelector('description');
-      const title = item.querySelector('title');
-      const pubDate = item.querySelector('pubDate');
-      const guid = item.querySelector('guid');
+      items.forEach((item) => {
+        const description = item.description;
+        const title = item.title;
+        const pubDate = item.pubDate;
+        const guid = item.guid;
 
-      expect(description).not.toBeNull();
-      expect(title).not.toBeNull();
-      console.log(pubDate, description)
-      expect(pubDate).not.toBeNull();
-      expect(guid).not.toBeNull();
+        expect(description).not.toBeNull();
+        expect(title).not.toBeNull();
+        expect(pubDate).not.toBeNull();
+        expect(guid).not.toBeNull();
+      });
     });
-  });
+  }
 });
