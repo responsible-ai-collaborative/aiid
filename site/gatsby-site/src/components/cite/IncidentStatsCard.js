b6f344d7c15108ea2c62534fa6c895f4384bad91
Luna McNulty <l.mcnulty@riseup.net>
2024-07-12 16:37:55 -0400

    Add styling to incident stats table

--- site/gatsby-site/src/components/cite/IncidentStatsCard.js
+++ site/gatsby-site/src/components/cite/IncidentStatsCard.js
@@ -51,16 +51,24 @@ const IncidentStatsCard = ({
           <Trans>Incident Stats</Trans>
         </h4>
       </Card.Header>
-      <Card.Body className="grid max-w-full p-5 grid-cols-2 lg:grid-cols-1fr-3fr">
-        {STATS.map((stat) => (
+      <div className="p-0 m-0 grid max-w-full grid-cols-2 lg:grid-cols-1fr-3fr [&>*:nth-child(4n)]:bg-gray-50">
+        {STATS.map((stat, i) => (
           <Fragment key={stat.key}>
-            <div className="pr-4 my-0.5">{stat.label}</div>
-            <div>{stats[stat.key]}</div>
+            <div
+              className={`p-4 bg-gray-100 border-1 ${i == 0 ? 'border-t-0' : ''} border-gray-200`}
+            >
+              {stat.label}
+            </div>
+            <div className={`p-4 border-1 ${i == 0 ? 'border-t-0' : ''} border-gray-200`}>
+              {stats[stat.key]}
+            </div>
           </Fragment>
         ))}
 
-        <div className="pr-4 my-0.5">Applied Taxonomies</div>
-        <div>
+        <div className={`p-4 bg-gray-100 border-1 border-gray-200  rounded-bl`}>
+          Applied Taxonomies
+        </div>
+        <div className={`p-4 border-1 border-gray-200 border-b-0 rounded-br`}>
           {taxonomiesWithClassifications.map((t, i) => {
             const color = { CSETv1: 'orange', GMF: 'blue' }[t] || 'gray';
 
@@ -81,7 +89,7 @@ const IncidentStatsCard = ({
             );
           })}
         </div>
-      </Card.Body>
+      </div>
     </Card>
   );
 };
