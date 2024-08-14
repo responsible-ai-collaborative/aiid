a3e0e114453cd89fcb46a0a40dbc0bafed3936a9
Clara Youdale <clara@botsfactory.io>
2024-08-14 15:39:56 -0300

    Add new user query to not fetch adminData

--- site/gatsby-site/src/graphql/users.js
+++ site/gatsby-site/src/graphql/users.js
@@ -35,6 +35,23 @@ export const FIND_USERS_BY_ROLE = gql(`
       userId
       first_name
       last_name
+      adminData {
+        email
+        disabled
+        creationDate
+        lastAuthenticationDate
+      }
+    }
+  }
+`);
+
+export const FIND_USERS_BY_ROLE_NO_ADMIN_DATA = gql(`
+  query FindUsersByRoleNoAdminData($role: [String!]) {
+    users(filter: { roles: { IN: $role } }) {
+      roles
+      userId
+      first_name
+      last_name
     }
   }
 `);
