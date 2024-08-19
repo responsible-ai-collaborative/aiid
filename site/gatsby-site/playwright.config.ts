1e6a4ae71e91d743a1f81ab79250a2859dc7b52e
Cesar Varela <mail@cesarvarela.com>
2024-08-19 20:56:20 -0300

    accept npx

--- site/gatsby-site/playwright.config.ts
+++ site/gatsby-site/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['blob'], ['line', { printSteps: true }]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -75,7 +75,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx pm2 start npm --name "web-server" -- run start',
+    command: 'npx -y pm2 start npm --name "web-server" -- run serve && npx pm2 logs "web-server"',
     url: 'http://localhost:8000',
     reuseExistingServer: !process.env.CI,
   },
