c4b73cc6487099015a05437cab0aee8008add76c
Cesar Varela <mail@cesarvarela.com>
2024-08-28 18:41:10 -0300

    Remove slow clause

--- site/gatsby-site/playwright/e2e-full/incidents/new.spec.ts
+++ site/gatsby-site/playwright/e2e-full/incidents/new.spec.ts
@@ -12,8 +12,6 @@ test.describe('New Incident page', () => {
 
     await init();
 
-    test.slow();
-
     await login(process.env.E2E_ADMIN_USERNAME, process.env.E2E_ADMIN_PASSWORD, { customData: { roles: ['admin'], first_name: 'John', last_name: 'Doe' } });
 
     await page.goto(url);
