7991590c1228473b7098f2350f0185e011f18e82
Cesar Varela <mail@cesarvarela.com>
2024-08-29 19:31:19 -0300

    Undo changes

--- site/gatsby-site/playwright/utils.ts
+++ site/gatsby-site/playwright/utils.ts
@@ -248,19 +248,9 @@ export async function listFiles(directoryPath: string): Promise<string[]> {
 export async function fillAutoComplete(page: Page, selector: string, sequence: string, target: string) {
 
     await expect(async () => {
-
-        await page.waitForTimeout(500);
-
         await page.locator(selector).clear();
-
-        await page.waitForTimeout(500);
-
-        await page.locator(selector).focus();
-
-        await page.keyboard.type(sequence, {delay: 100});
-
-        await page.getByText(target).first().click({ timeout: 2000 });
-
-        await page.mouse.click(0, 0);
+        await page.waitForTimeout(1000);
+        await page.locator(selector).pressSequentially(sequence, { delay: 500 });
+        await page.getByText(target).click({ timeout: 1000 });
     }).toPass();
 }
