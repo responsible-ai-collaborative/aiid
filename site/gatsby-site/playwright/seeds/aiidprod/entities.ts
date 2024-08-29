cfeed12a3b637b5aa127e52e5949546b98b18658
Cesar Varela <mail@cesarvarela.com>
2024-08-29 16:49:29 -0300

    Migrate entityEdit to playwright (#3047)
    * Migrate entityEdit to playwright
    
    * Fix date_modified time check
    
    * Delete entityEdit.cy.js
    
    * Add missing waitForRequest
    
    * Fix entities seed
    
    * Fix entityEdit test
    
    * Add date_modified to entities and move spec to e2e-full
    
    ---------
    
    Co-authored-by: Clara Youdale <clara@botsfactory.io>

--- site/gatsby-site/playwright/seeds/aiidprod/entities.ts
+++ site/gatsby-site/playwright/seeds/aiidprod/entities.ts
@@ -6,36 +6,42 @@ const entities: DBEntity[] = [
     {
         entity_id: 'entity1',
         name: 'Entity 1',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
 
     {
         entity_id: 'entity2',
         name: 'Entity 2',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
 
     {
         entity_id: 'entity3',
         name: 'Entity 3',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
     {
         entity_id: 'starbucks',
         name: 'Starbucks',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
 
     {
         entity_id: 'kronos',
         name: 'Kronos',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
 
     {
         entity_id: 'starbucks-employees',
         name: 'Starbucks Employees',
-        created_at: new Date(1609459200000).toString(),
+        created_at: new Date(1609459200000).toISOString(),
+        date_modified: new Date(1609459200000).toISOString(),
     },
 ]
 
