f50eef72f19b724e7e1dea120f06c0498e26908f
Cesar Varela <mail@cesarvarela.com>
2024-09-02 18:42:28 -0300

    fix imports

--- site/gatsby-site/playwright/e2e-full/submit.spec.ts
+++ site/gatsby-site/playwright/e2e-full/submit.spec.ts
@@ -1,10 +1,10 @@
 import { expect } from '@playwright/test';
 import { gql } from 'graphql-tag';
 import { isArray } from 'lodash';
-import { init, seedCollection } from '../../memory-mongo';
-import { fillAutoComplete, query, setEditorText, test } from '../../utils';
+import { init, seedCollection } from '../memory-mongo';
+import { fillAutoComplete, query, setEditorText, test } from '../utils';
 import config from '../../config';
-import { DBSubmission } from '../../seeds/aiidprod/submissions';
+import { DBSubmission } from '../seeds/aiidprod/submissions';
 import { ObjectId } from 'mongodb';
 
 test.describe('Submitted reports', () => {
