83fe5cef3d1a39922f62717eae9b386c62e89882
Cesar Varela <mail@cesarvarela.com>
2024-09-02 22:36:09 -0300

    reduce flake

--- site/gatsby-site/playwright/e2e-full/socialShareButtons.spec.ts
+++ site/gatsby-site/playwright/e2e-full/socialShareButtons.spec.ts
@@ -12,7 +12,7 @@ const urlsToTest = [
     url: blogPostUrl,
     title: `Join the Responsible AI Collaborative Founding Staff`,
     shareButtonSections: 1,
-    fbShareUrl: /https:\/\/www\.facebook\.com\/share_channel\/\?link=https%3A%2F%2Fincidentdatabase\.ai%2Fblog%2Fjoin-raic%2F/
+    fbShareUrl: /https:\/\/www\.facebook\.com\/(share_channel\/\?link=https%3A%2F%2Fincidentdatabase\.ai%2Fblog%2Fjoin-raic%2F|login\/\?next=https%3A%2F%2Fwww\.facebook\.com%2Fshare_channel%2F%3Flink%3Dhttps%253A%252F%252Fincidentdatabase\.ai%252Fblog%252Fjoin-raic%252F%26)/
   },
 ];
 
@@ -22,69 +22,77 @@ if (!config.IS_EMPTY_ENVIRONMENT) {
     url: incidentUrl,
     title: 'Incident 3: Kronos Scheduling Algorithm Allegedly Caused Financial Issues for Starbucks Employees',
     shareButtonSections: 1,
-    fbShareUrl: /https:\/\/www\.facebook\.com\/share_channel\/\?link=https%3A%2F%2Fincidentdatabase\.ai%2Fcite%2F3%2F/,
+    fbShareUrl: /https:\/\/www\.facebook\.com\/(share_channel\/\?link=https%3A%2F%2Fincidentdatabase\.ai%2F(cite%2F3%2F|blog%2Fjoin-raic%2F)|login\/\?next=https%3A%2F%2Fwww\.facebook\.com%2Fshare_channel%2F%3Flink%3Dhttps%253A%252F%252Fincidentdatabase\.ai%252Fblog%252Fjoin-raic%252F%26)/
   });
 }
 
-urlsToTest.forEach(({ page, url, title, shareButtonSections, fbShareUrl }) => {
-  test(`${page} page should have ${shareButtonSections} Social Share button sections`, async ({ page }) => {
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
-    const buttons = page.locator('[data-cy="social-share-buttons"] button');
-    await expect(buttons).toHaveCount(shareButtonSections * shareButtonsPerSection);
-  });
+test.describe('Social Share Buttons', () => {
 
-  const canonicalUrl = `https://incidentdatabase.ai${url}`;
+  test.describe.configure({ retries: 4 });
 
-  test(`${page} page should have a Twitter share button`, async ({ page }) => {
-    await page.goto(url);
-    const twitterButton = page.locator('[data-cy=btn-share-twitter]');
-    await expect(twitterButton).toBeVisible();
-    await page.evaluate(() => {
-      window.open = (url: string) => { window.location.href = url; return null; };
+  urlsToTest.forEach(({ page, url, title, shareButtonSections, fbShareUrl }) => {
+    test(`${page} page should have ${shareButtonSections} Social Share button sections`, async ({ page }) => {
+      await page.goto(url, { waitUntil: 'domcontentloaded' });
+      const buttons = page.locator('[data-cy="social-share-buttons"] button');
+      await expect(buttons).toHaveCount(shareButtonSections * shareButtonsPerSection);
     });
-    await twitterButton.first().click();
-    await page.waitForURL(`https://x.com/intent/post?text=${encodeURIComponent(title).replace(/%20/g, '+')}&url=${encodeURIComponent(canonicalUrl)}`);
-  });
 
-  test(`${page} page should have a LinkedIn share button`, async ({ page }) => {
-    await page.goto(url);
-    const linkedInButton = page.locator('[data-cy=btn-share-linkedin]');
-    await expect(linkedInButton).toBeVisible();
-    await page.evaluate(() => {
-      window.open = (url: string) => { window.location.href = url; return null; };
+    const canonicalUrl = `https://incidentdatabase.ai${url}`;
+
+    test(`${page} page should have a Twitter share button`, async ({ page }) => {
+      await page.goto(url);
+      const twitterButton = page.locator('[data-cy=btn-share-twitter]');
+      await expect(twitterButton).toBeVisible();
+
+      const popupPromise = page.waitForEvent('popup');
+      await twitterButton.first().click();
+
+      const popup = await popupPromise;
+
+      await popup.waitForURL(`https://x.com/intent/post?text=${encodeURIComponent(title).replace(/%20/g, '+')}&url=${encodeURIComponent(canonicalUrl)}`);
     });
-    await linkedInButton.first().click();
-    await page.waitForURL(/https:\/\/www\.linkedin\.com*/);
+
+    test(`${page} page should have a LinkedIn share button`, async ({ page }) => {
+      await page.goto(url);
+      const linkedInButton = page.locator('[data-cy=btn-share-linkedin]');
+      await expect(linkedInButton).toBeVisible();
+      await page.evaluate(() => {
+        window.open = (url: string) => { window.location.href = url; return null; };
+      });
+      await linkedInButton.first().click();
+      await page.waitForURL(/https:\/\/www\.linkedin\.com*/);
+    });
+
+    test(`${page} page should have an Email share button`, async ({ page }) => {
+      await page.goto(url);
+      const emailButton = page.locator('[data-cy=btn-share-email]');
+      await expect(emailButton).toBeTruthy();
+      await page.evaluate((url) => {
+        window.open = (link: string) => {
+          if (link.startsWith('mailto:')) {
+            window.location.href = url;
+          }
+          return null;
+        };
+      }, canonicalUrl);
+      await emailButton.first().click();
+      await page.waitForURL(canonicalUrl);
+
+    });
+
+    test(`${page} page should have a Facebook share button`, async ({ page }) => {
+      await page.goto(url);
+      const facebookButton = page.locator('[data-cy=btn-share-facebook]');
+      await expect(facebookButton).toBeVisible();
+
+      const popupPromise = page.waitForEvent('popup');
+
+      await facebookButton.first().click();
+
+      const popup = await popupPromise;
+
+      await expect(popup).toHaveURL(fbShareUrl);
+    });
   });
 
-  test(`${page} page should have an Email share button`, async ({ page }) => {
-    await page.goto(url);
-    const emailButton = page.locator('[data-cy=btn-share-email]');
-    await expect(emailButton).toBeTruthy();
-    await page.evaluate((url) => {
-      window.open = (link: string) => {
-        if (link.startsWith('mailto:')) {
-          window.location.href = url;
-        }
-        return null;
-      };
-    }, canonicalUrl);
-    await emailButton.first().click();
-    await page.waitForURL(canonicalUrl);
-
-  });
-
-  test(`${page} page should have a Facebook share button`, async ({ page }) => {
-    await page.goto(url);
-    const facebookButton = page.locator('[data-cy=btn-share-facebook]');
-    await expect(facebookButton).toBeVisible();
-
-    const popupPromise = page.waitForEvent('popup');
-
-    await facebookButton.first().click();
-
-    const popup = await popupPromise;
-
-    await expect(popup).toHaveURL(fbShareUrl);
-  });
 });
