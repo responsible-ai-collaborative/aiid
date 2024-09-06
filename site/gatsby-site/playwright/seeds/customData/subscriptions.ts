1569920356afa477ab714aa46871357235a5f786
Cesar Varela <mail@cesarvarela.com>
2024-09-06 17:59:16 -0300

    Add subscription seeds

--- site/gatsby-site/playwright/seeds/customData/subscriptions.ts
+++ site/gatsby-site/playwright/seeds/customData/subscriptions.ts
@@ -2,23 +2,30 @@ import { ObjectId } from 'bson';
 import { Subscription } from '../../../server/generated/graphql'
 
 export type DBSubscription = Omit<Subscription, 'entityId' | 'incident_id' | 'userId'>
-    & { entityId: string, incident_id: number, userId: string };
+    & { entityId?: string, incident_id?: number, userId?: string };
 
 const items: DBSubscription[] = [
     {
         _id: new ObjectId("619b47eb5eed5334edfa3bd7"),
         entityId: "entity1",
-        incident_id: 1,
+        incident_id: undefined,
         type: "entity",
         userId: "user1",
     },
     {
         _id: new ObjectId("619b47eb5eed5334edfa3bd9"),
-        entityId: "entity1",
+        entityId: undefined,
         incident_id: 1,
-        type: "entity",
+        type: "incident",
         userId: "user1",
     },
+    {
+        _id: new ObjectId('60a7c5b7b4f5b8a6d8f9c7e7'),
+        entityId: undefined,
+        incident_id: 1,
+        type: "incident",
+        userId: 'user1',
+    }
 ]
 
 export default items;
