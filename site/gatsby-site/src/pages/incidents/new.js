6cdb946e20f51f06198145430cf84f8f14b4c201
Cesar Varela <mail@cesarvarela.com>
2024-09-20 18:27:17 -0300

    Fix new incident form

--- site/gatsby-site/src/pages/incidents/new.js
+++ site/gatsby-site/src/pages/incidents/new.js
@@ -149,11 +149,11 @@ function NewIncidentPage() {
           AllegedHarmedOrNearlyHarmedParties: AllegedHarmedOrNearlyHarmedParties.map(
             (entity) => entity.entity_id
           ),
-          editor_notes,
+          editor_notes: editor_notes ?? '',
           editors: editors.map((editor) => editor.userId),
         });
       } else {
-        setInitialValues({ editors: [] });
+        setInitialValues({ editors: [], editor_notes: '' });
       }
     }
   }, [incidentToCloneData]);
