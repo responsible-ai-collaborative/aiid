62e1768e0271e5630f58ace5263b8b0acee59dd1
Pablo Costa <pablo@botsfactory.io>
2024-10-02 19:35:09 -0300

    Refactor environment variable checks to use string coercion

--- site/gatsby-site/envVars.js
+++ site/gatsby-site/envVars.js
@@ -1,20 +1,20 @@
 // Server environment variables
 const requiredServerEnvVars = {
-  REALM_API_APP_ID: process.env.REALM_API_APP_ID,
-  REALM_API_GROUP_ID: process.env.REALM_API_GROUP_ID,
-  REALM_API_PRIVATE_KEY: process.env.REALM_API_PRIVATE_KEY,
-  REALM_API_PUBLIC_KEY: process.env.REALM_API_PUBLIC_KEY,
-  REALM_GRAPHQL_API_KEY: process.env.REALM_GRAPHQL_API_KEY,
-  REALM_APP_ID: process.env.REALM_APP_ID,
-  API_MONGODB_CONNECTION_STRING: process.env.API_MONGODB_CONNECTION_STRING,
-  ROLLBAR_POST_SERVER_ITEM_ACCESS_TOKEN: process.env.ROLLBAR_POST_SERVER_ITEM_ACCESS_TOKEN,
+  REALM_API_APP_ID: String(process.env.REALM_API_APP_ID),
+  REALM_API_GROUP_ID: String(process.env.REALM_API_GROUP_ID),
+  REALM_API_PRIVATE_KEY: String(process.env.REALM_API_PRIVATE_KEY),
+  REALM_API_PUBLIC_KEY: String(process.env.REALM_API_PUBLIC_KEY),
+  REALM_GRAPHQL_API_KEY: String(process.env.REALM_GRAPHQL_API_KEY),
+  REALM_APP_ID: String(process.env.REALM_APP_ID),
+  API_MONGODB_CONNECTION_STRING: String(process.env.API_MONGODB_CONNECTION_STRING),
+  ROLLBAR_POST_SERVER_ITEM_ACCESS_TOKEN: String(process.env.ROLLBAR_POST_SERVER_ITEM_ACCESS_TOKEN),
 };
 
 const optionalServerEnvVars = {};
 
 const checkServerEnvVars = () => {
   Object.keys(requiredServerEnvVars).forEach((key) => {
-    if (requiredServerEnvVars[key] === undefined) {
+    if (requiredServerEnvVars[key] === 'undefined') {
       throw new Error(`Required Server environment variable "${key}" is undefined`);
     }
   });
@@ -22,18 +22,18 @@ const checkServerEnvVars = () => {
 
 // Cypress environment variables
 const requiredCypressEnvVars = {
-  E2E_ADMIN_PASSWORD: process.env.E2E_ADMIN_PASSWORD,
-  E2E_ADMIN_USERNAME: process.env.E2E_ADMIN_USERNAME,
+  E2E_ADMIN_PASSWORD: String(process.env.E2E_ADMIN_PASSWORD),
+  E2E_ADMIN_USERNAME: String(process.env.E2E_ADMIN_USERNAME),
 };
 
 const optionalCypressEnvVars = {
-  IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT ?? '',
-  AVAILABLE_LANGUAGES: process.env.GATSBY_AVAILABLE_LANGUAGES ?? '',
+  IS_EMPTY_ENVIRONMENT: String(process.env.IS_EMPTY_ENVIRONMENT ?? ''),
+  AVAILABLE_LANGUAGES: String(process.env.GATSBY_AVAILABLE_LANGUAGES ?? ''),
 };
 
 const checkCypressEnvVars = () => {
   Object.keys(requiredCypressEnvVars).forEach((key) => {
-    if (requiredCypressEnvVars[key] === undefined) {
+    if (requiredCypressEnvVars[key] === 'undefined') {
       throw new Error(`Required Cypress environment variable "${key}" is undefined`);
     }
   });
