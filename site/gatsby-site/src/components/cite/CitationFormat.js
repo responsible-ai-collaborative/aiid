c5f522c79089f0394e48930b7184d5184ff7fd93
Clara Youdale <clara@botsfactory.io>
2024-11-11 12:13:32 -0300

    Remove console log

--- site/gatsby-site/src/components/cite/CitationFormat.js
+++ site/gatsby-site/src/components/cite/CitationFormat.js
@@ -43,8 +43,6 @@ function CitationFormat({ incidentReports, incident }) {
             {Object.keys(formats).map((format) => {
               const Component = citationTypes[format].default;
 
-              console.log('incidentTitle', incident.title, incident.date, incident.incident_id);
-
               return (
                 <Component
                   key={format}
