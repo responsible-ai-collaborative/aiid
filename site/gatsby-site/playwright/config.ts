0f89fdfb94ea05a2370fa8ad3c0ffecd832b3ed4
Cesar Varela <mail@cesarvarela.com>
2024-11-19 21:40:39 -0300

    tmp add new variables

--- site/gatsby-site/playwright/config.ts
+++ site/gatsby-site/playwright/config.ts
@@ -3,7 +3,8 @@ type ConfigType = {
     E2E_ADMIN_USERNAME: string;
     IS_EMPTY_ENVIRONMENT: string;
     AVAILABLE_LANGUAGES?: string;
-    [key: string]: string;
+    NEXTAUTH_URL?: string;
+    NEXTAUTH_SECRET?: string;
 };
 
 const config: ConfigType = {
@@ -11,10 +12,14 @@ const config: ConfigType = {
     E2E_ADMIN_USERNAME: process.env.E2E_ADMIN_USERNAME!,
     IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT ?? '',
     AVAILABLE_LANGUAGES: process.env.GATSBY_AVAILABLE_LANGUAGES ?? '',
+
+    // TODO: add theses values to the workflow
+    NEXTAUTH_URL: 'http://localhost:8000',
+    NEXTAUTH_SECRET: '678x1irXYWeiOqTwCv1awvkAUbO9eHa5xzQEYhxhMms=',
 }
 
 Object.keys(config).forEach((key) => {
-    if (config[key] === undefined) {
+    if (config[key as keyof ConfigType] === undefined) {
         throw new Error(`Config property ${key} is undefined`);
     }
 });
