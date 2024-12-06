0480df361526774f9729dd0ba513b326b4507668
Clara Youdale <clara@botsfactory.io>
2024-12-06 12:02:17 -0300

    Base gtag tests on fixture to run in prod or other envs

--- site/gatsby-site/playwright/e2e/unit/gtag.spec.ts
+++ site/gatsby-site/playwright/e2e/unit/gtag.spec.ts
@@ -2,40 +2,39 @@ import { expect } from '@playwright/test';
 import config from '../../../config';
 import { test } from '../../utils';
 
-const productionUrl = config.gatsby.siteUrl;
 const googleTrackingId = config.gatsby.gaTrackingId;
 
 test.describe('Google Analytics Tracking', () => {
-  test('Should track gtag only on production', async ({ page }) => {
+  test('Should track gtag only on production', async ({ page, runOnlyInProduction }) => {
     await page.goto('/');
-
-    const currentUrl = page.url();
-
-    const isProduction = currentUrl.startsWith(productionUrl);
 
     const hasGoogleTracking = await page.locator(`script[src*="https://www.googletagmanager.com/gtag/js?id=${googleTrackingId}"]`).count();
 
-    if (isProduction) {
-      expect(hasGoogleTracking).toBeGreaterThan(0);
+    expect(hasGoogleTracking).toBeGreaterThan(0);
 
-      const [trackingRequest] = await Promise.all([
-        page.waitForRequest((request) => request.url().includes(`https://www.google-analytics.com`) && request.method() === 'POST'),
-      
-      ]);
+    const [trackingRequest] = await Promise.all([
+      page.waitForRequest((request) => request.url().includes(`https://www.google-analytics.com`) && request.method() === 'POST'),
 
-      expect(trackingRequest).toBeTruthy();
-    } else {
-      expect(hasGoogleTracking).toBe(0);
+    ]);
 
-      let trackingRequestMade = false;
-      page.on('request', (request) => {
-        if (request.url().includes(`https://www.google-analytics.com`) && request.method() === 'POST') {
-          trackingRequestMade = true;
-        }
-      });
+    expect(trackingRequest).toBeTruthy();
+  });
 
-      await page.reload();
-      expect(trackingRequestMade).toBe(false);
-    }
+
+  test('Should not track gtag outside production', async ({ page, runAnywhereExceptProduction }) => {
+    await page.goto('/');
+
+    const hasGoogleTracking = await page.locator(`script[src*="https://www.googletagmanager.com/gtag/js?id=${googleTrackingId}"]`).count();
+    expect(hasGoogleTracking).toBe(0);
+
+    let trackingRequestMade = false;
+    page.on('request', (request) => {
+      if (request.url().includes(`https://www.google-analytics.com`) && request.method() === 'POST') {
+        trackingRequestMade = true;
+      }
+    });
+
+    await page.reload();
+    expect(trackingRequestMade).toBe(false);
   });
 });
