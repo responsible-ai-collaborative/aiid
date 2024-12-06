0b82bad1cee1ede162985b39c28eb5d6212712f1
Pablo Costa <pablo@botsfactory.io>
2024-12-06 18:15:47 -0300

    Add Algolia mock to Playwright

--- site/gatsby-site/playwright/utils.ts
+++ site/gatsby-site/playwright/utils.ts
@@ -6,6 +6,7 @@ import assert from 'node:assert';
 import fs from 'fs';
 import path from 'path';
 import * as memoryMongo from './memory-mongo';
+import { algoliaMock } from './fixtures/algoliaMock';
 
 declare module '@playwright/test' {
     interface Request {
@@ -290,4 +291,32 @@ export function getLanguages() {
     { code: 'fr', hrefLang: 'fr', name: 'French', localName: 'Français', langDir: 'ltr', dateFormat: 'DD-MM-YYYY' },
     { code: 'ja', hrefLang: 'ja', name: 'Japanese', localName: '日本語', langDir: 'ltr', dateFormat: 'YYYY/MM/DD' },
   ];
+}
+
+// TODO: this mock should pull from the database instead of being hardcoded
+export async function mockAlgolia(page: Page) {
+
+    await page.route('**/*.algolia.net/1/indexes/*/queries*', async route => {
+        const response = await route.fetch();
+
+        await route.fulfill({
+            status: 200,
+            json: algoliaMock,
+            headers: {
+                ...response.headers(),
+            }
+        });
+    });
+
+    await page.route('**/*.algolianet.com/1/indexes/*/queries*', async route => {
+        const response = await route.fetch();
+
+        await route.fulfill({
+            status: 200,
+            json: algoliaMock,
+            headers: {
+                ...response.headers(),
+            }
+        });
+    });
 }
