09f1eef51efb10a451f1d1b40241dd8a5fa0fdc0
Cesar Varela <mail@cesarvarela.com>
2024-12-11 16:52:01 -0300

    Undo seed change

--- site/gatsby-site/playwright/seeds/customData/users.ts
+++ site/gatsby-site/playwright/seeds/customData/users.ts
@@ -10,7 +10,7 @@ const users: DBUser[] = [
     },
     {
         _id: new ObjectId("619b47eb5eed5334edfa3bd9"),
-        userId: "648a4c0eaf3f54bf50f018a3",
+        userId: "619b47ea5eed5334edfa3bbc",
         roles: ["admin"],
         first_name: "Sean",
         last_name: "McGregor"
