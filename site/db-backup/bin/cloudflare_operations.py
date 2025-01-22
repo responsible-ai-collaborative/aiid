1140f42bef500bbaccd73bbf82790a182dd8d16e
Pablo Costa <pablo@botsfactory.io>
2025-01-21 21:25:30 -0300

    Update db-backup workflow to pin boto3 version and clean up cloudflare client configuration

--- site/db-backup/bin/cloudflare_operations.py
+++ site/db-backup/bin/cloudflare_operations.py
@@ -4,8 +4,6 @@ import sys
 
 import argparse
 import boto3
-from botocore.config import Config
-
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -61,23 +59,12 @@ def parse_arguments():
 def create_cloudflare_client(account_id, access_key, secret_key, region="auto"):
     endpoint_url = f"https://{account_id}.r2.cloudflarestorage.com"
     
-    config = Config(
-        s3={
-            'payload_signing_enabled': False,
-            'use_accelerate_endpoint': False
-        },
-        signature_version='s3v4',
-        request_checksum_calculation='when_required',
-        response_checksum_validation='when_required'
-    )
-    
     cloudflare_client = boto3.client(
         service_name="s3",
         endpoint_url=endpoint_url,
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_key,
         region_name=region,
-        config=config
     )
     return cloudflare_client
 
