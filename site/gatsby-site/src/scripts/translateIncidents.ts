6b6a3b2f018806e20abdf7bb6d049793db9300fc
Pablo Costa <pablo@botsfactory.io>
2025-02-05 20:11:10 -0300

    Add IncidentTranslator and translation Typescript scripts for MongoDB incidents

---
+++ site/gatsby-site/src/scripts/translateIncidents.ts
@@ -0,0 +1,61 @@
+//import 'dotenv/config'; // Alternatively: import dotenv from 'dotenv'; dotenv.config();
+import config from '../../config';
+
+import { MongoClient } from 'mongodb';
+import { Translate } from '@google-cloud/translate/build/src/v2';
+import IncidentTranslator from './incidentTranslator';
+import { getLanguages } from '../../i18n';
+
+export const run = async () => {
+  console.log('Translating incidents...');
+
+  let mongoClient: MongoClient | null = null;
+
+  try {
+    mongoClient = new MongoClient(config.mongodb.translationsConnectionString || '');
+    try {
+      await mongoClient.connect();
+      
+    } catch (mongoError: any) {
+      throw new Error(`Error connecting to MongoDB: ${mongoError.message}`);
+    }
+
+    if (!config.i18n.translateApikey) {
+      throw new Error('Google Translate API (GOOGLE_TRANSLATE_API_KEY) key is missing.');
+    }
+
+    const translateClient = new Translate({ key: config.i18n.translateApikey });
+
+    const translator = new IncidentTranslator({
+      mongoClient,
+      translateClient,
+      languages: getLanguages().filter((language) => language.code !== 'en').map(l => l.code), // We do not translate to English since it's the source language
+      reporter: {
+        log: console.log,
+        error: console.error,
+        warn: console.warn,
+      },
+    });
+
+    // Execute the translation process
+    await translator.run();
+
+    console.log('Translation completed successfully.');
+  } catch (error: any) {
+    console.error('Error during the translation process:', error.message);
+    process.exit(1);
+  } finally {
+    if (mongoClient) {
+      try {
+        await mongoClient.close();
+        console.log('MongoDB connection closed gracefully.');
+      } catch (closeError: any) {
+        console.error('Error closing MongoDB connection:', closeError.message);
+      }
+    }
+  }
+};
+
+if (require.main === module) {
+  run();
+}
