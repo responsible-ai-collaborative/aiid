9b484727a4d1ce0b244e8faf3579f3b20ce6c956
Cesar Varela <mail@cesarvarela.com>
2025-02-06 18:53:54 -0300

    Add configurable sentry environment.

--- site/gatsby-site/sentry-instrumentation.ts
+++ site/gatsby-site/sentry-instrumentation.ts
@@ -2,10 +2,14 @@ import * as  Sentry from '@sentry/aws-serverless'
 
 if (process.env.SENTRY_DSN) {
 
+    const environment = process.env.SENTRY_ENVIRONMENT || 'development';
+
     Sentry.init({
         dsn: process.env.SENTRY_DSN,
         // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
         tracesSampleRate: 0.1, // one out of ten transactions
+        environment,
+        debug: environment === 'development',
     });
 }
 
