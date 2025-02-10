bbbfe27ab17503ce25668b6c3760344e027edccd
Cesar Varela <mail@cesarvarela.com>
2025-02-10 17:35:46 -0300

    Move tests to be either e2e of probes

--- site/gatsby-site/playwright/e2e/login.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../utils';
-
-test.describe('Login', () => {
-  const url = '/login';
-
-  test('Should successfully load login page', async ({ page }) => {
-    await page.goto(url);
-  });
-});
