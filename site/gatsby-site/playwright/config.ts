0d7321b9df18af9b85286482716b74d351ab753a
Cesar Varela <mail@cesarvarela.com>
2025-02-21 20:30:00 -0300

    Fix small issues related to playwright tests refactor (#3396)
    * Fix wrong path
    
    * Improve on EMPTY_ENVIRONMENT ambiguity

--- site/gatsby-site/playwright/config.ts
+++ site/gatsby-site/playwright/config.ts
@@ -9,11 +9,11 @@ type ConfigType = {
 };
 
 const config: ConfigType = {
-    IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT ?? '',
-    AVAILABLE_LANGUAGES: process.env.GATSBY_AVAILABLE_LANGUAGES ?? '',
+    IS_EMPTY_ENVIRONMENT: process.env.IS_EMPTY_ENVIRONMENT || 'false',
+    AVAILABLE_LANGUAGES: process.env.GATSBY_AVAILABLE_LANGUAGES!,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
-    SITE_URL: process.env.SITE_URL ?? '',
+    SITE_URL: process.env.SITE_URL,
 }
 
 validateEnv(config);
