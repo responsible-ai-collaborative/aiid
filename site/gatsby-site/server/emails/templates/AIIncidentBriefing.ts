5b827770537db7e1c5ead16695a79f24006747b2
Clara Youdale <clara@botsfactory.io>
2025-02-24 09:50:25 -0300

    Enhance AI Incident Briefing email template with improved styling and structure

--- site/gatsby-site/server/emails/templates/AIIncidentBriefing.ts
+++ site/gatsby-site/server/emails/templates/AIIncidentBriefing.ts
@@ -15,6 +15,29 @@ const getEmailTemplate = () => {
     border-bottom: 1px solid #ccc;
     font-size: 90%;
   `);
+  
+  const incidentStyle = ignoreWhitespace(`
+    padding: 32px;
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    font-size: 90%;
+  `);
+
+  const incidentImageStyle = ignoreWhitespace(`
+    border-radius: 8px;
+    float: right; 
+    margin-top: 16px;
+    margin-left: 16px;
+    margin-bottom: 16px;
+  `);
+
+  const entityStyle = ignoreWhitespace(`
+    border: 1px solid;
+    text-decoration: none;
+    padding: 2px 6px;
+    margin: 0px 2px;
+    border-radius: 4px;
+  `);
 
   return insertContent(
     `
@@ -29,17 +52,33 @@ const getEmailTemplate = () => {
 
       <div style="${sectionStyle}">
         <h2>New Incidents & Reports</h2>
-        {{#if newIncidents}}
-          {{#each newIncidents}}
-            <p>
-              <a href="{{this.url}}">Incident {{this.id}}: {{this.title}}</a>
-              <br>
-              <span style="font-size: 85%;">{{this.date}}</span>
-            </p>
-          {{/each}}
-        {{else}}
+        {% if newIncidents %}
+          <ul>
+            {% for incident in newIncidents %}
+              <li>
+                <div style="${incidentStyle}">
+                  <h1 style="font-size: 100%; margin-top: 0px;">
+                    Incident {{ incident.id }}: {{ incident.title }}
+                  </h1>
+
+                  <p style="font-size: 85%;">{{ incident.date }}</p>
+
+                  <p style="font-size: 85%;">
+                    {{ incident.description }}
+                  </p>
+                  <p style="margin-bottom: 0px; line-height: 1.75;">
+                    <strong>Alleged</strong>:
+                    <span style="${entityStyle}">{{ incident.developers }}</span> developed an AI system deployed by
+                    <span style="${entityStyle}">{{ incident.deployers }}</span> which harmed 
+                    <span style="${entityStyle}">{{ incident.entitiesHarmed }}</span>.
+                  </p>
+                </div>
+              </li>
+            {% endfor %}
+          </ul>
+        {% else %}
           <p>No new incidents were reported this week.</p>
-        {{/if}}
+        {% endif %}
       </div>
 
       <div style="${sectionStyle}">
