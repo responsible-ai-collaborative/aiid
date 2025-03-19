a1581c8506d7d03a2d23456bbfa3ec258f3cd950
Cesar Varela <mail@cesarvarela.com>
2025-03-18 22:08:29 -0300

    Delete unused query

--- site/gatsby-site/src/graphql/machineClassifications.js
+++ site/gatsby-site/src/graphql/machineClassifications.js
@@ -1,20 +1,5 @@
 import { gql } from '../../server/generated/gql';
 
-export const MACHINE_CLASSIFICATION = gql(`
-  mutation MachineClassification($text: String!, $taxonomy: String!) {
-    machineClassification(text: $text, taxonomy: $taxonomy) {
-      confidence
-      classification {
-        namespace
-        attributes {
-          short_name
-          value_json
-        }
-      }
-    }
-  }
-`);
-
 export const MACHINE_CLASSIFICATION_BY_INCIDENT_ID = gql(`
   mutation MachineClassificationByIncidentId($incident_id: Int!, $taxonomy: String!) {
     machineClassificationByIncidentId(incident_id: $incident_id, taxonomy: $taxonomy) {
