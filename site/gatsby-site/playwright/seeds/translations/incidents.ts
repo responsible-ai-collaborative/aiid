b49ff112d4b2146aaddedf65f7b491eb26da0661
Pablo Costa <pablo@botsfactory.io>
2025-03-26 19:28:08 -0300

    Add the "AI translated" badge on the incident page

--- site/gatsby-site/playwright/seeds/translations/incidents.ts
+++ site/gatsby-site/playwright/seeds/translations/incidents.ts
@@ -20,9 +20,9 @@ const items: DBIncidentTranslation[] = [
     },
     {
         _id: new ObjectId("62f40cd14016f5858d723861"),
-        incident_id: 2,
+        incident_id: 1,
         language: "fr",
-        title: "Titre de l'incident 2",
+        title: "Titre de l'incident 1",
         description: "Description du deuxième incident",
         created_at: new Date(2024, 0, 2),
     },
