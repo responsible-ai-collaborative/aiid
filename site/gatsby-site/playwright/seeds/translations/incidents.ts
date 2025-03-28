18b9d184ef854ff4fd04e32c84e0a0d7163c9079
Pablo Costa <pablo@botsfactory.io>
2025-03-28 15:54:37 -0300

    Remove `created_at` from `incidents` collection

--- site/gatsby-site/playwright/seeds/translations/incidents.ts
+++ site/gatsby-site/playwright/seeds/translations/incidents.ts
@@ -6,7 +6,6 @@ type DBIncidentTranslation = {
     language: string;
     title: string;
     description: string;
-    created_at: Date;
 };
 
 const items: DBIncidentTranslation[] = [
@@ -16,7 +15,6 @@ const items: DBIncidentTranslation[] = [
         language: "es",
         title: "Título del Incidente 1",
         description: "Descripción del incidente 1",
-        created_at: new Date(2024, 0, 1),
     },
     {
         _id: new ObjectId("62f40cd14016f5858d723861"),
@@ -24,7 +22,6 @@ const items: DBIncidentTranslation[] = [
         language: "fr",
         title: "Titre de l'incident 1",
         description: "Description du deuxième incident",
-        created_at: new Date(2024, 0, 2),
     },
 ];
 
