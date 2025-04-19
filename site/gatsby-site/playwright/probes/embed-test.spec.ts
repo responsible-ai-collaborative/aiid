0e774d3224c6d29ffec5a2eb72737ebbf0e3355d
cesar varela <mail@cesarvarela.com>
2025-04-18 21:57:27 -0300

    update tests

--- site/gatsby-site/playwright/probes/embed-test.spec.ts
+++ site/gatsby-site/playwright/probes/embed-test.spec.ts
@@ -6,11 +6,15 @@ test.describe('AIID Embed Test Page', () => {
     await page.goto(`https://aiid-embed-test.netlify.app?embedBaseURL=${config.SITE_URL}`);
 
     // Check for the first link
-    const link1 = page.locator('a:has-text("See it on the AIID #1")');
+    const link1 = page.locator('a:has-text("View incident #1 on AIID")');
     await expect(link1).toHaveAttribute('href', `${config.SITE_URL}/cite/1`);
 
     // Check for the second link
-    const link999 = page.locator('a:has-text("See it on the AIID #999")');
+    const link999 = page.locator('a:has-text("View incident #999 on AIID")');
     await expect(link999).toHaveAttribute('href', `${config.SITE_URL}/cite/999`);
+
+    // Check for the third link
+    const link2 = page.locator('a:has-text("Explore incident 2 details")');
+    await expect(link2).toHaveAttribute('href', `${config.SITE_URL}/cite/2`);
   });
 });
