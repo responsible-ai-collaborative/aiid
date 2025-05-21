628ca6e7b3bc408d82a80b4f160cf1d9edae7ae5
cesar varela <mail@cesarvarela.com>
2025-05-21 19:51:19 -0300

    Add more than one button to seeds

--- site/gatsby-site/playwright/seeds/aiidprod/incident_links.ts
+++ site/gatsby-site/playwright/seeds/aiidprod/incident_links.ts
@@ -13,6 +13,11 @@ const incident_links: DBIncidentLink[] = [
         sameAs: 'https://oecd.ai/en/incidents/2025-01-09-99fe',
         source_namespace: 'OECD',
     },
+    {
+        incident_id: 1,
+        sameAs: 'https://oecd.ai/en/incidents/2025-03-20-e2d2',
+        source_namespace: 'OECD',
+    },
 ];
 
 export default incident_links;
