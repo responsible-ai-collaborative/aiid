365da59d98b29ff3d2362530138fa787b12f96b6
Luna McNulty <l.mcnulty@riseup.net>
2025-05-22 14:36:40 -0400

    Prevent description+notes from overflowing w/ long words

--- site/gatsby-site/src/templates/citeTemplate.js
+++ site/gatsby-site/src/templates/citeTemplate.js
@@ -246,6 +246,7 @@ function CiteTemplate({
           <Row>
             <Col>
               <div
+                style={{ 'overflow-wrap': 'anywhere' }}
                 className={`${incident.isTranslated ? 'flex flex-wrap' : ''}`}
                 data-testid="incident-description-section"
               >
@@ -265,7 +266,7 @@ function CiteTemplate({
           {incident.editor_notes && incident.editor_notes !== '' && (
             <Row className="mt-2">
               <Col>
-                <div>
+                <div style={{ 'overflow-wrap': 'anywhere' }}>
                   <strong>Editor Notes</strong>: {incident.editor_notes}
                 </div>
               </Col>
