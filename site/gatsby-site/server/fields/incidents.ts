1036802a05a32bbe02474863091376f619d538ec
Pablo Costa <pablo@botsfactory.io>
2025-06-05 18:21:31 -0300

    Add dirty field to `updateOneIncidentTranslation`

--- site/gatsby-site/server/fields/incidents.ts
+++ site/gatsby-site/server/fields/incidents.ts
@@ -1,4 +1,4 @@
-import { GraphQLFieldConfigMap, GraphQLInputObjectType, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLString } from "graphql";
+import { GraphQLBoolean, GraphQLFieldConfigMap, GraphQLInputObjectType, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLString } from "graphql";
 import { allow } from "graphql-shield";
 import { generateMutationFields, generateQueryFields, getQueryResolver } from "../utils";
 import { Context, DBIncident } from "../interfaces";
@@ -114,6 +114,7 @@ export const mutationFields: GraphQLFieldConfigMap<any, Context> = {
                         incident_id: { type: new GraphQLNonNull(GraphQLInt) },
                         title: { type: new GraphQLNonNull(GraphQLString) },
                         description: { type: new GraphQLNonNull(GraphQLString) },
+                        dirty: { type: GraphQLBoolean },
                     },
                 }))
             }
@@ -128,6 +129,7 @@ export const mutationFields: GraphQLFieldConfigMap<any, Context> = {
                 title: args.input.title,
                 description: args.input.description,
                 language: args.input.language,
+                dirty: args.input.dirty,
             };
 
             await translationsCollection.updateOne(
