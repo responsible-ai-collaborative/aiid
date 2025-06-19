e6c0e84c080f9a5dcbbcae93a0822e712072864b
cesar varela <mail@cesarvarela.com>
2025-06-19 20:52:07 -0300

    Add magic link interstitial page to prevent Safe Links like tools to use the token

---
+++ site/gatsby-site/src/pages/magic-link.js
@@ -0,0 +1,49 @@
+import React, { useCallback } from 'react';
+import { Button } from 'flowbite-react';
+import { Trans, useTranslation } from 'react-i18next';
+import HeadContent from 'components/HeadContent';
+
+const MagicLink = ({ location }) => {
+  const params = new URLSearchParams(location.search);
+
+  const link = params.get('link');
+
+  const handleClick = useCallback(() => {
+    if (link) {
+      window.location.href = decodeURIComponent(link);
+    }
+  }, [link]);
+
+  if (!link) {
+    return (
+      <div>
+        <Trans>Invalid link</Trans>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <p>
+        <Trans>Click the button below to continue.</Trans>
+      </p>
+      <Button onClick={handleClick} data-cy="magic-link-btn">
+        <Trans>Continue</Trans>
+      </Button>
+    </div>
+  );
+};
+
+export const Head = (props) => {
+  const { t } = useTranslation();
+
+  return (
+    <HeadContent
+      metaTitle={t('AIID - Continue')}
+      path={props.location.pathname}
+      metaDescription={t('Continue to your destination')}
+    />
+  );
+};
+
+export default MagicLink;
