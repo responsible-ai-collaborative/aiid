3ce6229aabf78673c120233c41b8b9dabe5a6068
Pablo Costa <pablo@botsfactory.io>
2025-07-22 15:01:25 -0300

    Add Sentry test button on all environments

--- site/gatsby-site/src/components/SentryTestButton.js
+++ site/gatsby-site/src/components/SentryTestButton.js
@@ -64,9 +64,9 @@ const SentryTestButton = () => {
   };
 
   // Only show in development
-  if (process.env.NODE_ENV !== 'development') {
-    return null;
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return null;
+  // }
 
   return (
     <div className="fixed bottom-4 right-4 bg-white p-4 rounded-lg shadow-lg border z-50">
