633001c2d96fdc8ce9469181208784f4b829908f
Luna McNulty <l.mcnulty@riseup.net>
2025-09-11 13:14:31 -0400

    Add snippet control limits in UI

--- site/gatsby-site/src/components/reports/ReportText.js
+++ site/gatsby-site/src/components/reports/ReportText.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeTruncate from 'rehype-truncate';
+import Link from '../ui/Link';
 
-function ReportText({ text, maxChars = null }) {
+function ReportText({ text, maxUiChars = null, maxQuotationChars = null, continueLink = null }) {
   const rehypePlugins = [];
+
+  let maxChars = null;
+
+  if (maxUiChars && maxQuotationChars) {
+    maxChars = Math.min(maxUiChars, maxQuotationChars);
+  } else if (maxUiChars) {
+    maxChars = maxUiChars;
+  } else if (maxQuotationChars) {
+    maxChars = maxQuotationChars;
+  }
 
   if (maxChars) {
     rehypePlugins.push([rehypeTruncate, { maxChars }]);
   }
 
+  const isLimitedByUiChars = maxUiChars && maxUiChars < text.length;
+
+  const isLimitedByQuotationChars = maxQuotationChars && maxQuotationChars < text.length;
+
+  const displayContinueLink = continueLink && !isLimitedByUiChars && isLimitedByQuotationChars;
+
   return (
-    <ReactMarkdown rehypePlugins={rehypePlugins} className="react-markdown prose max-w-full">
-      {text}
-    </ReactMarkdown>
+    <>
+      <ReactMarkdown rehypePlugins={rehypePlugins} className="react-markdown prose max-w-full">
+        {text}
+      </ReactMarkdown>
+      {displayContinueLink && (
+        <>
+          Continue reading at <Link to={continueLink}>the source</Link>
+        </>
+      )}
+      maxQuotationChars: {JSON.stringify(maxQuotationChars)}
+    </>
   );
 }
 
