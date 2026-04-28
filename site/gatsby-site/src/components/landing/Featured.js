446e1e8141e5a809cca5a24646ffcbef67534a15
Sean McGregor <gitcommit@seanbmcgregor.com>
2026-04-28 14:59:45 -0400

    added more logos (#3850)
    Co-authored-by: Sean McGregor <sean.mcgregor@averi.org>

--- site/gatsby-site/src/components/landing/Featured.js
+++ site/gatsby-site/src/components/landing/Featured.js
@@ -18,43 +18,73 @@ export default function Featured() {
           <a
             href="https://www.vice.com/en/article/m7agjq/this-database-is-finally-holding-ai-accountable"
             target="_blank"
-            rel="noreferrer"
-          >
+                     >
             Vice News
           </a>
           ,{' '}
           <a
             href="https://venturebeat.com/2021/01/15/the-ai-incident-database-wants-to-improve-the-safety-of-machine-learning/"
             target="_blank"
-            rel="noreferrer"
-          >
+                     >
             Venture Beat
           </a>
           ,{' '}
           <a
             href="https://www.wired.com/story/artificial-intelligence-hall-shame/"
             target="_blank"
-            rel="noreferrer"
-          >
+                     >
             Wired
           </a>
           ,{' '}
           <a
             href="https://thebulletin.org/2026/01/what-experts-can-learn-by-tracking-ai-harms/"
             target="_blank"
-            rel="noreferrer"
-          >
+                     >
             Bulletin of the Atomic Scientists
-          </a>{' '}
-          , and{' '}
+          </a>
+          ,{' '}
+          <a
+            href="https://hai.stanford.edu/ai-index/2025-ai-index-report"
+            target="_blank"
+                     >
+            Stanford AI Index
+          </a>
+          ,{' '}
+          <a
+            href="https://www.rollingstone.com/culture/culture-features/ai-deepfakes-creators-fight-back-1235535955/"
+            target="_blank"
+                     >
+            Rolling Stone
+          </a>
+          ,{' '}
+          <a
+            href="https://www.theguardian.com/technology/2026/feb/06/deepfake-taking-place-on-an-industrial-scale-study-finds"
+            target="_blank"
+                     >
+            the Guardian
+          </a>
+          ,{' '}
+          <a
+            href="https://hbr.org/2026/03/ai-agents-act-a-lot-like-malware-heres-how-to-contain-the-risks"
+            target="_blank"
+                     >
+            Harvard Business Review
+          </a>
+          ,{' '}
+          <a
+            href="https://www.brasilemfolhas.com.br/2026/02/fraudes-com-deepfake-se-tornam-comuns-em-escala-industrial-alertam-especialistas/"
+            target="_blank"
+                     >
+            Brasil em Folhas
+          </a>
+          ,{' '}
           <a
             href="https://www.newsweek.com/ai-accidents-set-skyrocket-this-year-1795928"
             target="_blank"
-            rel="noreferrer"
-          >
+                     >
             Newsweek
-          </a>{' '}
-          among other outlets.
+          </a>
+          , and other outlets.
         </Trans>
       </p>
 
@@ -62,42 +92,86 @@ export default function Featured() {
         <a
           href="https://arxiv.org/abs/2011.08512"
           target="_blank"
-          rel="noreferrer"
-          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+                   className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
         >
           <img src="/images/news/arxiv.png" alt="Arxiv Logo" loading="lazy" />
         </a>
         <a
           href="https://venturebeat.com/2021/01/15/the-ai-incident-database-wants-to-improve-the-safety-of-machine-learning/"
           target="_blank"
-          rel="noreferrer"
-          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+                   className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
         >
           <img src="/images/news/VentureBeat.png" alt="Venture Beat Logo" loading="lazy" />
         </a>
         <a
           href="https://www.wired.com/story/artificial-intelligence-hall-shame/"
           target="_blank"
-          rel="noreferrer"
-          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+                   className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
         >
           <img src="/images/news/Wired_logo.svg" alt="Wired Logo" loading="lazy" />
         </a>
         <a
           href="https://www.vice.com/en/article/m7agjq/this-database-is-finally-holding-ai-accountable"
           target="_blank"
-          rel="noreferrer"
-          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+                   className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
         >
           <img src="/images/news/vice.png" alt="Vice logo" loading="lazy" />
         </a>
         <a
           href="https://www.newsweek.com/ai-accidents-set-skyrocket-this-year-1795928"
           target="_blank"
-          rel="noreferrer"
+                   className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/newsweek.svg" alt="Newsweek logo" loading="lazy" />
+        </a>
+        <a
+          href="https://time.com/7346091/ai-harm-risk/"
+          target="_blank"
           className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
         >
-          <img src="/images/news/newsweek.svg" alt="Vice logo" loading="lazy" />
+          <img src="/images/news/time.svg" alt="Time logo" loading="lazy" />
+        </a>
+        <a
+          href="https://thebulletin.org/2026/01/what-experts-can-learn-by-tracking-ai-harms/"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/bulletin-atomic-scientists.svg" alt="Bulletin of the Atomic Scientists logo" loading="lazy" />
+        </a>
+        <a
+          href="https://hai.stanford.edu/ai-index/2025-ai-index-report"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/stanford-hai.svg" alt="Stanford HAI logo" loading="lazy" />
+        </a>
+        <a
+          href="https://www.rollingstone.com/culture/culture-features/ai-deepfakes-creators-fight-back-1235535955/"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/rolling-stone.svg" alt="Rolling Stone logo" loading="lazy" />
+        </a>
+        <a
+          href="https://www.theguardian.com/technology/2026/feb/06/deepfake-taking-place-on-an-industrial-scale-study-finds"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/the-guardian.svg" alt="The Guardian logo" loading="lazy" />
+        </a>
+        <a
+          href="https://hbr.org/2026/03/ai-agents-act-a-lot-like-malware-heres-how-to-contain-the-risks"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/hbr.svg" alt="Harvard Business Review logo" loading="lazy" />
+        </a>
+        <a
+          href="https://www.brasilemfolhas.com.br/2026/02/fraudes-com-deepfake-se-tornam-comuns-em-escala-industrial-alertam-especialistas/"
+          target="_blank"
+          className="flex justify-center items-center min-w-[5rem] max-w-[10rem] flex-1"
+        >
+          <img src="/images/news/brasil-em-folhas.svg" alt="Brasil em Folhas logo" loading="lazy" />
         </a>
       </div>
     </Card>
